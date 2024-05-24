b161c1cc154fb1ed5006ba173c5e36e9b5b5c466
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-24 01:11:43 -0700

    solution

--- leet/roman-to-integer.ts
+++ leet/roman-to-integer.ts
@@ -2,5 +2,43 @@
  * https://leetcode.com/problems/roman-to-integer/description/
  */
 export function romanToInt(s: string): number {
-  return -1;
+  let i = 0;
+  let result = 0;
+
+  while (i < s.length) {
+    if (i < s.length - 1) {
+      const twoChars = s[i] + s[i + 1];
+      const subtractive = SUBTRACTIVE[twoChars];
+      if (subtractive !== undefined) {
+        result += subtractive;
+        i += 2;
+        continue;
+      }
+    }
+
+    const roman = ROMAN[s[i]];
+    result += roman;
+    i++;
+  }
+
+  return result;
 }
+
+const ROMAN: Record<string, number> = {
+  M: 1000,
+  D: 500,
+  C: 100,
+  L: 50,
+  X: 10,
+  V: 5,
+  I: 1,
+};
+
+const SUBTRACTIVE: Record<string, number> = {
+  CM: 900,
+  CD: 400,
+  XC: 90,
+  XL: 40,
+  IX: 9,
+  IV: 4,
+};
