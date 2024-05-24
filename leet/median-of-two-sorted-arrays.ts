b48c86e3eb40e99dfd0047291d16cbd0f5bd8b40
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-23 20:20:09 -0700

    solution

--- leet/median-of-two-sorted-arrays.ts
+++ leet/median-of-two-sorted-arrays.ts
@@ -5,5 +5,35 @@ export function findMedianSortedArrays(
   nums1: number[],
   nums2: number[]
 ): number {
-  return -1;
+  const merged: number[] = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < nums1.length || j < nums2.length) {
+    if (i >= nums1.length) {
+      merged.push(nums2[j]);
+      j++;
+      continue;
+    } else if (j >= nums2.length) {
+      merged.push(nums1[i]);
+      i++;
+      continue;
+    }
+
+    if (nums1[i] < nums2[j]) {
+      merged.push(nums1[i]);
+      i++;
+    } else {
+      merged.push(nums2[j]);
+      j++;
+    }
+  }
+
+  if (merged.length % 2 === 1) {
+    const index = Math.floor(merged.length / 2);
+    return merged[index];
+  } else {
+    const half = merged.length / 2;
+    return (merged[half] + merged[half - 1]) / 2;
+  }
 }
