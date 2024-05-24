470a584e3dc2d55f8cf6cf83752b321ee6cef007
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-24 01:27:25 -0700

    solution

--- leet/longest-common-prefix.ts
+++ leet/longest-common-prefix.ts
@@ -2,5 +2,23 @@
  * https://leetcode.com/problems/longest-common-prefix/description/
  */
 export function longestCommonPrefix(strs: string[]): string {
-  return "";
+  if (strs.length === 0) return "";
+  if (strs.length === 1) return strs[0];
+
+  let shortestLength = Infinity;
+  for (let s of strs) {
+    if (s.length < shortestLength) shortestLength = s.length;
+  }
+
+  let result = "";
+  for (let i = 0; i < shortestLength; i++) {
+    for (let strsIndex = 0; strsIndex < strs.length; strsIndex++) {
+      if (strs[0][i] !== strs[strsIndex][i]) {
+        return result;
+      }
+    }
+    result += strs[0][i];
+  }
+
+  return result;
 }
