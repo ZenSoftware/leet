f7638ed1350eaa54998f551ba2fffe406dd3f7d2
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-23 23:45:22 -0700

    efficiency

--- leet/palindrome-number.ts
+++ leet/palindrome-number.ts
@@ -3,5 +3,14 @@
  * Given an integer x, return true if x is a palindrome, and false otherwise.
  */
 export function isPalindrome(x: number): boolean {
-  return false;
+  if (x < 0) return false;
+  const s = x.toString();
+  let start = 0;
+  let end = s.length - 1;
+  while (start < end) {
+    if (s[start] !== s[end]) return false;
+    start++;
+    end--;
+  }
+  return true;
 }
