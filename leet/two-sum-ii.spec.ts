4c45bc2666c54bcfe79031e4329803cc6b8f4785
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-24 06:45:35 -0700

    improved tests

--- leet/two-sum-ii.spec.ts
+++ leet/two-sum-ii.spec.ts
@@ -8,18 +8,19 @@ describe("Two Sum II", () => {
   });
 
   it("evaluates binary search correctly", () => {
-    expect(binarySearch([0, 2, 4, 6, 8, 10], 0)).toEqual(0);
-    expect(binarySearch([0, 2, 4, 6, 8, 10], 2)).toEqual(1);
-    expect(binarySearch([0, 2, 4, 6, 8, 10], 4)).toEqual(2);
-    expect(binarySearch([0, 2, 4, 6, 8, 10], 6)).toEqual(3);
-    expect(binarySearch([0, 2, 4, 6, 8, 10], 8)).toEqual(4);
-    expect(binarySearch([0, 2, 4, 6, 8, 10], 10)).toEqual(5);
-    expect(binarySearch([0, 2, 4, 6, 8, 10], -1)).toEqual(-1);
-    expect(binarySearch([0, 2, 4, 6, 8, 10], 1)).toEqual(-1);
-    expect(binarySearch([0, 2, 4, 6, 8, 10], 3)).toEqual(-1);
-    expect(binarySearch([0, 2, 4, 6, 8, 10], 5)).toEqual(-1);
-    expect(binarySearch([0, 2, 4, 6, 8, 10], 7)).toEqual(-1);
-    expect(binarySearch([0, 2, 4, 6, 8, 10], 9)).toEqual(-1);
-    expect(binarySearch([0, 2, 4, 6, 8, 10], 11)).toEqual(-1);
+    const sample = [0, 2, 4, 6, 8, 10];
+    expect(binarySearch(sample, 0)).toEqual(0);
+    expect(binarySearch(sample, 2)).toEqual(1);
+    expect(binarySearch(sample, 4)).toEqual(2);
+    expect(binarySearch(sample, 6)).toEqual(3);
+    expect(binarySearch(sample, 8)).toEqual(4);
+    expect(binarySearch(sample, 10)).toEqual(5);
+    expect(binarySearch(sample, -1)).toEqual(-1);
+    expect(binarySearch(sample, 1)).toEqual(-1);
+    expect(binarySearch(sample, 3)).toEqual(-1);
+    expect(binarySearch(sample, 5)).toEqual(-1);
+    expect(binarySearch(sample, 7)).toEqual(-1);
+    expect(binarySearch(sample, 9)).toEqual(-1);
+    expect(binarySearch(sample, 11)).toEqual(-1);
   });
 });
