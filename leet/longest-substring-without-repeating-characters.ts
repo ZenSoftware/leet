ac7b659e1e89cb0b81225dde03b2f58eac0a0d4c
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-23 18:31:09 -0700

    skeleton

---
+++ leet/longest-substring-without-repeating-characters.ts
@@ -0,0 +1,8 @@
+/**
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+ * Given a string s, find the length of the longest substring without repeating characters.
+ */
+
+export function lengthOfLongestSubstring(s: string): number {
+  return -1;
+}
