3f9e02717b2534e7ef9754e6baff98769fb2b2df
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-23 23:49:30 -0700

    skeleton

---
+++ leet/container-with-most-water.spec.ts
@@ -0,0 +1,7 @@
+import { maxArea } from "./container-with-most-water";
+
+describe("Container With Most Water", () => {
+  it("evaluates correctly", () => {
+    expect(true).toEqual(true);
+  });
+});
