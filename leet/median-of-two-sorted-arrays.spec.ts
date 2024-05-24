43d014f403042dcd8eee3addc20b0ef992df677c
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-23 20:03:38 -0700

    added tests

--- leet/median-of-two-sorted-arrays.spec.ts
+++ leet/median-of-two-sorted-arrays.spec.ts
@@ -1,5 +1,8 @@
 import { findMedianSortedArrays } from "./median-of-two-sorted-arrays";
 
 describe("Median of Two Sorted Arrays", () => {
-  it("evaluates correctly", () => {});
+  it("evaluates correctly", () => {
+    expect(findMedianSortedArrays([1, 3], [2])).toEqual(2);
+    expect(findMedianSortedArrays([1, 2], [3, 4])).toEqual(2.5);
+  });
 });
