67822c3316e23b4b2f5d6717f386090fa95f16a9
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-24 00:06:50 -0700

    skeleton

---
+++ leet/integer-to-roman.spec.ts
@@ -0,0 +1,7 @@
+/**
+ * https://leetcode.com/problems/integer-to-roman/description/
+ */
+
+export function intToRoman(num: number): string {
+  return "";
+}
---
+++ leet/integer-to-roman.ts
@@ -0,0 +1,5 @@
+import { intToRoman } from "./integer-to-roman.spec";
+
+describe("Integer to Roman", () => {
+  it("evaluates correctly", () => {});
+});
