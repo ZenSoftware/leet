a0a017aeeeb50285f36b06acd3e55d26b1c9793d
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-24 08:11:56 -0700

    removed bad solution

--- leet/two-sum-ii.spec.ts
+++ leet/two-sum-ii.spec.ts
@@ -1,4 +1,4 @@
-import { twoSum, binarySearch } from "./two-sum-ii";
+import { twoSum } from "./two-sum-ii-b";
 
 describe("Two Sum II", () => {
   it("evaluates two sum correctly", () => {
@@ -7,30 +7,5 @@ describe("Two Sum II", () => {
     expect(twoSum([-1, 0], -1)).toEqual([1, 2]);
     expect(twoSum([0, 0, 3, 4], 0)).toEqual([1, 2]);
     expect(twoSum([1, 2, 3, 4, 4, 9, 56, 90], 8)).toEqual([4, 5]);
-  });
-
-  it("evaluates binary search correctly", () => {
-    const sample = [0, 2, 4, 6, 8, 10];
-    expect(binarySearch(sample, 0, -1)).toEqual(0);
-    expect(binarySearch(sample, 2, -1)).toEqual(1);
-    expect(binarySearch(sample, 4, -1)).toEqual(2);
-    expect(binarySearch(sample, 6, -1)).toEqual(3);
-    expect(binarySearch(sample, 8, -1)).toEqual(4);
-    expect(binarySearch(sample, 10, -1)).toEqual(5);
-
-    expect(binarySearch(sample, 0, 0)).toEqual(-1);
-    expect(binarySearch(sample, 2, 1)).toEqual(-1);
-    expect(binarySearch(sample, 4, 2)).toEqual(-1);
-    expect(binarySearch(sample, 6, 3)).toEqual(-1);
-    expect(binarySearch(sample, 8, 4)).toEqual(-1);
-    expect(binarySearch(sample, 10, 5)).toEqual(-1);
-
-    expect(binarySearch(sample, -1, -1)).toEqual(-1);
-    expect(binarySearch(sample, 1, -1)).toEqual(-1);
-    expect(binarySearch(sample, 3, -1)).toEqual(-1);
-    expect(binarySearch(sample, 5, -1)).toEqual(-1);
-    expect(binarySearch(sample, 7, -1)).toEqual(-1);
-    expect(binarySearch(sample, 9, -1)).toEqual(-1);
-    expect(binarySearch(sample, 11, -1)).toEqual(-1);
   });
 });
--- leet/two-sum-ii.ts
+++ leet/two-sum-ii.ts
@@ -2,38 +2,15 @@
  * https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
  */
 export function twoSum(numbers: number[], target: number): number[] {
-  let result: number[] = [];
+  let l = 0;
+  let r = numbers.length - 1;
 
-  for (let i = 0; i < numbers.length; i++) {
-    const find = target - numbers[i];
-    const foundIndex = binarySearch(numbers, find, i);
-    if (foundIndex !== -1) {
-      if (i < foundIndex) {
-        return [i + 1, foundIndex + 1];
-      } else {
-        return [foundIndex + 1, i + 1];
-      }
-    }
+  while (l < r) {
+    let sum = numbers[l] + numbers[r];
+    if (sum === target) return [l + 1, r + 1];
+    else if (sum > target) r--;
+    else l++;
   }
 
-  return result;
-}
-
-export function binarySearch(
-  numbers: number[],
-  find: number,
-  excludeIndex: number
-) {
-  function bs(start: number, end: number) {
-    if (excludeIndex !== start && numbers[start] === find) return start;
-    if (excludeIndex !== end && numbers[end] === find) return end;
-    if (start >= end) return -1;
-
-    const mid = Math.floor((start + end) / 2);
-
-    if (find <= numbers[mid]) return bs(start, mid);
-    else return bs(mid + 1, end);
-  }
-
-  return bs(0, numbers.length - 1);
+  throw Error("No solution exists");
 }
