c4df63b1d626608918d85d4e814ea4c5eb5ff1ac
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-24 07:44:30 -0700

    comment

--- leet/two-sum-ii-b.ts
+++ leet/two-sum-ii-b.ts
@@ -2,5 +2,15 @@
  * https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
  */
 export function twoSum(numbers: number[], target: number): number[] {
-  return [];
+  let l = 0;
+  let r = numbers.length - 1;
+
+  while (l < r) {
+    let sum = numbers[l] + numbers[r];
+    if (sum === target) return [l + 1, r + 1];
+    else if (sum > target) r--;
+    else l++;
+  }
+
+  throw Error("No solution exists");
 }
