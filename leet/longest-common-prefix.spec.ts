20e2fa1d109a58c3460bafbad3637e4fd75f98a7
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-24 01:14:53 -0700

    added tests

--- leet/longest-common-prefix.spec.ts
+++ leet/longest-common-prefix.spec.ts
@@ -1,5 +1,8 @@
 import { longestCommonPrefix } from "./longest-common-prefix";
 
 describe("Longest Common Prefix", () => {
-  it("evaluates correctly", () => {});
+  it("evaluates correctly", () => {
+    expect(longestCommonPrefix(["flower", "flow", "flight"])).toEqual("fl");
+    expect(longestCommonPrefix(["dog", "racecar", "car"])).toEqual("");
+  });
 });
