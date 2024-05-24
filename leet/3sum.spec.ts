a01be1db19c734c62fb5911d08307334fd25b789
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-24 01:44:17 -0700

    added tests

--- leet/3sum.spec.ts
+++ leet/3sum.spec.ts
@@ -2,6 +2,11 @@ import { threeSum } from "./3sum";
 
 describe("3Sum", () => {
   it("evaluates correctly", () => {
-    expect(true).toEqual(true);
+    expect([-1, 0, 1, 2, -1, -4]).toEqual([
+      [-1, -1, 2],
+      [-1, 0, 1],
+    ]);
+    expect([0, 1, 1]).toEqual([]);
+    expect([0, 0, 0]).toEqual([[0, 0, 0]]);
   });
 });
