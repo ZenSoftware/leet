352d6f7e81b13e07fe7c6d193eef60c9182e8533
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-23 23:40:45 -0700

    added tests

--- leet/palindrome-number.spec.ts
+++ leet/palindrome-number.spec.ts
@@ -1,5 +1,9 @@
 import { isPalindrome } from "./palindrome-number";
 
 describe("Palindrome Number", () => {
-  it("evaluates correctly", () => {});
+  it("evaluates correctly", () => {
+    expect(isPalindrome(121)).toEqual(true);
+    expect(isPalindrome(-121)).toEqual(false);
+    expect(isPalindrome(10)).toEqual(false);
+  });
 });
