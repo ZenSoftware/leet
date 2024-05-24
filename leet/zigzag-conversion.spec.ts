8cff701f229fa04955e4b21ee3fd09a937f87625
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-23 22:19:36 -0700

    added tests

--- leet/zigzag-conversion.spec.ts
+++ leet/zigzag-conversion.spec.ts
@@ -1,5 +1,9 @@
 import { convert } from "./zigzag-conversion";
 
 describe("Zigzag Conversion", () => {
-  it("evaluates correctly", () => {});
+  it("evaluates correctly", () => {
+    expect(convert("PAYPALISHIRING", 3)).toEqual("PAHNAPLSIIGYIR");
+    expect(convert("PAYPALISHIRING", 4)).toEqual("PINALSIGYAHRPI");
+    expect(convert("A", 1)).toEqual("A");
+  });
 });
