55d4e6a462d65bec7c9e68296441f4b9ab4a7c59
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-24 00:26:30 -0700

    skeleton

--- leet/integer-to-roman.ts
+++ leet/integer-to-roman.ts
@@ -3,5 +3,27 @@
  */
 
 export function intToRoman(num: number): string {
-  return "";
+  const s = num.toString();
+  let result = "";
+
+  return result;
 }
+
+const ROMAN = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
+
+const SUBTRACTIVE = {
+  4: "IV",
+  9: "IX",
+  40: "XL",
+  90: "XC",
+  400: "CD",
+  900: "CM",
+};
