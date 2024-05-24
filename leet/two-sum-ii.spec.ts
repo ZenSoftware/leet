f20397cd2e1fd5436778d746c014789dea3d2407
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-24 07:09:11 -0700

    solution

--- leet/two-sum-ii.spec.ts
+++ leet/two-sum-ii.spec.ts
@@ -5,22 +5,32 @@ describe("Two Sum II", () => {
     expect(twoSum([2, 7, 11, 15], 9)).toEqual([1, 2]);
     expect(twoSum([2, 3, 4], 6)).toEqual([1, 3]);
     expect(twoSum([-1, 0], -1)).toEqual([1, 2]);
+    expect(twoSum([0, 0, 3, 4], 0)).toEqual([1, 2]);
+    expect(twoSum([1, 2, 3, 4, 4, 9, 56, 90], 8)).toEqual([4, 5]);
   });
 
   it("evaluates binary search correctly", () => {
     const sample = [0, 2, 4, 6, 8, 10];
-    expect(binarySearch(sample, 0)).toEqual(0);
-    expect(binarySearch(sample, 2)).toEqual(1);
-    expect(binarySearch(sample, 4)).toEqual(2);
-    expect(binarySearch(sample, 6)).toEqual(3);
-    expect(binarySearch(sample, 8)).toEqual(4);
-    expect(binarySearch(sample, 10)).toEqual(5);
-    expect(binarySearch(sample, -1)).toEqual(-1);
-    expect(binarySearch(sample, 1)).toEqual(-1);
-    expect(binarySearch(sample, 3)).toEqual(-1);
-    expect(binarySearch(sample, 5)).toEqual(-1);
-    expect(binarySearch(sample, 7)).toEqual(-1);
-    expect(binarySearch(sample, 9)).toEqual(-1);
-    expect(binarySearch(sample, 11)).toEqual(-1);
+    expect(binarySearch(sample, 0, -1)).toEqual(0);
+    expect(binarySearch(sample, 2, -1)).toEqual(1);
+    expect(binarySearch(sample, 4, -1)).toEqual(2);
+    expect(binarySearch(sample, 6, -1)).toEqual(3);
+    expect(binarySearch(sample, 8, -1)).toEqual(4);
+    expect(binarySearch(sample, 10, -1)).toEqual(5);
+
+    expect(binarySearch(sample, 0, 0)).toEqual(-1);
+    expect(binarySearch(sample, 2, 1)).toEqual(-1);
+    expect(binarySearch(sample, 4, 2)).toEqual(-1);
+    expect(binarySearch(sample, 6, 3)).toEqual(-1);
+    expect(binarySearch(sample, 8, 4)).toEqual(-1);
+    expect(binarySearch(sample, 10, 5)).toEqual(-1);
+
+    expect(binarySearch(sample, -1, -1)).toEqual(-1);
+    expect(binarySearch(sample, 1, -1)).toEqual(-1);
+    expect(binarySearch(sample, 3, -1)).toEqual(-1);
+    expect(binarySearch(sample, 5, -1)).toEqual(-1);
+    expect(binarySearch(sample, 7, -1)).toEqual(-1);
+    expect(binarySearch(sample, 9, -1)).toEqual(-1);
+    expect(binarySearch(sample, 11, -1)).toEqual(-1);
   });
 });
--- leet/two-sum-ii.ts
+++ leet/two-sum-ii.ts
@@ -2,13 +2,31 @@
  * https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
  */
 export function twoSum(numbers: number[], target: number): number[] {
-  return [];
+  let result: number[] = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    const find = target - numbers[i];
+    const foundIndex = binarySearch(numbers, find, i);
+    if (foundIndex !== -1) {
+      if (i < foundIndex) {
+        return [i + 1, foundIndex + 1];
+      } else {
+        return [foundIndex + 1, i + 1];
+      }
+    }
+  }
+
+  return result;
 }
 
-export function binarySearch(numbers: number[], find: number) {
+export function binarySearch(
+  numbers: number[],
+  find: number,
+  excludeIndex: number
+) {
   function bs(start: number, end: number) {
-    if (numbers[start] === find) return start;
-    if (numbers[end] === find) return end;
+    if (excludeIndex !== start && numbers[start] === find) return start;
+    if (excludeIndex !== end && numbers[end] === find) return end;
     if (start >= end) return -1;
 
     const mid = Math.floor((start + end) / 2);
