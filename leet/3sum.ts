230bebfff347042d20f4e8f554a44854d10b0856
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-24 02:05:54 -0700

    solution

--- leet/3sum.ts
+++ leet/3sum.ts
@@ -2,7 +2,32 @@
  * https://leetcode.com/problems/3sum/description/
  */
 export function threeSum(nums: number[]): number[][] {
-  let result: number[][] = [];
+  let results: number[][] = [];
 
-  return result;
+  for (let i = 0; i < nums.length - 2; i++) {
+    for (let j = i + 1; j < nums.length - 1; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        if (nums[i] + nums[j] + nums[k] === 0) {
+          const solution = [nums[i], nums[j], nums[k]];
+          solution.sort((a, b) => a - b);
+          if (!contains(results, solution)) {
+            results.push(solution);
+          }
+        }
+      }
+    }
+  }
+
+  return results;
+}
+
+function contains(result: number[][], solution: number[]): boolean {
+  for (let r of result) {
+    let contains = false;
+    for (let i = 0; i < 3; i++) {
+      if (r[i] !== solution[i]) break;
+      if (i === 2 && r[i] === solution[i]) return true;
+    }
+  }
+  return false;
 }
