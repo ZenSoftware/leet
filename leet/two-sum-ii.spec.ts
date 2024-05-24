eef0e8638e33cad1c2dd2b94f39dbdf106ddac52
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-24 06:42:05 -0700

    binary search done

--- leet/two-sum-ii.spec.ts
+++ leet/two-sum-ii.spec.ts
@@ -1,6 +1,6 @@
 import { twoSum, binarySearch } from "./two-sum-ii";
 
-describe("", () => {
+describe("Two Sum II", () => {
   it("evaluates correctly", () => {
     expect(twoSum([2, 7, 11, 15], 9)).toEqual([1, 2]);
     expect(twoSum([2, 3, 4], 6)).toEqual([1, 3]);
@@ -8,6 +8,18 @@ describe("", () => {
   });
 
   it("evaluates binary search correctly", () => {
-    expect(binarySearch([2, 7, 11, 15], 7)).toEqual(1);
+    expect(binarySearch([0, 2, 4, 6, 8, 10], 0)).toEqual(0);
+    expect(binarySearch([0, 2, 4, 6, 8, 10], 2)).toEqual(1);
+    expect(binarySearch([0, 2, 4, 6, 8, 10], 4)).toEqual(2);
+    expect(binarySearch([0, 2, 4, 6, 8, 10], 6)).toEqual(3);
+    expect(binarySearch([0, 2, 4, 6, 8, 10], 8)).toEqual(4);
+    expect(binarySearch([0, 2, 4, 6, 8, 10], 10)).toEqual(5);
+    expect(binarySearch([0, 2, 4, 6, 8, 10], -1)).toEqual(-1);
+    expect(binarySearch([0, 2, 4, 6, 8, 10], 1)).toEqual(-1);
+    expect(binarySearch([0, 2, 4, 6, 8, 10], 3)).toEqual(-1);
+    expect(binarySearch([0, 2, 4, 6, 8, 10], 5)).toEqual(-1);
+    expect(binarySearch([0, 2, 4, 6, 8, 10], 7)).toEqual(-1);
+    expect(binarySearch([0, 2, 4, 6, 8, 10], 9)).toEqual(-1);
+    expect(binarySearch([0, 2, 4, 6, 8, 10], 11)).toEqual(-1);
   });
 });
--- leet/two-sum-ii.ts
+++ leet/two-sum-ii.ts
@@ -6,7 +6,16 @@ export function twoSum(numbers: number[], target: number): number[] {
 }
 
 export function binarySearch(numbers: number[], target: number) {
-  function bs(start: number, end: number) {}
+  function bs(start: number, end: number) {
+    if (numbers[start] === target) return start;
+    if (numbers[end] === target) return end;
+    if (start >= end) return -1;
+
+    const mid = Math.floor((start + end) / 2);
+
+    if (target <= numbers[mid]) return bs(start, mid);
+    else return bs(mid + 1, end);
+  }
 
   return bs(0, numbers.length - 1);
 }
