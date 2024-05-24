974b6648219eef412b6f567d0306cf220c51f045
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-24 01:42:34 -0700

    skeleton

---
+++ leet/3sum.spec.ts
@@ -0,0 +1,5 @@
+import { threeSum } from "./3sum";
+
+describe("", () => {
+  it("evaluates correctly", () => {});
+});
---
+++ leet/3sum.ts
@@ -0,0 +1,6 @@
+/**
+ * https://leetcode.com/problems/3sum/description/
+ */
+export function threeSum(nums: number[]): number[][] {
+  return null as any;
+}
