4d7a6105ca0812d19b059aac49c7cc347f759690
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-24 06:47:52 -0700

    tests

--- leet/two-sum-ii.spec.ts
+++ leet/two-sum-ii.spec.ts
@@ -1,7 +1,7 @@
 import { twoSum, binarySearch } from "./two-sum-ii";
 
 describe("Two Sum II", () => {
-  it("evaluates correctly", () => {
+  it("evaluates two sum correctly", () => {
     expect(twoSum([2, 7, 11, 15], 9)).toEqual([1, 2]);
     expect(twoSum([2, 3, 4], 6)).toEqual([1, 3]);
     expect(twoSum([-1, 0], -1)).toEqual([1, 2]);
