7ea98b74096f36eff2b3fb6058e64d63d89f498b
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-23 20:32:16 -0700

    added tests

--- leet/longest-palindromic-substring.spec.ts
+++ leet/longest-palindromic-substring.spec.ts
@@ -9,6 +9,8 @@ describe("Longest Palindromic Substring", () => {
   it("evaluates isPalindrome correctly", () => {
     expect(isPalindrome("abba")).toEqual(true);
     expect(isPalindrome("abcba")).toEqual(true);
+    expect(isPalindrome("a")).toEqual(true);
+    expect(isPalindrome("")).toEqual(true);
     expect(isPalindrome("abc")).toEqual(false);
   });
 });
