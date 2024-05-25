2a197fc3e099baa4979c020685f6c9e6863f2773
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-25 15:44:45 -0700

    added tests

---
+++ leet/remove-duplicates-from-sorted-array.spec.ts
@@ -0,0 +1,20 @@
+import { removeDuplicates } from './remove-duplicates-from-sorted-array';
+
+describe('Remove Duplicates from Sorted Array', () => {
+  it('evaluates correctly 1', () => {
+    const input = [1, 1, 2];
+    expect(removeDuplicates(input)).toEqual(2);
+    expect(input[0]).toEqual(1);
+    expect(input[1]).toEqual(2);
+  });
+
+  it('evaluates correctly 1', () => {
+    const input = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+    expect(removeDuplicates(input)).toEqual(5);
+    expect(input[0]).toEqual(0);
+    expect(input[1]).toEqual(1);
+    expect(input[2]).toEqual(2);
+    expect(input[3]).toEqual(3);
+    expect(input[4]).toEqual(4);
+  });
+});
---
+++ leet/remove-duplicates-from-sorted-array.ts
@@ -0,0 +1,6 @@
+/**
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+ */
+export function removeDuplicates(nums: number[]): number {
+  return -1;
+}
