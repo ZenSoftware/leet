4d5f3fc19b3c0331f3ef9205bd31cf22785b9723
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-25 08:54:45 -0700

    skeleton

---
+++ leet/merge-two-sorted-lists.spec.ts
@@ -0,0 +1,7 @@
+import { mergeTwoLists } from './merge-two-sorted-lists';
+
+describe('Merge Two Sorted Lists', () => {
+  it('evaluates correctly', () => {
+    expect(true).toEqual(false);
+  });
+});
---
+++ leet/merge-two-sorted-lists.ts
@@ -0,0 +1,14 @@
+/**
+ * https://leetcode.com/problems/merge-two-sorted-lists/description/
+ */
+
+export function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {}
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
