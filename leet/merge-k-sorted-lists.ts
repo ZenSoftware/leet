b1ed877f05a8c9527424df771f9a217e8599f5c0
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-25 14:54:04 -0700

    fixed

--- leet/merge-k-sorted-lists.ts
+++ leet/merge-k-sorted-lists.ts
@@ -17,20 +17,27 @@ class ListNode {
 
 export function toList(elements: number[]) {
   if (elements.length === 0) return null;
+
   let pointer: ListNode | null = null;
+
   for (let i = elements.length - 1; i >= 0; i--) {
     const node = new ListNode(elements[i]);
     node.next = pointer;
+    pointer = node;
   }
+
   return pointer;
 }
 
 export function toArray(list: ListNode | null) {
   if (!list) return [];
+
   const result: number[] = [];
+
   while (list) {
     result.push(list.val);
     list = list.next;
   }
+
   return result;
 }
