978420ea85191fdaa4ec5aca276adad9f95b82a9
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-25 08:48:55 -0700

    solution

--- leet/valid-parentheses.ts
+++ leet/valid-parentheses.ts
@@ -1,4 +1,21 @@
 /**
  * https://leetcode.com/problems/valid-parentheses/description/
  */
-export function isValid(s: string): boolean {}
+export function isValid(s: string): boolean {
+  const history: string[] = [];
+  const openBrackets = ['(', '[', '{'];
+
+  for (let char of s) {
+    if (openBrackets.includes(char)) {
+      history.push(char);
+    } else {
+      const last = history.pop();
+      if (last === '(' && char === ')') continue;
+      else if (last === '[' && char === ']') continue;
+      else if (last === '{' && char === '}') continue;
+      return false;
+    }
+  }
+
+  return history.length === 0;
+}
