7f9ce450483ade177a9c401fe0d7ac80abb46193
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-25 15:07:49 -0700

    utilities

--- leet/swap-nodes-in-pairs.spec.ts
+++ leet/swap-nodes-in-pairs.spec.ts
@@ -1,7 +1,13 @@
-import { swapPairs } from './swap-nodes-in-pairs';
+import { swapPairs, toArray, toList } from './swap-nodes-in-pairs';
 
 describe('Swap Nodes in Pairs', () => {
   it('evaluates correctly', () => {
     expect(true).toEqual(true);
+  });
+
+  it('constructs lists correctly', () => {
+    const list = toList([1, 2, 3, 4]);
+    const array = toArray(list);
+    expect(array).toEqual([1, 2, 3, 4]);
   });
 });
