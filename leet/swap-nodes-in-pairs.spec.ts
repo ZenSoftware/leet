68280e7fa4624b2245c9f28de099a08e1f9f7e47
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-25 15:09:20 -0700

    added tests

--- leet/swap-nodes-in-pairs.spec.ts
+++ leet/swap-nodes-in-pairs.spec.ts
@@ -1,8 +1,28 @@
 import { swapPairs, toArray, toList } from './swap-nodes-in-pairs';
 
 describe('Swap Nodes in Pairs', () => {
-  it('evaluates correctly', () => {
-    expect(true).toEqual(true);
+  it('evaluates correctly 1', () => {
+    const input = toList([1, 2, 3, 4]);
+    const result = swapPairs(input);
+    expect(toArray(result)).toEqual([2, 1, 4, 3]);
+  });
+
+  it('evaluates correctly 2', () => {
+    const input = toList([]);
+    const result = swapPairs(input);
+    expect(toArray(result)).toEqual([]);
+  });
+
+  it('evaluates correctly 3', () => {
+    const input = toList([1]);
+    const result = swapPairs(input);
+    expect(toArray(result)).toEqual([1]);
+  });
+
+  it('evaluates correctly 4', () => {
+    const input = toList([1, 2, 3]);
+    const result = swapPairs(input);
+    expect(toArray(result)).toEqual([2, 1, 3]);
   });
 
   it('constructs lists correctly', () => {
