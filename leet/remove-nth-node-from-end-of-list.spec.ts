7716de663631faef4d16177c8a3088bf5be3b53f
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-25 01:40:01 -0700

    tests

--- leet/remove-nth-node-from-end-of-list.spec.ts
+++ leet/remove-nth-node-from-end-of-list.spec.ts
@@ -1,7 +1,19 @@
-import { removeNthFromEnd } from './remove-nth-node-from-end-of-list';
+import { removeNthFromEnd, toList, toArray } from './remove-nth-node-from-end-of-list';
 
 describe('Remove Nth Node From End of List', () => {
-  it('evaluates correctly', () => {
-    expect(true).toEqual(true);
+  it('evaluates correctly 1', () => {
+    const list = toList([1, 2, 3, 4, 5]);
+    const result = removeNthFromEnd(list, 2);
+    expect(toArray(result)).toEqual([1, 2, 3, 5]);
+  });
+  it('evaluates correctly 2', () => {
+    const list = toList([1]);
+    const result = removeNthFromEnd(list, 1);
+    expect(toArray(result)).toEqual([]);
+  });
+  it('evaluates correctly 3', () => {
+    const list = toList([1, 2]);
+    const result = removeNthFromEnd(list, 1);
+    expect(toArray(result)).toEqual([1]);
   });
 });
--- leet/remove-nth-node-from-end-of-list.ts
+++ leet/remove-nth-node-from-end-of-list.ts
@@ -14,3 +14,29 @@ class ListNode {
     this.next = next === undefined ? null : next;
   }
 }
+
+export function toList(elements: number[]) {
+  if (elements.length === 0) return null;
+
+  let pointer: ListNode | null = null;
+  for (let i = elements.length - 1; i >= 0; i--) {
+    const node = new ListNode(elements[i]);
+    node.next = pointer;
+    pointer = node;
+  }
+
+  return pointer;
+}
+
+export function toArray(list: ListNode | null) {
+  if (!list) return [];
+
+  const result: number[] = [];
+  let pointer: ListNode | null = list;
+  while (pointer) {
+    result.push(pointer.val);
+    pointer = pointer.next;
+  }
+
+  return result;
+}
