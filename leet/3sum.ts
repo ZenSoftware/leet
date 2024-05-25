53b79274a31b93f57cc7d34aceee72bd7eb3e353
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-25 01:16:05 -0700

    solution

--- leet/3sum.ts
+++ leet/3sum.ts
@@ -5,5 +5,25 @@ export function threeSum(nums: number[]): number[][] {
   let results: number[][] = [];
   nums.sort((a, b) => a - b);
 
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (nums[i] === nums[i - 1]) continue;
+
+    let left = i + 1;
+    let right = nums.length - 1;
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+      if (sum === 0) {
+        results.push([nums[i], nums[left], nums[right]]);
+        left++;
+        right--;
+        while (nums[left] === nums[left + 1] && left < right) left++;
+      } else if (sum < 0) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+
   return results;
 }
