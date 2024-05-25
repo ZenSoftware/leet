dcaf16143b1d64a6d59c2e1b5666b93d49dd2206
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-25 16:22:15 -0700

    really slow solution

--- leet/remove-duplicates-from-sorted-array.spec.ts
+++ leet/remove-duplicates-from-sorted-array.spec.ts
@@ -8,7 +8,18 @@ describe('Remove Duplicates from Sorted Array', () => {
     expect(input[1]).toEqual(2);
   });
 
-  it('evaluates correctly 1', () => {
+  it('evaluates correctly 2', () => {
+    const input: number[] = [];
+    expect(removeDuplicates(input)).toEqual(0);
+  });
+
+  it('evaluates correctly 3', () => {
+    const input = [1];
+    expect(removeDuplicates(input)).toEqual(1);
+    expect(input[0]).toEqual(1);
+  });
+
+  it('evaluates correctly 4', () => {
     const input = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
     expect(removeDuplicates(input)).toEqual(5);
     expect(input[0]).toEqual(0);
--- leet/remove-duplicates-from-sorted-array.ts
+++ leet/remove-duplicates-from-sorted-array.ts
@@ -2,5 +2,29 @@
  * https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
  */
 export function removeDuplicates(nums: number[]): number {
-  return -1;
+  if (nums.length === 0) return 0;
+  if (nums.length === 1) return 1;
+
+  let k = 0;
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    let j = i;
+
+    while (nums[j] === nums[j + 1]) {
+      j++;
+
+      const temp = nums[j];
+
+      for (let k = j; k < nums.length - 1; k++) {
+        nums[k] = nums[k + 1];
+      }
+
+      nums[nums.length - 1] = temp;
+    }
+
+    k++;
+    if (nums[i] > nums[i + 1]) break;
+  }
+
+  return k;
 }
