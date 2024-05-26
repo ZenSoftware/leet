1f51a69de0bf743d11e5bf6a91abb56fde107cf8
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-26 13:04:20 -0700

    binary search

--- leet/find-first-and-last-position-of-element-in-sorted-array.spec.ts
+++ leet/find-first-and-last-position-of-element-in-sorted-array.spec.ts
@@ -1,9 +1,27 @@
-import { searchRange } from './find-first-and-last-position-of-element-in-sorted-array';
+import {
+  searchRange,
+  binarySearch,
+} from './find-first-and-last-position-of-element-in-sorted-array';
 
 describe('Find First and Last Position of Element in Sorted Array', () => {
   it('evaluates correctly', () => {
     expect(searchRange([5, 7, 7, 8, 8, 10], 8)).toEqual([3, 4]);
     expect(searchRange([5, 7, 7, 8, 8, 10], 6)).toEqual([-1, -1]);
     expect(searchRange([], 0)).toEqual([-1, -1]);
+  });
+
+  it('evaluates binary search correctly', () => {
+    expect(binarySearch([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], -1)).toEqual(-1);
+    expect(binarySearch([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 0)).toEqual(0);
+    expect(binarySearch([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 1)).toEqual(1);
+    expect(binarySearch([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 2)).toEqual(2);
+    expect(binarySearch([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 3)).toEqual(3);
+    expect(binarySearch([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 4)).toEqual(4);
+    expect(binarySearch([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 5)).toEqual(5);
+    expect(binarySearch([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 6)).toEqual(6);
+    expect(binarySearch([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 7)).toEqual(7);
+    expect(binarySearch([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 8)).toEqual(8);
+    expect(binarySearch([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 9)).toEqual(9);
+    expect(binarySearch([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10)).toEqual(-1);
   });
 });
--- leet/find-first-and-last-position-of-element-in-sorted-array.ts
+++ leet/find-first-and-last-position-of-element-in-sorted-array.ts
@@ -5,3 +5,18 @@
 export function searchRange(nums: number[], target: number): number[] {
   return [];
 }
+
+export function binarySearch(nums: number[], find: number): number {
+  function bs(start: number, end: number) {
+    if (nums[start] === find) return start;
+    if (nums[end] === find) return end;
+    if (start >= end) return -1;
+
+    const mid = Math.floor((start + end) / 2);
+
+    if (find <= nums[mid]) return bs(start, mid);
+    else return bs(mid + 1, end);
+  }
+
+  return bs(0, nums.length);
+}
