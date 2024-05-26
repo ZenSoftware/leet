e5893ac1e03e3e94e656a2eed4d45ea498f09a5e
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-25 17:48:02 -0700

    skeleton

---
+++ leet/substring-with-concatenation-of-all-words.spec.ts
@@ -0,0 +1,7 @@
+import { findSubstring } from './substring-with-concatenation-of-all-words';
+
+describe('Substring with Concatenation of All Words', () => {
+  it('evaluates correctly', () => {
+    expect(true).toEqual(true);
+  });
+});
---
+++ leet/substring-with-concatenation-of-all-words.ts
@@ -0,0 +1,6 @@
+/**
+ * https://leetcode.com/problems/substring-with-concatenation-of-all-words/description/
+ */
+export function findSubstring(s: string, words: string[]): number[] {
+  return [];
+}
