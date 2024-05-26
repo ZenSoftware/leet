2c573d27a8abc91684be40433d184e05a81a1685
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-25 17:53:41 -0700

    utlities

--- leet/substring-with-concatenation-of-all-words.spec.ts
+++ leet/substring-with-concatenation-of-all-words.spec.ts
@@ -1,7 +1,18 @@
-import { findSubstring } from './substring-with-concatenation-of-all-words';
+import { findSubstring, permutations } from './substring-with-concatenation-of-all-words';
 
 describe('Substring with Concatenation of All Words', () => {
   it('evaluates correctly', () => {
     expect(true).toEqual(true);
+  });
+
+  it('constructs permutations', () => {
+    expect(permutations(['a', 'b', 'c'])).toEqual([
+      ['a', 'b', 'c'],
+      ['b', 'a', 'c'],
+      ['b', 'c', 'a'],
+      ['a', 'c', 'b'],
+      ['c', 'a', 'b'],
+      ['c', 'b', 'a'],
+    ]);
   });
 });
--- leet/substring-with-concatenation-of-all-words.ts
+++ leet/substring-with-concatenation-of-all-words.ts
@@ -4,3 +4,20 @@
 export function findSubstring(s: string, words: string[]): number[] {
   return [];
 }
+
+export function permutations(words: string[]): string[][] {
+  if (words.length === 0) return [[]];
+
+  const first = words[0];
+  const withoutFirst = words.slice(1);
+  const permsWithoutFirst = permutations(withoutFirst);
+  const result: string[][] = [];
+
+  for (let perm of permsWithoutFirst) {
+    for (let i = 0; i <= perm.length; i++) {
+      result.push([...perm.slice(0, i), first, ...perm.slice(i)]);
+    }
+  }
+
+  return result;
+}
