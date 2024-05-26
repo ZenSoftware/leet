aaf90dc5f22aa35d70c9b70c378d5d7c52618d97
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-26 13:16:01 -0700

    binary search example

--- leet/search-insert-position.ts
+++ leet/search-insert-position.ts
@@ -4,3 +4,18 @@
 export function searchInsert(nums: number[], target: number): number {
   return -2;
 }
+
+export function binarySearch(nums: number[], target: number): number {
+  function bs(start: number, end: number): number {
+    if (nums[start] === target) return start;
+    if (nums[end] === target) return end;
+    if (start >= end) return -1;
+
+    const mid = Math.floor((start + end) / 2);
+
+    if (target <= nums[mid]) return bs(start, mid);
+    else return bs(mid + 1, end);
+  }
+
+  return bs(0, nums.length - 1);
+}
