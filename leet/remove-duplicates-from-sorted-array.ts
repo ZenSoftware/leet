c424704e5b27ea308aae2772b41dde9ac1f93c22
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-25 17:28:09 -0700

    removed unecessary nulling

--- leet/remove-duplicates-from-sorted-array.ts
+++ leet/remove-duplicates-from-sorted-array.ts
@@ -25,7 +25,6 @@ export function removeDuplicates(nums: (number | null)[]): number {
   while (j < nums.length) {
     if (nums[j] !== null) {
       nums[i] = nums[j];
-      nums[j] = null;
       i++;
     }
     j++;
