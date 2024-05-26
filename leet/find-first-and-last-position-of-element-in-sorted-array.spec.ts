f9f2fc332d6eaedf06713ec4a2e626c6b66afe85
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-26 12:58:59 -0700

    skeleton

---
+++ leet/find-first-and-last-position-of-element-in-sorted-array.spec.ts
@@ -0,0 +1,7 @@
+import { searchRange } from './find-first-and-last-position-of-element-in-sorted-array';
+
+describe('Find First and Last Position of Element in Sorted Array', () => {
+  it('evaluates correctly', () => {
+    expect(true).toEqual(true);
+  });
+});
---
+++ leet/find-first-and-last-position-of-element-in-sorted-array.ts
@@ -0,0 +1,7 @@
+/**
+ * https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+ */
+
+export function searchRange(nums: number[], target: number): number[] {
+  return [];
+}
