ab0cf564a68de936ab778d0052f44a382dbbbe48
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-27 00:15:48 -0700

    solution

--- leet/first-missing-positive.ts
+++ leet/first-missing-positive.ts
@@ -2,5 +2,24 @@
  * https://leetcode.com/problems/first-missing-positive/
  */
 export function firstMissingPositive(nums: number[]): number {
-  return 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] <= 0) {
+      nums[i] = nums.length + 1;
+    }
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    const absNum = Math.abs(nums[i]);
+    if (absNum <= nums.length && nums[absNum - 1] > 0) {
+      nums[absNum - 1] *= -1;
+    }
+  }
+
+  for (let c = 1; c <= nums.length; c++) {
+    if (nums[c - 1] > 0) {
+      return c;
+    }
+  }
+
+  return nums.length + 1;
 }
