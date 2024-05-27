9d5656f33ebaa278eeabe6cdba96f00723d19732
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-27 14:44:49 -0700

    reanmed

--- leet/jump-game-ii.ts
+++ leet/jump-game-ii.ts
@@ -7,17 +7,17 @@ export function jump(nums: number[]): number {
   while (i < nums.length - 1) {
     let steps = nums[i];
     let candidateMax = 0;
-    let candidateIndex = 0;
+    let bestIndex = 0;
     for (let j = 0; j < steps; j++) {
-      const nextIndex = i + steps - j;
-      const candidateMove = nextIndex + nums[nextIndex];
+      const candidateIndex = i + steps - j;
+      const candidateMove = candidateIndex + nums[candidateIndex];
       if (candidateMove > candidateMax) {
         candidateMax = candidateMove;
-        candidateIndex = nextIndex;
+        bestIndex = candidateIndex;
       }
-      if (candidateIndex >= nums.length - 1) return count + 1;
+      if (bestIndex >= nums.length - 1) return count + 1;
     }
-    i = candidateIndex;
+    i = bestIndex;
     count++;
   }
   return count;
