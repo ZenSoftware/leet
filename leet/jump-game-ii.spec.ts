97afee4469e98414c0f9e5d5c5c38a701b1e1787
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-27 13:25:26 -0700

    added tests

--- leet/jump-game-ii.spec.ts
+++ leet/jump-game-ii.spec.ts
@@ -2,6 +2,7 @@ import { jump } from './jump-game-ii';
 
 describe('Jump Game II', () => {
   it('evaluates correctly', () => {
-    expect(false).toEqual(true);
+    expect(jump([2, 3, 1, 1, 4])).toEqual(2);
+    expect(jump([2, 3, 0, 1, 4])).toEqual(2);
   });
 });
