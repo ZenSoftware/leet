8568b2aad9ab1372cfd69288df13646911abfddd
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-26 21:21:36 -0700

    solution

--- leet/combination-sum.ts
+++ leet/combination-sum.ts
@@ -2,5 +2,23 @@
  * https://leetcode.com/problems/combination-sum/description/
  */
 export function combinationSum(candidates: number[], target: number): number[][] {
-  return [];
+  const result: number[][] = [];
+
+  const bt = (i: number, path: number[], sum: number): void => {
+    if (sum > target) return;
+    if (sum === target) {
+      result.push([...path]);
+      return;
+    }
+
+    for (let j = i; j < candidates.length; j++) {
+      path.push(candidates[j]);
+      bt(j, path, sum + candidates[j]);
+      path.pop();
+    }
+  };
+
+  bt(0, [], 0);
+
+  return result;
 }
