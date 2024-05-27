392d81d8692ac61fa22eceff609d3eb93e49e2d7
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-27 16:31:45 -0700

    rotate 3x3

--- leet/rotate-image.ts
+++ leet/rotate-image.ts
@@ -2,4 +2,14 @@
  * https://leetcode.com/problems/rotate-image/description/
  */
 
-export function rotate(matrix: number[][]): void {}
+export function rotate(matrix: number[][]): void {
+  // Transpose
+  [matrix[0][1], matrix[1][0]] = [matrix[1][0], matrix[0][1]];
+  [matrix[0][2], matrix[2][0]] = [matrix[2][0], matrix[0][2]];
+  [matrix[1][2], matrix[2][1]] = [matrix[2][1], matrix[1][2]];
+
+  // Swap columns
+  [matrix[0][0], matrix[0][2]] = [matrix[0][2], matrix[0][0]];
+  [matrix[1][0], matrix[1][2]] = [matrix[1][2], matrix[1][0]];
+  [matrix[2][0], matrix[2][2]] = [matrix[2][2], matrix[2][0]];
+}
