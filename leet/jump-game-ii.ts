ad042beb45fde06ac01e96fc0a7c8809fdb0d9a8
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-27 14:05:38 -0700

    incorrect greedy solution

--- leet/jump-game-ii.ts
+++ leet/jump-game-ii.ts
@@ -2,5 +2,21 @@
  * https://leetcode.com/problems/jump-game-ii/
  */
 export function jump(nums: number[]): number {
-  return -1;
+  let count = 0;
+  let i = 0;
+
+  while (i < nums.length - 1) {
+    let steps = nums[i];
+
+    for (let j = 0; j < steps; j++) {
+      const forwardIndex = i + steps - j;
+      if (forwardIndex >= nums.length - 1 || nums[forwardIndex] > 0) {
+        i = forwardIndex;
+        count++;
+        break;
+      }
+    }
+  }
+
+  return count;
 }
