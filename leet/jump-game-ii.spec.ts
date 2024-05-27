92ecaa8f95127285cde108f1cee80a2f7aab4d91
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-27 13:24:33 -0700

    skeleton

---
+++ leet/jump-game-ii.spec.ts
@@ -0,0 +1,7 @@
+import { jump } from './jump-game-ii';
+
+describe('Jump Game II', () => {
+  it('evaluates correctly', () => {
+    expect(false).toEqual(true);
+  });
+});
---
+++ leet/jump-game-ii.ts
@@ -0,0 +1,6 @@
+/**
+ * https://leetcode.com/problems/jump-game-ii/
+ */
+export function jump(nums: number[]): number {
+  return -1;
+}
