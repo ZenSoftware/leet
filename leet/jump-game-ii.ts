5b4f159a7b5adc2efd1845f72cc790c58c5a59b3
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-27 14:42:45 -0700

    solution

--- leet/jump-game-ii.ts
+++ leet/jump-game-ii.ts
@@ -4,19 +4,21 @@
 export function jump(nums: number[]): number {
   let count = 0;
   let i = 0;
-
   while (i < nums.length - 1) {
     let steps = nums[i];
-
+    let candidateMax = 0;
+    let candidateIndex = 0;
     for (let j = 0; j < steps; j++) {
       const forwardIndex = i + steps - j;
-      if (forwardIndex >= nums.length - 1 || nums[forwardIndex] > 0) {
-        i = forwardIndex;
-        count++;
-        break;
+      const candidateMove = forwardIndex + nums[forwardIndex];
+      if (candidateMove > candidateMax) {
+        candidateMax = candidateMove;
+        candidateIndex = forwardIndex;
       }
+      if (candidateIndex >= nums.length - 1) return count + 1;
     }
+    i = candidateIndex;
+    count++;
   }
-
   return count;
 }
