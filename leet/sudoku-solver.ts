c4e5581ed17680f105b983efc7fc6a4f5b2dea17
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-26 19:17:56 -0700

    progress

--- leet/sudoku-solver.ts
+++ leet/sudoku-solver.ts
@@ -2,22 +2,23 @@
  * https://leetcode.com/problems/sudoku-solver/description/
  */
 export function solveSudoku(board: string[][]): void {
-  const usedTotals = init(board);
+  const unused2x2 = init(board);
 }
 
-export function init(board: string[][]): number[][] {
+export function init(board: string[][]): (Set<string> | null)[][] {
   // Count the number of used values for each 3x3 subsection's
-  const subSectionTotals: number[][] = new Array(3);
+  const subSectionUsed: Set<string>[][] = Array(3);
   for (let sectionRow = 0; sectionRow <= 2; sectionRow++) {
-    subSectionTotals[sectionRow] = Array(3).fill(0);
+    subSectionUsed[sectionRow] = Array(3);
     for (let sectionCol = 0; sectionCol <= 2; sectionCol++) {
+      subSectionUsed[sectionRow][sectionCol] = new Set();
       const rowOffset = sectionRow * 3;
       for (let row = 0 + rowOffset; row <= 2 + rowOffset; row++) {
         const colOffset = sectionCol * 3;
         for (let col = 0 + colOffset; col <= 2 + colOffset; col++) {
           const val = board[row][col];
           if (val !== '.') {
-            subSectionTotals[sectionRow][sectionCol]++;
+            subSectionUsed[sectionRow][sectionCol].add(val);
           }
         }
       }
@@ -25,9 +26,9 @@ export function init(board: string[][]): number[][] {
   }
 
   // Count the number of used values for each individual cell
-  const individualCellTotals: number[][] = new Array(9);
+  const unused: Set<string>[][] = new Array(9);
   for (let row = 0; row < 9; row++) {
-    individualCellTotals[row] = Array(9).fill(null);
+    unused[row] = Array(9).fill(null);
   }
 
   for (let sectionRow = 0; sectionRow <= 2; sectionRow++) {
@@ -38,17 +39,20 @@ export function init(board: string[][]): number[][] {
         for (let col = 0 + colOffset; col <= 2 + colOffset; col++) {
           const val = board[row][col];
           if (val === '.') {
-            individualCellTotals[row][col] = subSectionTotals[sectionRow][sectionCol];
+            unused[row][col] = new Set(['1', '2', '3', '4', '5', '6', '7', '8', '9']);
+            for (let usedVal of subSectionUsed[sectionRow][sectionCol]) {
+              unused[row][col].delete(usedVal);
+            }
 
             for (let i = 0; i < 9; i++) {
               if (board[i][col] !== '.' && !(rowOffset <= i && i <= rowOffset + 2)) {
-                individualCellTotals[row][col]++;
+                unused[row][col].delete(board[i][col]);
               }
             }
 
             for (let j = 0; j < 9; j++) {
               if (board[row][j] !== '.' && !(colOffset <= j && j <= colOffset + 2)) {
-                individualCellTotals[row][col]++;
+                unused[row][col].delete(board[row][j]);
               }
             }
           }
@@ -57,5 +61,5 @@ export function init(board: string[][]): number[][] {
     }
   }
 
-  return individualCellTotals;
+  return unused;
 }
