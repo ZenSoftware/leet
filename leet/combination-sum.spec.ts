f85c01e5171e8446b784c6154b032fa780cf8174
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-26 20:16:40 -0700

    skeleton

---
+++ leet/combination-sum.spec.ts
@@ -0,0 +1,7 @@
+import { combinationSum } from './combination-sum';
+
+describe('Combination Sum', () => {
+  it('evaluates correctly', () => {
+    expect(true).toEqual(true);
+  });
+});
---
+++ leet/combination-sum.ts
@@ -0,0 +1,6 @@
+/**
+ * https://leetcode.com/problems/combination-sum/description/
+ */
+export function combinationSum(candidates: number[], target: number): number[][] {
+  return [];
+}
