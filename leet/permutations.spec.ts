0449a7e8265a88ad81ba8d80591edb1d3470f5bf
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-27 14:47:41 -0700

    added test

--- leet/permutations.spec.ts
+++ leet/permutations.spec.ts
@@ -2,6 +2,13 @@ import { permute } from './permutations';
 
 describe('Permutations', () => {
   it('evaluates correctly', () => {
-    expect(false).toEqual(true);
+    expect(permute([1, 2, 3])).toEqual([
+      [1, 2, 3],
+      [1, 3, 2],
+      [2, 1, 3],
+      [2, 3, 1],
+      [3, 1, 2],
+      [3, 2, 1],
+    ]);
   });
 });
