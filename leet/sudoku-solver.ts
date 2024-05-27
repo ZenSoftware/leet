e1366ac59ba56934040707eaf7b9c76e27e5efde
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-26 19:32:49 -0700

    progress

--- leet/sudoku-solver.ts
+++ leet/sudoku-solver.ts
@@ -2,11 +2,30 @@
  * https://leetcode.com/problems/sudoku-solver/description/
  */
 export function solveSudoku(board: string[][]): void {
-  const unused2x2 = init(board);
+  const unused2D = getUnused2D(board);
+  const unusedArray: Cell[] = getUnusedArray(unused2D);
+  unusedArray.sort((a, b) => a.unused.size - b.unused.size);
+  console.log(unusedArray[0]);
 }
 
-export function init(board: string[][]): (Set<string> | null)[][] {
-  // Count the number of used values for each 3x3 subsection's
+function getUnusedArray(unused2D: (Set<string> | null)[][]): Cell[] {
+  const unusedArray: Cell[] = [];
+
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+      const unused = unused2D[row][col];
+      if (unused !== null) {
+        const cell: Cell = { row, col, unused };
+        unusedArray.push(cell);
+      }
+    }
+  }
+
+  return unusedArray;
+}
+
+function getUnused2D(board: string[][]): (Set<string> | null)[][] {
+  // Aggregate the used values for each 3x3 subsection
   const subSectionUsed: Set<string>[][] = Array(3);
   for (let sectionRow = 0; sectionRow <= 2; sectionRow++) {
     subSectionUsed[sectionRow] = Array(3);
@@ -25,7 +44,7 @@ export function init(board: string[][]): (Set<string> | null)[][] {
     }
   }
 
-  // Count the number of used values for each individual cell
+  // Compute the unused values for each cell
   const unused: Set<string>[][] = new Array(9);
   for (let row = 0; row < 9; row++) {
     unused[row] = Array(9).fill(null);
@@ -37,22 +56,23 @@ export function init(board: string[][]): (Set<string> | null)[][] {
       for (let row = 0 + rowOffset; row <= 2 + rowOffset; row++) {
         const colOffset = sectionCol * 3;
         for (let col = 0 + colOffset; col <= 2 + colOffset; col++) {
-          const val = board[row][col];
-          if (val === '.') {
+          if (board[row][col] === '.') {
             unused[row][col] = new Set(['1', '2', '3', '4', '5', '6', '7', '8', '9']);
             for (let usedVal of subSectionUsed[sectionRow][sectionCol]) {
               unused[row][col].delete(usedVal);
             }
 
             for (let i = 0; i < 9; i++) {
-              if (board[i][col] !== '.' && !(rowOffset <= i && i <= rowOffset + 2)) {
-                unused[row][col].delete(board[i][col]);
+              const val = board[i][col];
+              if (val !== '.' && !(rowOffset <= i && i <= rowOffset + 2)) {
+                unused[row][col].delete(val);
               }
             }
 
             for (let j = 0; j < 9; j++) {
-              if (board[row][j] !== '.' && !(colOffset <= j && j <= colOffset + 2)) {
-                unused[row][col].delete(board[row][j]);
+              const val = board[row][j];
+              if (val !== '.' && !(colOffset <= j && j <= colOffset + 2)) {
+                unused[row][col].delete(val);
               }
             }
           }
@@ -62,4 +82,10 @@ export function init(board: string[][]): (Set<string> | null)[][] {
   }
 
   return unused;
+}
+
+interface Cell {
+  row: number;
+  col: number;
+  unused: Set<string>;
 }
