2e7760ce81dc2e13df9ed2fb041f894c597454d1
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-27 14:52:17 -0700

    permutations

--- leet/permutations.spec.ts
+++ leet/permutations.spec.ts
@@ -2,6 +2,7 @@ import { permute } from './permutations';
 
 describe('Permutations', () => {
   it('evaluates correctly', () => {
+    const result = permute([1, 2, 3]);
     expect(permute([1, 2, 3])).toEqual([
       [1, 2, 3],
       [1, 3, 2],
--- leet/permutations.ts
+++ leet/permutations.ts
@@ -1,6 +1,19 @@
 /**
- *
+ * https://leetcode.com/problems/permutations/description/
  */
 export function permute(nums: number[]): number[][] {
-  return [];
+  if (nums.length === 0) return [[]];
+
+  const first = nums[0];
+  const withoutFirst = nums.slice(1);
+  const permsWithoutFirst = permute(withoutFirst);
+  const result: number[][] = [];
+
+  for (let perm of permsWithoutFirst) {
+    for (let i = 0; i <= perm.length; i++) {
+      result.push([...perm.slice(0, i), first, ...perm.slice(i)]);
+    }
+  }
+
+  return result;
 }
