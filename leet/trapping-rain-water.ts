4da3cf87c65b12ad83f61fa0424a4a53d694b10f
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-27 04:47:47 -0700

    cleaned up

--- leet/trapping-rain-water.ts
+++ leet/trapping-rain-water.ts
@@ -2,25 +2,26 @@
  * https://leetcode.com/problems/trapping-rain-water/description/
  */
 export function trap(height: number[]): number {
-  let maxL = Array(height.length);
+  const length = height.length;
+  let maxL = Array(length);
   maxL[0] = 0;
-  for (let i = 1; i < height.length; i++) {
+  for (let i = 1; i < length; i++) {
     maxL[i] = Math.max(height[i - 1], maxL[i - 1]);
   }
 
-  let maxR = Array(height.length);
-  maxR[height.length - 1] = 0;
-  for (let i = height.length - 2; i >= 0; i--) {
+  let maxR = Array(length);
+  maxR[length - 1] = 0;
+  for (let i = length - 2; i >= 0; i--) {
     maxR[i] = Math.max(height[i + 1], maxR[i + 1]);
   }
 
-  let min = Array(height.length);
-  for (let i = 0; i < height.length; i++) {
+  let min = Array(length);
+  for (let i = 0; i < length; i++) {
     min[i] = Math.min(maxL[i], maxR[i]);
   }
 
   let result = 0;
-  for (let i = 0; i < height.length; i++) {
+  for (let i = 0; i < length; i++) {
     let volume = min[i] - height[i];
     if (volume > 0) result += volume;
   }
