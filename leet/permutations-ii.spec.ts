999e166b00cd6a37afc36d80d565f7301ebc99c9
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-27 15:00:58 -0700

    standard permute

--- leet/permutations-ii.spec.ts
+++ leet/permutations-ii.spec.ts
@@ -2,6 +2,7 @@ import { permuteUnique } from './permutations-ii';
 
 describe('Permutations II', () => {
   it('evaluates correctly', () => {
+    const result = permuteUnique([1, 1, 2]);
     expect(false).toEqual(true);
   });
 });
--- leet/permutations-ii.ts
+++ leet/permutations-ii.ts
@@ -2,5 +2,23 @@
  * https://leetcode.com/problems/permutations-ii/
  */
 export function permuteUnique(nums: number[]): number[][] {
-  return [];
+  function permute(elements: number[]) {
+    if (elements.length === 0) return [[]];
+
+    const first = elements[0];
+    const withoutFirst = elements.slice(1);
+    const permsWithoutFirst = permute(withoutFirst);
+    const result: number[][] = [];
+
+    for (let perm of permsWithoutFirst) {
+      for (let i = 0; i <= perm.length; i++) {
+        result.push([...perm.slice(0, i), first, ...perm.slice(i)]);
+      }
+    }
+
+    return result;
+  }
+
+  nums.sort((a, b) => a - b);
+  return permute(nums);
 }
