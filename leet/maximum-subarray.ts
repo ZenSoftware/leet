0def63fcd2797f9ac441eef8d1a7c12684c5d9ca
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-27 19:29:52 -0700

    inefficient

--- leet/maximum-subarray.ts
+++ leet/maximum-subarray.ts
@@ -2,5 +2,15 @@
  * https://leetcode.com/problems/maximum-subarray/
  */
 export function maxSubArray(nums: number[]): number {
-  return Infinity;
+  let largest = -Infinity;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i; j < nums.length; j++) {
+      let sum = 0;
+      for (let k = i; k <= j; k++) {
+        sum += nums[k];
+      }
+      if (sum > largest) largest = sum;
+    }
+  }
+  return largest;
 }
