f0313b0d8c5eccf9d2a4816de011c60033f36f60
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-28 16:01:44 -0700

    solution

--- ubc/add-binary.ts
+++ ubc/add-binary.ts
@@ -4,5 +4,32 @@
 export { addBinary };
 
 function addBinary(a: string, b: string): string {
-  return '';
+  let solution = '';
+  let carry = 0;
+  let longestLength = Math.max(a.length, b.length);
+  let aLastIndex = a.length - 1;
+  let bLastIndex = b.length - 1;
+
+  for (let i = 0; i <= longestLength; i++) {
+    let aInt = toInt(a[aLastIndex - i]);
+    let bInt = toInt(b[bLastIndex - i]);
+    let sum = aInt + bInt + carry;
+
+    if (sum > 1) {
+      if (sum === 2) solution = '0' + solution;
+      else solution = '1' + solution;
+      carry = 1;
+    } else {
+      if (sum === 1) solution = '1' + solution;
+      else solution = '0' + solution;
+      carry = 0;
+    }
+  }
+
+  return solution;
+}
+
+function toInt(x: string | undefined): number {
+  if (x === undefined || x === '0') return 0;
+  else return 1;
 }
