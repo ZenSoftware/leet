da376c4622b07c5d9d9447824c616416d3279081
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-28 00:36:23 -0700

    tests

--- leet/rotate-list.spec.ts
+++ leet/rotate-list.spec.ts
@@ -1,8 +1,16 @@
 import { rotateRight, toArray, toList } from './rotate-list';
 
 describe('Rotate List', () => {
-  it('evaluates correctly', () => {
-    expect(false).toEqual(true);
+  it('evaluates correctly 1', () => {
+    const input = toList([1, 2, 3, 4, 5]);
+    const result = rotateRight(input, 2);
+    expect(toArray(result)).toEqual([4, 5, 1, 2, 3]);
+  });
+
+  it('evaluates correctly 2', () => {
+    const input = toList([0, 1, 2]);
+    const result = rotateRight(input, 4);
+    expect(toArray(result)).toEqual([2, 0, 1]);
   });
 
   it('constructs lists correctly', () => {
