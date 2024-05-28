a26299be8e158ed65125722643eaad6fc5c0568f
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-27 22:45:31 -0700

    initialize

--- leet/spiral-matrix-ii.ts
+++ leet/spiral-matrix-ii.ts
@@ -3,5 +3,15 @@
  */
 
 export function generateMatrix(n: number): number[][] {
-  return [];
+  let top = 0;
+  let bottom = n - 1;
+  let left = 0;
+  let right = n - 1;
+
+  let result: number[][] = Array(n);
+  for (let i = 0; i < n; i++) {
+    result[i] = Array(n);
+  }
+
+  return result;
 }
