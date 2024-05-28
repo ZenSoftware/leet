78a7b64570b7c0394bb9574b4b4b38a9c963510b
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-27 18:02:38 -0700

    added tests

--- leet/group-anagrams.spec.ts
+++ leet/group-anagrams.spec.ts
@@ -2,6 +2,12 @@ import { groupAnagrams } from './group-anagrams';
 
 describe('Group Anagrams', () => {
   it('evaluates correctly', () => {
-    expect(true).toEqual(true);
+    expect(groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat'])).toEqual([
+      ['bat'],
+      ['nat', 'tan'],
+      ['ate', 'eat', 'tea'],
+    ]);
+    expect(groupAnagrams([''])).toEqual([['']]);
+    expect(groupAnagrams(['a'])).toEqual([['a']]);
   });
 });
