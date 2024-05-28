66fc89cd27c131c72b99d45273de25ab82b28b8b
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-28 12:21:15 -0700

    formatting

--- no-internet/unique-paths.ts
+++ no-internet/unique-paths.ts
@@ -4,5 +4,15 @@
 export { uniquePaths };
 
 function uniquePaths(m: number, n: number): number {
-  return -1;
+  function dfs(x: number, y: number) {
+    if (x > m || y > n) return 0;
+    if (x === m && y === n) return 1;
+
+    let total = 0;
+    total += dfs(x + 1, y);
+    total += dfs(x, y + 1);
+    return total;
+  }
+
+  return dfs(1, 1);
 }
