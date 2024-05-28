f9d14749700d2c0cc44992e5133c1116caa35d44
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-27 19:43:29 -0700

    added tests

--- leet/spiral-matrix.spec.ts
+++ leet/spiral-matrix.spec.ts
@@ -2,6 +2,20 @@ import { spiralOrder } from './spiral-matrix';
 
 describe('Spiral Matrix', () => {
   it('evaluates correctly', () => {
-    expect(true).toEqual(true);
+    expect(
+      spiralOrder([
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+      ])
+    ).toEqual([1, 2, 3, 6, 9, 8, 7, 4, 5]);
+
+    expect(
+      spiralOrder([
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+      ])
+    ).toEqual([1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]);
   });
 });
