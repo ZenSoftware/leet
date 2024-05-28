e469f623cd5461ab2e6f44f0f6d197b3d3383edd
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-28 13:01:03 -0700

    solution

--- no-internet/unique-paths-ii.ts
+++ no-internet/unique-paths-ii.ts
@@ -4,7 +4,12 @@
 export { uniquePathsWithObstacles };
 
 function uniquePathsWithObstacles(obstacleGrid: number[][]): number {
+  const memo: Record<string, number> = {};
+
   function dfs(x: number, y: number): number {
+    let key = `${x},${y}`;
+    if (key in memo) return memo[key];
+
     const lastX = obstacleGrid.length - 1;
     const lastY = obstacleGrid[0].length - 1;
     if (x > lastX || y > lastY) return 0;
@@ -14,6 +19,8 @@ function uniquePathsWithObstacles(obstacleGrid: number[][]): number {
     let sum = 0;
     sum += dfs(x + 1, y);
     sum += dfs(x, y + 1);
+
+    memo[key] = sum;
     return sum;
   }
 
