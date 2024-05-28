24d01148ba2d9f3beb8d96f74234e4ff56cb0a46
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-27 22:17:48 -0700

    solution

--- leet/merge-intervals.spec.ts
+++ leet/merge-intervals.spec.ts
@@ -1,4 +1,4 @@
-import { merge } from './merge-intervals';
+import { merge, combineTwo } from './merge-intervals';
 
 describe('Merge Intervals', () => {
   it('evaluates correctly 1', () => {
@@ -23,5 +23,21 @@ describe('Merge Intervals', () => {
         [4, 5],
       ])
     ).toEqual([[1, 5]]);
+  });
+
+  it('evaluates correctly 3', () => {
+    expect(
+      merge([
+        [1, 4],
+        [2, 3],
+      ])
+    ).toEqual([[1, 4]]);
+  });
+
+  it('evaluates combine two correctly', () => {
+    expect(combineTwo([1, 4], [3, 5])).toEqual([1, 5]);
+    expect(combineTwo([1, 4], [4, 5])).toEqual([1, 5]);
+    expect(combineTwo([1, 2], [4, 5])).toEqual(null);
+    expect(combineTwo([1, 4], [2, 3])).toEqual([1, 4]);
   });
 });
--- leet/merge-intervals.ts
+++ leet/merge-intervals.ts
@@ -2,5 +2,35 @@
  * https://leetcode.com/problems/merge-intervals/
  */
 export function merge(intervals: number[][]): number[][] {
-  return [];
+  intervals.sort((a, b) => {
+    const first = a[0] - b[0];
+    const second = a[1] - b[1];
+    return first || second;
+  });
+
+  const result: number[][] = [];
+
+  let accum = intervals[0];
+  for (let i = 1; i < intervals.length; i++) {
+    let combined = combineTwo(accum, intervals[i]);
+    if (combined !== null) {
+      accum = combined;
+    } else {
+      result.push(accum);
+      accum = intervals[i];
+    }
+  }
+  result.push(accum);
+
+  return result;
+}
+
+export function combineTwo(x: number[], y: number[]): number[] | null {
+  if (y[0] <= x[1]) {
+    const start = Math.min(x[0], y[0]);
+    const end = Math.max(x[1], y[1]);
+    return [start, end];
+  }
+
+  return null;
 }
