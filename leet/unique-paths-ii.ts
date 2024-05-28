acece8b83a8bfdd25557bc94dfcf1c340094cd03
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-28 13:06:54 -0700

    optimization

--- leet/unique-paths-ii.ts
+++ leet/unique-paths-ii.ts
@@ -7,14 +7,13 @@ function uniquePathsWithObstacles(obstacleGrid: number[][]): number {
   const memo: Record<string, number> = {};
 
   function dfs(x: number, y: number): number {
-    let key = `${x},${y}`;
-    if (key in memo) return memo[key];
-
     const lastX = obstacleGrid.length - 1;
     const lastY = obstacleGrid[0].length - 1;
     if (x > lastX || y > lastY) return 0;
     if (obstacleGrid[x][y] === 1) return 0;
     if (x === lastX && y === lastY) return 1;
+    let key = `${x},${y}`;
+    if (key in memo) return memo[key];
 
     let sum = 0;
     sum += dfs(x + 1, y);
