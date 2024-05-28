1e81e3d21609d33dd6865fdf60747cab89c85e0a
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-27 22:39:08 -0700

    solution

--- leet/length-of-last-word.spec.ts
+++ leet/length-of-last-word.spec.ts
@@ -5,5 +5,6 @@ describe('Length of Last Word', () => {
     expect(lengthOfLastWord('Hello World')).toEqual(5);
     expect(lengthOfLastWord('   fly me   to   the moon  ')).toEqual(4);
     expect(lengthOfLastWord('luffy is still joyboy')).toEqual(6);
+    expect(lengthOfLastWord('luffyisstilljoyboy')).toEqual(18);
   });
 });
--- leet/length-of-last-word.ts
+++ leet/length-of-last-word.ts
@@ -2,5 +2,13 @@
  * https://leetcode.com/problems/length-of-last-word/
  */
 export function lengthOfLastWord(s: string): number {
-  return -1;
+  const trimmed = s.trimEnd();
+
+  for (let i = trimmed.length - 1; i >= 0; i--) {
+    if (trimmed[i] === ' ') {
+      return trimmed.length - i - 1;
+    }
+  }
+
+  return trimmed.length;
 }
