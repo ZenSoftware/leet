ac411326d5649b2b2c7f6ee497120e4949b84340
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-27 18:13:30 -0700

    solution

--- leet/group-anagrams.ts
+++ leet/group-anagrams.ts
@@ -2,5 +2,11 @@
  * https://leetcode.com/problems/group-anagrams/
  */
 export function groupAnagrams(strs: string[]): string[][] {
-  return [];
+  const grouped: Record<string, string[]> = {};
+  for (let s of strs) {
+    const sorted = s.split('').sort().join();
+    if (!grouped[sorted]) grouped[sorted] = [];
+    grouped[sorted].push(s);
+  }
+  return Object.values(grouped);
 }
