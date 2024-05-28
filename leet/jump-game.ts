0ff662d0572b80dd27698d58072fb51fecc8df9a
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-27 21:05:00 -0700

    solution

--- leet/jump-game.ts
+++ leet/jump-game.ts
@@ -3,5 +3,18 @@
  */
 
 export function canJump(nums: number[]): boolean {
-  return true;
+  function search(position: number): boolean {
+    if (position >= nums.length - 1) return true;
+    if (nums[position] === 0) return false;
+
+    const steps = nums[position];
+    for (let i = 1; i <= steps; i++) {
+      const done = search(position + i);
+      if (done) return true;
+    }
+
+    return false;
+  }
+
+  return search(0);
 }
