4212b6ac7400cdaa0652e7d4caba763b2c821c81
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-28 13:04:42 -0700

    memoized

--- leet/unique-paths.ts
+++ leet/unique-paths.ts
@@ -4,13 +4,18 @@
 export { uniquePaths };
 
 function uniquePaths(m: number, n: number): number {
+  const memo: Record<string, number> = {};
+
   function dfs(x: number, y: number) {
     if (x > m || y > n) return 0;
     if (x === m && y === n) return 1;
+    const key = `${x},${y}`;
+    if (key in memo) return memo[key];
 
     let total = 0;
     total += dfs(x + 1, y);
     total += dfs(x, y + 1);
+    memo[key] = total;
     return total;
   }
 
