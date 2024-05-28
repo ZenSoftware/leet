06e1039cd56ad668c0a495aa80cfa685db7e8a33
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-28 15:18:54 -0700

    solution

--- ubc/plus-one.ts
+++ ubc/plus-one.ts
@@ -4,5 +4,25 @@
 export { plusOne };
 
 function plusOne(digits: number[]): number[] {
-  return [];
+  const solution = Array(digits.length);
+  let carry = 0;
+
+  const lastIndex = digits.length - 1;
+  for (let i = lastIndex; i >= 0; i--) {
+    let sum = digits[i] + carry;
+
+    if (i === lastIndex) sum += 1;
+
+    if (sum >= 10) {
+      solution[i] = sum - 10;
+      carry = 1;
+    } else {
+      solution[i] = sum;
+      carry = 0;
+    }
+  }
+
+  if (carry > 0) solution.unshift(1);
+
+  return solution;
 }
