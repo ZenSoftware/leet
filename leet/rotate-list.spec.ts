9ea27e64a619ef1ffd0d6dd4b36d417b9a77bbba
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-28 00:34:17 -0700

    utilities

--- leet/rotate-list.spec.ts
+++ leet/rotate-list.spec.ts
@@ -1,7 +1,13 @@
-import { rotateRight } from './rotate-list';
+import { rotateRight, toArray, toList } from './rotate-list';
 
 describe('Rotate List', () => {
   it('evaluates correctly', () => {
     expect(false).toEqual(true);
+  });
+
+  it('constructs lists correctly', () => {
+    const list = toList([1, 2, 3, 4]);
+    const array = toArray(list);
+    expect(array).toEqual([1, 2, 3, 4]);
   });
 });
--- leet/rotate-list.ts
+++ leet/rotate-list.ts
@@ -13,3 +13,26 @@ class ListNode {
     this.next = next === undefined ? null : next;
   }
 }
+
+export function toList(elements: number[]): ListNode | null {
+  if (elements.length === 0) return null;
+
+  let pointer: ListNode | null = null;
+  for (let i = elements.length - 1; i >= 0; i--) {
+    const node = new ListNode(elements[i]);
+    node.next = pointer;
+    pointer = node;
+  }
+  return pointer;
+}
+
+export function toArray(list: ListNode | null): number[] {
+  if (!list) return [];
+
+  const result: number[] = [];
+  while (list) {
+    result.push(list.val);
+    list = list.next;
+  }
+  return result;
+}
