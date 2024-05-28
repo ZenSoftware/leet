06ae9c1cfeb70b20ab66acabc0f2c49e0a94a62e
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-27 18:01:42 -0700

    skeleton

---
+++ leet/group-anagrams.spec.ts
@@ -0,0 +1,7 @@
+import { groupAnagrams } from './group-anagrams';
+
+describe('Group Anagrams', () => {
+  it('evaluates correctly', () => {
+    expect(true).toEqual(true);
+  });
+});
---
+++ leet/group-anagrams.ts
@@ -0,0 +1,6 @@
+/**
+ * https://leetcode.com/problems/group-anagrams/
+ */
+export function groupAnagrams(strs: string[]): string[][] {
+  return [];
+}
