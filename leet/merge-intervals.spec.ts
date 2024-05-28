90e70cb5abcbb587f83f664980139ce263ff737e
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-27 21:36:13 -0700

    added test

--- leet/merge-intervals.spec.ts
+++ leet/merge-intervals.spec.ts
@@ -1,7 +1,27 @@
 import { merge } from './merge-intervals';
 
 describe('Merge Intervals', () => {
-  it('evaluates correctly', () => {
-    expect(true).toEqual(true);
+  it('evaluates correctly 1', () => {
+    expect(
+      merge([
+        [1, 3],
+        [2, 6],
+        [8, 10],
+        [15, 18],
+      ])
+    ).toEqual([
+      [1, 6],
+      [8, 10],
+      [15, 18],
+    ]);
+  });
+
+  it('evaluates correctly 2', () => {
+    expect(
+      merge([
+        [1, 4],
+        [4, 5],
+      ])
+    ).toEqual([[1, 5]]);
   });
 });
