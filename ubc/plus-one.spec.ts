75ddbd6287da79618ab583c75583c894fb727223
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-28 15:01:28 -0700

    skeleton

---
+++ ubc/plus-one.spec.ts
@@ -0,0 +1,9 @@
+import { plusOne } from './plus-one';
+
+describe('Plus One', () => {
+  it('evaluates correctly', () => {
+    expect(plusOne([1, 2, 3])).toEqual([1, 2, 4]);
+    expect(plusOne([4, 3, 2, 1])).toEqual([4, 3, 2, 2]);
+    expect(plusOne([9])).toEqual([1, 0]);
+  });
+});
---
+++ ubc/plus-one.ts
@@ -0,0 +1,8 @@
+/**
+ * https://leetcode.com/problems/plus-one/
+ */
+export { plusOne };
+
+function plusOne(digits: number[]): number[] {
+  return [];
+}
