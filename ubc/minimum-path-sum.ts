70bc5bd936a451dbbb15d39886ae92c3a7498781
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-28 14:17:04 -0700

    slow solution

--- ubc/minimum-path-sum.ts
+++ ubc/minimum-path-sum.ts
@@ -4,5 +4,20 @@
 export { minPathSum };
 
 function minPathSum(grid: number[][]): number {
-  return -1;
+  let minimum = Infinity;
+  const lastX = grid.length - 1;
+  const lastY = grid[0].length - 1;
+
+  function dfs(x: number, y: number, sum: number) {
+    if (x === lastX && y === lastY) {
+      minimum = Math.min(minimum, sum + grid[x][y]);
+      return;
+    }
+
+    if (x + 1 <= lastX) dfs(x + 1, y, sum + grid[x][y]);
+    if (y + 1 <= lastY) dfs(x, y + 1, sum + grid[x][y]);
+  }
+
+  dfs(0, 0, 0);
+  return minimum;
 }
