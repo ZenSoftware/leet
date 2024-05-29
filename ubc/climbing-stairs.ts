9db1087c8eb5e4dee9028c5009c1679942a6c354
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-28 18:21:49 -0700

    brute force solution

--- ubc/climbing-stairs.ts
+++ ubc/climbing-stairs.ts
@@ -4,5 +4,18 @@
 export { climbStairs };
 
 function climbStairs(n: number): number {
-  return -1;
+  let result = 0;
+
+  function dfs(current: number) {
+    if (current > n) return;
+    if (current === n) {
+      result++;
+      return;
+    }
+    dfs(current + 1);
+    dfs(current + 2);
+  }
+
+  dfs(0);
+  return result;
 }
