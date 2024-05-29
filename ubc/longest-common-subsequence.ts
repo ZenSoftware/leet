91fd0ffa9e8fba549f010bb576ced68afd01d579
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-28 20:01:03 -0700

    skeleton

---
+++ ubc/longest-common-subsequence.ts
@@ -0,0 +1,8 @@
+/**
+ * https://leetcode.com/problems/longest-common-subsequence/
+ */
+export { longestCommonSubsequence };
+
+function longestCommonSubsequence(text1: string, text2: string): number {
+  return -1;
+}
