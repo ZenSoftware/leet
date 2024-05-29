7a142303c02b800024b2cfd5132b029cf1f9e72f
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-28 18:24:40 -0700

    added tests

--- ubc/climbing-stairs.spec.ts
+++ ubc/climbing-stairs.spec.ts
@@ -4,5 +4,6 @@ describe('Climbing Stairs', () => {
   it('evaluates correctly', () => {
     expect(climbStairs(2)).toEqual(2);
     expect(climbStairs(3)).toEqual(3);
+    expect(climbStairs(44)).toEqual(1134903170);
   });
 });
