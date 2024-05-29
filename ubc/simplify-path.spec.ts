1429a5ef82c372dd39397109d5844bdaba7c90f6
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-28 19:52:48 -0700

    progress

--- ubc/simplify-path.spec.ts
+++ ubc/simplify-path.spec.ts
@@ -1,7 +1,19 @@
-import { simplifyPath } from './simplify-path';
+import { simplifyPath, removeExtraSlashes, removeUpLevels } from './simplify-path';
 
 describe('Simplify Path', () => {
-  it('evaluates correctly', () => {
-    expect(true).toEqual(true);
+  it('evaluates simplifyPath correctly', () => {
+    expect(simplifyPath('/.//a///b//././')).toEqual('/a/b');
+    expect(simplifyPath('/.//a///b//././.../')).toEqual('/a/b/...');
+    expect(simplifyPath('/.//a///b//././.../.')).toEqual('/a/b/...');
+    expect(simplifyPath('/.//a///b.//././.../.')).toEqual('/a/b./...');
+  });
+
+  it('evaluates removeExtraSlashes correctly', () => {
+    expect(removeExtraSlashes('///a///b////')).toEqual('/a/b');
+    expect(removeExtraSlashes('/')).toEqual('/');
+  });
+
+  it('evaluates removeUpLevels correctly', () => {
+    expect(removeUpLevels('../../a')).toEqual('/a');
   });
 });
--- ubc/simplify-path.ts
+++ ubc/simplify-path.ts
@@ -1,8 +1,55 @@
 /**
  * https://leetcode.com/problems/simplify-path/
  */
-export { simplifyPath };
+export { simplifyPath, removeExtraSlashes, removeUpLevels };
 
 function simplifyPath(path: string): string {
-  return '';
+  path = removeCurrentDirectory(path);
+  path = removeExtraSlashes(path);
+  path = removeUpLevels(path);
+
+  return path;
+}
+
+function removeExtraSlashes(path: string): string {
+  if (path.length < 2) return path;
+
+  let cleaned = '';
+  for (let i = 0; i < path.length; i++) {
+    if (path[i] === '/' && path[i + 1] === '/') continue;
+    cleaned += path[i];
+  }
+
+  if (cleaned[cleaned.length - 1] === '/') {
+    return cleaned.substring(0, cleaned.length - 1);
+  }
+
+  return cleaned;
+}
+
+function removeCurrentDirectory(path: string): string {
+  while (path.includes('/./')) {
+    path = path.replaceAll('/./', '/');
+  }
+  if (path.endsWith('/.')) {
+    return path.substring(0, path.length - 2);
+  }
+  return path;
+}
+
+function removeUpLevels(path: string): string {
+  let splitPath = path.split('/');
+
+  // remove leading '..'
+  let i = 0;
+  while (splitPath[i] === '..') {
+    i++;
+  }
+  splitPath = splitPath.slice(i);
+
+  // Add back leading  /
+  path = splitPath.join('/');
+  if (path[0] !== '/') path = '/' + path;
+
+  return path;
 }
