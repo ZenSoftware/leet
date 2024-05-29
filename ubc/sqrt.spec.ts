1320346b6cda46489db774daa1fd4f31c66a41d9
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-28 18:14:02 -0700

    solution

--- ubc/sqrt.spec.ts
+++ ubc/sqrt.spec.ts
@@ -4,5 +4,6 @@ describe('Sqrt(x)', () => {
   it('evaluates correctly', () => {
     expect(mySqrt(4)).toEqual(2);
     expect(mySqrt(8)).toEqual(2);
+    expect(mySqrt(1)).toEqual(1);
   });
 });
--- ubc/sqrt.ts
+++ ubc/sqrt.ts
@@ -4,5 +4,13 @@
 export { mySqrt };
 
 function mySqrt(x: number): number {
-  return -1;
+  if (x === 1) return 1;
+
+  let i = 1;
+
+  for (; i < x; i++) {
+    if (i * i > x) break;
+  }
+
+  return i - 1;
 }
