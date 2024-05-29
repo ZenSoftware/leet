a70f7cd85a4331254586932c9329083cfd2699ae
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-29 06:48:45 -0700

    renamed

--- solved/word-search.ts
+++ solved/word-search.ts
@@ -18,32 +18,32 @@ function exist(board: string[][], word: string): boolean {
     const rightKey = `${x + 1},${y}`;
     if (!visited.has(rightKey)) {
       visited.add(rightKey);
-      const right = dfs(x + 1, y, next, visited);
-      if (right) return true;
+      const rightResult = dfs(x + 1, y, next, visited);
+      if (rightResult) return true;
       visited.delete(rightKey);
     }
 
     const leftKey = `${x - 1},${y}`;
     if (!visited.has(leftKey)) {
       visited.add(leftKey);
-      const left = dfs(x - 1, y, next, visited);
-      if (left) return true;
+      const leftResult = dfs(x - 1, y, next, visited);
+      if (leftResult) return true;
       visited.delete(leftKey);
     }
 
     const upKey = `${x},${y + 1}`;
     if (!visited.has(upKey)) {
       visited.add(upKey);
-      const up = dfs(x, y + 1, next, visited);
-      if (up) return true;
+      const upResult = dfs(x, y + 1, next, visited);
+      if (upResult) return true;
       visited.delete(upKey);
     }
 
     const downKey = `${x},${y - 1}`;
     if (!visited.has(downKey)) {
       visited.add(downKey);
-      const down = dfs(x, y - 1, next, visited);
-      if (down) return true;
+      const downResult = dfs(x, y - 1, next, visited);
+      if (downResult) return true;
       visited.delete(downKey);
     }
 
