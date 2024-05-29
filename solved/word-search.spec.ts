b226d563e961a745cbbcdfa469f35ddc7f4881b5
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-29 05:32:52 -0700

    skeleton

---
+++ solved/word-search.spec.ts
@@ -0,0 +1,7 @@
+import { exist } from './word-search';
+
+describe('Word Search', () => {
+  it('evaluates correctly', () => {
+    expect(false).toEqual(true);
+  });
+});
---
+++ solved/word-search.ts
@@ -0,0 +1,8 @@
+/**
+ * https://leetcode.com/problems/word-search/
+ */
+export { exist };
+
+function exist(board: string[][], word: string): boolean {
+  return false;
+}
