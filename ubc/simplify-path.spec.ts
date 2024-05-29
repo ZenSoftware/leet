f5f9124eefb0777b1e26e6e9ed614df1d5b1502d
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-28 18:46:38 -0700

    skeleton

---
+++ ubc/simplify-path.spec.ts
@@ -0,0 +1,7 @@
+import { simplifyPath } from './simplify-path';
+
+describe('Simplify Path', () => {
+  it('evaluates correctly', () => {
+    expect(true).toEqual(true);
+  });
+});
---
+++ ubc/simplify-path.ts
@@ -0,0 +1,8 @@
+/**
+ * https://leetcode.com/problems/simplify-path/
+ */
+export { simplifyPath };
+
+function simplifyPath(path: string): string {
+  return '';
+}
