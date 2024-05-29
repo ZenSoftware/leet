1db2c573b69272adb44bb0e63ad754054dc1e426
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-29 05:34:49 -0700

    added tests

--- solved/word-search.spec.ts
+++ solved/word-search.spec.ts
@@ -1,7 +1,42 @@
 import { exist } from './word-search';
 
 describe('Word Search', () => {
-  it('evaluates correctly', () => {
-    expect(false).toEqual(true);
+  it('evaluates correctly 1', () => {
+    expect(
+      exist(
+        [
+          ['A', 'B', 'C', 'E'],
+          ['S', 'F', 'C', 'S'],
+          ['A', 'D', 'E', 'E'],
+        ],
+        'ABCCED'
+      )
+    ).toEqual(true);
+  });
+
+  it('evaluates correctly 2', () => {
+    expect(
+      exist(
+        [
+          ['A', 'B', 'C', 'E'],
+          ['S', 'F', 'C', 'S'],
+          ['A', 'D', 'E', 'E'],
+        ],
+        'SEE'
+      )
+    ).toEqual(true);
+  });
+
+  it('evaluates correctly 3', () => {
+    expect(
+      exist(
+        [
+          ['A', 'B', 'C', 'E'],
+          ['S', 'F', 'C', 'S'],
+          ['A', 'D', 'E', 'E'],
+        ],
+        'ABCB'
+      )
+    ).toEqual(false);
   });
 });
