1b8e0399b86fed8e1808aa076b2334beb2541e7b
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-29 22:49:05 -0700

    solution

--- today/merge-sorted-array.ts
+++ today/merge-sorted-array.ts
@@ -4,5 +4,35 @@
 export { merge };
 
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-  //
+  const solution: number[] = Array(m + n);
+
+  let iS = 0;
+  let i1 = 0;
+  let i2 = 0;
+  while (i1 < m && i2 < n) {
+    if (nums1[i1] < nums2[i2]) {
+      solution[iS] = nums1[i1];
+      i1++;
+    } else {
+      solution[iS] = nums2[i2];
+      i2++;
+    }
+    iS++;
+  }
+
+  while (i1 < m) {
+    solution[iS] = nums1[i1];
+    iS++;
+    i1++;
+  }
+
+  while (i2 < n) {
+    solution[iS] = nums2[i2];
+    iS++;
+    i2++;
+  }
+
+  for (let i = 0; i < m + n; i++) {
+    nums1[i] = solution[i];
+  }
 }
