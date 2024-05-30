e65956c4c4ce166c1a93edf515e85fd5c5a818b0
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-29 20:22:40 -0700

    skeleton

---
+++ today/search-in-rotated-sorted-array-ii.ts
@@ -0,0 +1,8 @@
+/**
+ * https://leetcode.com/problems/search-in-rotated-sorted-array-ii/description/
+ */
+export { search };
+
+function search(nums: number[], target: number): boolean {
+  return false;
+}
