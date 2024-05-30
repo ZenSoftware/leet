102a02172b7c458930ae335d866e9ce0149afe3e
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-30 02:37:40 -0700

    solution

--- today/subsets-ii.spec.ts
+++ today/subsets-ii.spec.ts
@@ -2,6 +2,6 @@ import { subsetsWithDup } from './subsets-ii';
 
 describe('Subsets II', () => {
   it('evaluates correctly', () => {
-    expect(true).toEqual(true);
+    expect(subsetsWithDup([1, 2, 2])).toEqual([[], [1], [1, 2], [1, 2, 2], [2], [2, 2]]);
   });
 });
