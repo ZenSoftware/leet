ede81e8b86e6f9f8ecd365e86787fecf05339c4d
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-30 16:06:25 -0700

    example permutations

--- today/unique-binary-search-trees-ii.ts
+++ today/unique-binary-search-trees-ii.ts
@@ -17,3 +17,29 @@ class TreeNode {
     this.right = right === undefined ? null : right;
   }
 }
+
+// function permutations(n: number): number[][] {
+//   function perms(remaining: number[]): number[][] {
+//     if (remaining.length === 0) return [[]];
+
+//     const firstEl = remaining[0];
+//     const withoutFirst = remaining.slice(1);
+//     const permsWithoutFirst = perms(withoutFirst);
+//     const permsWithFirst: number[][] = [];
+
+//     for (let perm of permsWithoutFirst) {
+//       for (let i = 0; i <= perm.length; i++) {
+//         permsWithFirst.push([...perm.slice(0, i), firstEl, ...perm.slice(i)]);
+//       }
+//     }
+
+//     return permsWithFirst;
+//   }
+
+//   const elements: number[] = Array(n);
+//   for (let i = 1; i <= n; i++) {
+//     elements[i - 1] = i;
+//   }
+
+//   return perms(elements);
+// }
