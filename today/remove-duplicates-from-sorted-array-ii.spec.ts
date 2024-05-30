896f2a1ff104b32fe84ca8a614c0625f70c7f6d8
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-29 20:16:52 -0700

    solution

--- today/remove-duplicates-from-sorted-array-ii.spec.ts
+++ today/remove-duplicates-from-sorted-array-ii.spec.ts
@@ -12,7 +12,7 @@ describe('Remove Duplicates from Sorted Array II', () => {
   });
 
   it('evaluates correctly 2', () => {
-    const input = [0, 0, 1, 1, 1, 1, 2, 3, 3];
+    const input = [0, 0, 1, 1, 1, 1, 2, 3, 3, 3];
     expect(removeDuplicates(input)).toEqual(7);
     expect(input[0]).toEqual(0);
     expect(input[1]).toEqual(0);
--- today/remove-duplicates-from-sorted-array-ii.ts
+++ today/remove-duplicates-from-sorted-array-ii.ts
@@ -4,15 +4,39 @@
 export { removeDuplicates };
 
 function removeDuplicates(nums: number[]): number {
-  if (nums.length === 0) return 1;
+  if (nums.length === 1) return 1;
 
-  let duplicateCount = 0;
+  let lastUnique = nums[0];
+  let duplicateCount = 1;
   for (let i = 1; i < nums.length; i++) {
-    if (nums[i - 1] === nums[i]) duplicateCount++;
-    if (duplicateCount === 2) {
-      console.log();
+    if (lastUnique === nums[i]) {
+      duplicateCount++;
+    } else {
+      duplicateCount = 1;
+      lastUnique = nums[i];
+    }
+
+    if (duplicateCount > 2) {
+      nums[i] = null as any;
     }
   }
 
-  return -1;
+  let i = 0;
+  while (i < nums.length) {
+    if (nums[i] === null) {
+      break;
+    }
+    i++;
+  }
+
+  let j = i + 1;
+  while (j < nums.length) {
+    if (nums[j] !== null) {
+      nums[i] = nums[j];
+      i++;
+    }
+    j++;
+  }
+
+  return i;
 }
