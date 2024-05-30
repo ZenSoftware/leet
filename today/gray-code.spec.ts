3926998a240d3a24dad8ced8261d9b8f3c23aade
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-29 23:23:50 -0700

    solution - slow

--- today/gray-code.spec.ts
+++ today/gray-code.spec.ts
@@ -1,7 +1,7 @@
 import { grayCode } from './gray-code';
 
 describe('Gray Code', () => {
-  it('evaluates correctly', () => {
+  it('evaluates correctly 1', () => {
     expect(grayCode(2)).toEqual([0, 1, 3, 2]);
   });
 });
--- today/gray-code.ts
+++ today/gray-code.ts
@@ -8,7 +8,18 @@ function grayCode(n: number): number[] {
   const result: number[] = [];
 
   for (let i = 0; i <= lastIndex; i++) {
-    //
+    let binary = i.toString(2);
+    binary = binary.padStart(n, '0');
+    let gray = binary[0];
+    for (let j = 0; j < n - 1; j++) {
+      if (binary[j] === binary[j + 1]) {
+        gray += '0';
+      } else {
+        gray += '1';
+      }
+    }
+    const decimal = Number.parseInt(gray, 2);
+    result.push(decimal);
   }
 
   return result;
