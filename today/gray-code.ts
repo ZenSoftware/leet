2842dc251306cf0c141d0226c21ad84f153c78b2
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-29 23:54:29 -0700

    binary solution

--- today/gray-code.ts
+++ today/gray-code.ts
@@ -4,23 +4,36 @@
 export { grayCode };
 
 function grayCode(n: number): number[] {
-  const lastIndex = 2 ** n - 1;
-  const result: number[] = [];
-
-  for (let i = 0; i <= lastIndex; i++) {
-    let binary = i.toString(2);
-    binary = binary.padStart(n, '0');
-    let gray = binary[0];
-    for (let j = 0; j < n - 1; j++) {
-      if (binary[j] === binary[j + 1]) {
-        gray += '0';
-      } else {
-        gray += '1';
-      }
-    }
-    const decimal = Number.parseInt(gray, 2);
-    result.push(decimal);
+  const size = 1 << n;
+  let result: number[] = Array(n);
+  for (let i = 0; i < size; i++) {
+    result[i] = i ^ (i >> 1);
   }
-
   return result;
 }
+
+/**
+ * String version
+ */
+
+// function grayCode(n: number): number[] {
+//   const lastIndex = 2 ** n - 1;
+//   const result: number[] = [];
+
+//   for (let i = 0; i <= lastIndex; i++) {
+//     let binary = i.toString(2);
+//     binary = binary.padStart(n, '0');
+//     let gray = binary[0];
+//     for (let j = 0; j < n - 1; j++) {
+//       if (binary[j] === binary[j + 1]) {
+//         gray += '0';
+//       } else {
+//         gray += '1';
+//       }
+//     }
+//     const decimal = Number.parseInt(gray, 2);
+//     result.push(decimal);
+//   }
+
+//   return result;
+// }
