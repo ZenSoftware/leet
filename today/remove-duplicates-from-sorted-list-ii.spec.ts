a27fc24ee882f4558895487a27084ba40665da05
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-29 21:46:32 -0700

    utilities

--- today/remove-duplicates-from-sorted-list-ii.spec.ts
+++ today/remove-duplicates-from-sorted-list-ii.spec.ts
@@ -1,7 +1,13 @@
-import { deleteDuplicates } from './remove-duplicates-from-sorted-list-ii';
+import { deleteDuplicates, toList, toArray } from './remove-duplicates-from-sorted-list-ii';
 
 describe('Remove Duplicates from Sorted List II', () => {
   it('evaluates correctly', () => {
     expect(true).toEqual(true);
+  });
+
+  it('constructs lists correctly', () => {
+    const list = toList([1, 2, 3, 4]);
+    const array = toArray(list);
+    expect(array).toEqual([1, 2, 3, 4]);
   });
 });
--- today/remove-duplicates-from-sorted-list-ii.ts
+++ today/remove-duplicates-from-sorted-list-ii.ts
@@ -1,10 +1,31 @@
 /**
  * https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
  */
-export { deleteDuplicates };
+export { deleteDuplicates, toList, toArray };
 
 function deleteDuplicates(head: ListNode | null): ListNode | null {
   return null;
+}
+
+function toList(elements: number[]): ListNode | null {
+  if (elements.length === 0) return null;
+  let pointer: ListNode | null = null;
+  for (let i = elements.length - 1; i >= 0; i--) {
+    const node = new ListNode(elements[i]);
+    node.next = pointer;
+    pointer = node;
+  }
+  return pointer;
+}
+
+function toArray(list: ListNode | null): number[] {
+  if (!list) return [];
+  const result: number[] = [];
+  while (list) {
+    result.push(list.val);
+    list = list.next;
+  }
+  return result;
 }
 
 class ListNode {
