3538ed57ce6f6d8260459ccb1676ea68097303ed
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-30 14:59:34 -0700

    added tests

--- today/reverse-linked-list-ii.spec.ts
+++ today/reverse-linked-list-ii.spec.ts
@@ -1,8 +1,14 @@
 import { reverseBetween, toArray, toList } from './reverse-linked-list-ii';
 
 describe('Reverse Linked List II', () => {
-  it('evaluates correctly', () => {
-    expect(true).toEqual(true);
+  it('evaluates correctly 1', () => {
+    const input = toList([1, 2, 3, 4, 5]);
+    expect(reverseBetween(input, 2, 4)).toEqual([1, 4, 3, 2, 5]);
+  });
+
+  it('evaluates correctly 2', () => {
+    const input = toList([5]);
+    expect(reverseBetween(input, 1, 1)).toEqual([5]);
   });
 
   it('constructs lists correctly', () => {
