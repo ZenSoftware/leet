047995cdd25ea4385f82229c74f8c0c27fbdfffa
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-30 15:48:23 -0700

    skeleton

---
+++ today/binary-tree-inorder-traversal.ts
@@ -0,0 +1,19 @@
+/**
+ * https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+ */
+export { inorderTraversal };
+
+function inorderTraversal(root: TreeNode | null): number[] {
+  return [];
+}
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
