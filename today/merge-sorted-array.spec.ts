def8b8f8e4d8e8b609eac8f4f0b37df6384e9b52
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-29 22:35:40 -0700

    skeleton

---
+++ today/merge-sorted-array.spec.ts
@@ -0,0 +1,7 @@
+import { merge } from './merge-sorted-array';
+
+describe('Merge Sorted Array', () => {
+  it('evaluates correctly', () => {
+    expect(true).toEqual(true);
+  });
+});
---
+++ today/merge-sorted-array.ts
@@ -0,0 +1,8 @@
+/**
+ * https://leetcode.com/problems/merge-sorted-array/description/
+ */
+export { merge };
+
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+  //
+}
