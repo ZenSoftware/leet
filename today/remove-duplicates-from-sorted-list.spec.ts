0de48a4586ad6200822e953e52e5255478bebadf
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-29 22:12:32 -0700

    added tests

--- today/remove-duplicates-from-sorted-list.spec.ts
+++ today/remove-duplicates-from-sorted-list.spec.ts
@@ -1,7 +1,15 @@
 import { deleteDuplicates, toArray, toList } from './remove-duplicates-from-sorted-list';
 
 describe('Remove Duplicates from Sorted List', () => {
-  it('evaluates correctly', () => {
-    expect(true).toEqual(true);
+  it('evaluates correctly 1', () => {
+    const input = toList([1, 1, 2]);
+    const solution = deleteDuplicates(input);
+    expect(toArray(solution)).toEqual([1, 2]);
+  });
+
+  it('evaluates correctly 2', () => {
+    const input = toList([1, 1, 2, 3, 3]);
+    const solution = deleteDuplicates(input);
+    expect(toArray(solution)).toEqual([1, 2, 3]);
   });
 });
