f97452b68b7728c94db6484158faa8f9b4dd2c7b
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-30 14:58:30 -0700

    utilities

--- today/reverse-linked-list-ii.spec.ts
+++ today/reverse-linked-list-ii.spec.ts
@@ -1,7 +1,13 @@
-import { reverseBetween } from './reverse-linked-list-ii';
+import { reverseBetween, toArray, toList } from './reverse-linked-list-ii';
 
 describe('Reverse Linked List II', () => {
   it('evaluates correctly', () => {
     expect(true).toEqual(true);
+  });
+
+  it('constructs lists correctly', () => {
+    const list = toList([1, 2, 3, 4, 5]);
+    const array = toArray(list);
+    expect(array).toEqual([1, 2, 3, 4, 5]);
   });
 });
--- today/reverse-linked-list-ii.ts
+++ today/reverse-linked-list-ii.ts
@@ -1,7 +1,7 @@
 /**
- *
+ * https://leetcode.com/problems/reverse-linked-list-ii/description/
  */
-export { reverseBetween };
+export { reverseBetween, toList, toArray };
 
 function reverseBetween(head: ListNode | null, left: number, right: number): ListNode | null {
   return null;
@@ -14,4 +14,25 @@ class ListNode {
     this.val = val === undefined ? 0 : val;
     this.next = next === undefined ? null : next;
   }
+}
+
+function toList(elements: number[]): ListNode | null {
+  if (elements.length === 0) return null;
+  let prev: ListNode | null = null;
+  for (let i = elements.length - 1; i >= 0; i--) {
+    const node = new ListNode(elements[i]);
+    node.next = prev;
+    prev = node;
+  }
+  return prev;
+}
+
+function toArray(list: ListNode | null): number[] {
+  if (!list) return [];
+  const result: number[] = [];
+  while (list) {
+    result.push(list.val);
+    list = list.next;
+  }
+  return result;
 }
