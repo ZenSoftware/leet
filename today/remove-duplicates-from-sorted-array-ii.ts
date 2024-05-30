25eccefedd2323298df8cc493afe73c9bcf5eb82
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-29 20:19:25 -0700

    docs

--- today/remove-duplicates-from-sorted-array-ii.ts
+++ today/remove-duplicates-from-sorted-array-ii.ts
@@ -6,6 +6,7 @@ export { removeDuplicates };
 function removeDuplicates(nums: number[]): number {
   if (nums.length === 1) return 1;
 
+  // Replace all values with duplicates greater than 2 with null
   let lastUnique = nums[0];
   let duplicateCount = 1;
   for (let i = 1; i < nums.length; i++) {
@@ -21,6 +22,7 @@ function removeDuplicates(nums: number[]): number {
     }
   }
 
+  // Initialize first null position
   let i = 0;
   while (i < nums.length) {
     if (nums[i] === null) {
@@ -29,6 +31,7 @@ function removeDuplicates(nums: number[]): number {
     i++;
   }
 
+  // Shuffle all none null values backwards
   let j = i + 1;
   while (j < nums.length) {
     if (nums[j] !== null) {
