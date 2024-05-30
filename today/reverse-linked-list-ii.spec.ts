f431282f529166999c0bc3b2b0141ba7ce30ccf9
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-30 15:45:52 -0700

    solution

--- today/reverse-linked-list-ii.spec.ts
+++ today/reverse-linked-list-ii.spec.ts
@@ -3,12 +3,14 @@ import { reverseBetween, toArray, toList } from './reverse-linked-list-ii';
 describe('Reverse Linked List II', () => {
   it('evaluates correctly 1', () => {
     const input = toList([1, 2, 3, 4, 5]);
-    expect(reverseBetween(input, 2, 4)).toEqual([1, 4, 3, 2, 5]);
+    const solution = reverseBetween(input, 2, 4);
+    expect(toArray(solution)).toEqual([1, 4, 3, 2, 5]);
   });
 
   it('evaluates correctly 2', () => {
     const input = toList([5]);
-    expect(reverseBetween(input, 1, 1)).toEqual([5]);
+    const solution = reverseBetween(input, 1, 1);
+    expect(toArray(solution)).toEqual([5]);
   });
 
   it('constructs lists correctly', () => {
--- today/reverse-linked-list-ii.ts
+++ today/reverse-linked-list-ii.ts
@@ -4,7 +4,39 @@
 export { reverseBetween, toList, toArray };
 
 function reverseBetween(head: ListNode | null, left: number, right: number): ListNode | null {
-  return null;
+  if (!head?.next) return head;
+
+  let dummyHead = new ListNode(Infinity, head);
+  let pointer: ListNode | null = dummyHead;
+  const rotateElements: ListNode[] = [];
+
+  let l = 1;
+  while (l < left) {
+    pointer = pointer!.next;
+    l++;
+  }
+
+  let pointerBefore = pointer;
+  pointer = pointer!.next;
+
+  let r = l;
+  while (r < right) {
+    rotateElements.push(pointer as ListNode);
+    pointer = pointer!.next;
+    r++;
+  }
+  rotateElements.push(pointer as ListNode);
+
+  let pointerAfter = pointer!.next;
+
+  for (let i = rotateElements.length - 1; i >= 1; i--) {
+    rotateElements[i].next = rotateElements[i - 1];
+  }
+
+  pointerBefore!.next = rotateElements[rotateElements.length - 1];
+  rotateElements[0].next = pointerAfter;
+
+  return dummyHead.next;
 }
 
 class ListNode {
