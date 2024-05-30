23bae4d485db279147012afc193e07500257ee0a
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-29 21:39:42 -0700

    work

--- today/search-in-rotated-sorted-array-ii.spec.ts
+++ today/search-in-rotated-sorted-array-ii.spec.ts
@@ -1,8 +1,31 @@
-import { search } from './search-in-rotated-sorted-array-ii';
+import { search, binarySearch } from './search-in-rotated-sorted-array-ii';
 
 describe('Search in Rotated Sorted Array II', () => {
   it('evaluates correctly', () => {
     expect(search([2, 5, 6, 0, 0, 1, 2], 0)).toEqual(true);
     expect(search([2, 5, 6, 0, 0, 1, 2], 3)).toEqual(false);
+  });
+
+  it('evaluates binary search correctly', () => {
+    const input1 = [0, 1, 2, 3, 4, 5];
+    expect(binarySearch(input1, -1)).toEqual(-1);
+    expect(binarySearch(input1, 0)).toEqual(0);
+    expect(binarySearch(input1, 1)).toEqual(1);
+    expect(binarySearch(input1, 2)).toEqual(2);
+    expect(binarySearch(input1, 3)).toEqual(3);
+    expect(binarySearch(input1, 4)).toEqual(4);
+    expect(binarySearch(input1, 5)).toEqual(5);
+    expect(binarySearch(input1, 6)).toEqual(-1);
+
+    const input2 = [0, 1, 2, 3, 4, 5, 6];
+    expect(binarySearch(input2, -1)).toEqual(-1);
+    expect(binarySearch(input2, 0)).toEqual(0);
+    expect(binarySearch(input2, 1)).toEqual(1);
+    expect(binarySearch(input2, 2)).toEqual(2);
+    expect(binarySearch(input2, 3)).toEqual(3);
+    expect(binarySearch(input2, 4)).toEqual(4);
+    expect(binarySearch(input2, 5)).toEqual(5);
+    expect(binarySearch(input2, 6)).toEqual(6);
+    expect(binarySearch(input2, 7)).toEqual(-1);
   });
 });
--- today/search-in-rotated-sorted-array-ii.ts
+++ today/search-in-rotated-sorted-array-ii.ts
@@ -1,8 +1,28 @@
 /**
  * https://leetcode.com/problems/search-in-rotated-sorted-array-ii/description/
+ * [NeetCode Solution](https://www.youtube.com/watch?v=oUnF7o88_Xc&t=359s)
  */
-export { search };
+export { search, binarySearch };
 
 function search(nums: number[], target: number): boolean {
   return false;
+}
+
+function binarySearch(nums: number[], target: number): number {
+  function bs(start: number, end: number) {
+    const mid = Math.floor((start + end) / 2);
+    if (nums[mid] === target) return mid;
+    if (start >= end) return -1;
+
+    if (nums[start] < nums[mid]) {
+      // in left rotation
+    } else {
+      // in right rotation
+    }
+
+    if (target <= nums[mid]) return bs(start, mid);
+    else return bs(mid + 1, end);
+  }
+
+  return bs(0, nums.length - 1);
 }
