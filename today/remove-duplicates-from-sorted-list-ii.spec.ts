0086ca65cd966dff27e0941dee3ac9049b79dddf
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-29 22:03:47 -0700

    partial solution

--- today/remove-duplicates-from-sorted-list-ii.spec.ts
+++ today/remove-duplicates-from-sorted-list-ii.spec.ts
@@ -1,8 +1,22 @@
 import { deleteDuplicates, toList, toArray } from './remove-duplicates-from-sorted-list-ii';
 
 describe('Remove Duplicates from Sorted List II', () => {
-  it('evaluates correctly', () => {
-    expect(true).toEqual(true);
+  it('evaluates correctly 1', () => {
+    const input = toList([1, 2, 3, 3, 4, 4, 5]);
+    const solution = deleteDuplicates(input);
+    expect(toArray(solution)).toEqual([1, 2, 5]);
+  });
+
+  it('evaluates correctly 2', () => {
+    const input = toList([1, 1, 1, 2, 3]);
+    const solution = deleteDuplicates(input);
+    expect(toArray(solution)).toEqual([2, 3]);
+  });
+
+  it('evaluates correctly 3', () => {
+    const input = toList([1, 1]);
+    const solution = deleteDuplicates(input);
+    expect(toArray(solution)).toEqual([]);
   });
 
   it('constructs lists correctly', () => {
--- today/remove-duplicates-from-sorted-list-ii.ts
+++ today/remove-duplicates-from-sorted-list-ii.ts
@@ -4,7 +4,39 @@
 export { deleteDuplicates, toList, toArray };
 
 function deleteDuplicates(head: ListNode | null): ListNode | null {
-  return null;
+  if (!head?.next) return head;
+
+  const cache = new Set<number>();
+  const duplicate = new Set<number>();
+  const dummyHead: ListNode | null = new ListNode(undefined, head);
+
+  let pointer: ListNode | null = head;
+
+  while (pointer) {
+    if (cache.has(pointer.val)) {
+      duplicate.add(pointer.val);
+    } else {
+      cache.add(pointer.val);
+    }
+    pointer = pointer.next;
+  }
+
+  let parent: ListNode | null = dummyHead;
+  pointer = dummyHead.next;
+
+  while (pointer) {
+    if (duplicate.has(pointer.val)) {
+      while (pointer && duplicate.has(pointer.val)) {
+        pointer = pointer.next;
+      }
+      parent!.next = pointer;
+    }
+
+    parent = parent!.next;
+    pointer = pointer!.next;
+  }
+
+  return dummyHead.next;
 }
 
 function toList(elements: number[]): ListNode | null {
