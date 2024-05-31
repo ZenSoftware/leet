175ae7ed2631eb6765d99def3a71c8f0d07b0aaa
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-30 19:48:53 -0700

    insert done

--- experiments/binary-search-tree.spec.ts
+++ experiments/binary-search-tree.spec.ts
@@ -1,7 +1,16 @@
 import { BinarySearchTree } from './binary-search-tree';
 
 describe('Binary Search Tree', () => {
-  it('evaluates correctly', () => {
-    expect(false).toEqual(true);
+  it('inserts correctly', () => {
+    const bst = new BinarySearchTree();
+    bst.insert(6);
+    bst.insert(3);
+    bst.insert(1);
+    bst.insert(8);
+    bst.insert(4);
+    bst.insert(2);
+    bst.insert(7);
+    bst.insert(5);
+    expect(bst.getInOrder()).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
   });
 });
--- experiments/binary-search-tree.ts
+++ experiments/binary-search-tree.ts
@@ -3,4 +3,41 @@
  */
 export { BinarySearchTree };
 
-class BinarySearchTree {}
+class BinarySearchTree<T = number> {
+  constructor(public root?: Node<T>) {}
+
+  insert(value: T) {
+    if (!this.root) this.root = { value };
+    else this.insertHelper(this.root, value);
+  }
+
+  insertHelper(root: Node<T>, value: T) {
+    if (value < root.value) {
+      if (root.left) this.insertHelper(root.left, value);
+      else root.left = { value };
+    } else {
+      if (root.right) this.insertHelper(root.right, value);
+      else root.right = { value };
+    }
+  }
+
+  getInOrder() {
+    if (!this.root) return [];
+    else {
+      const result: T[] = [];
+      function dfs(root: Node<T>) {
+        if (root.left) dfs(root.left);
+        result.push(root.value);
+        if (root.right) dfs(root.right);
+      }
+      dfs(this.root);
+      return result;
+    }
+  }
+}
+
+interface Node<T> {
+  value: T;
+  left?: Node<T>;
+  right?: Node<T>;
+}
