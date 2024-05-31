4693c30e5b86d9fbfb54964043ea9f8f659d4700
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-30 20:50:45 -0700

    implemented balancing

--- experiments/binary-search-tree.spec.ts
+++ experiments/binary-search-tree.spec.ts
@@ -1,16 +1,51 @@
 import { BinarySearchTree } from './binary-search-tree';
 
 describe('Binary Search Tree', () => {
-  it('inserts correctly', () => {
+  it('constructs BST from array correctly', () => {
     const bst = new BinarySearchTree([5, 4, 2, 1, 6, 8, 7, 3]);
-    // bst.insert(6);
-    // bst.insert(3);
-    // bst.insert(1);
-    // bst.insert(8);
-    // bst.insert(4);
-    // bst.insert(2);
-    // bst.insert(7);
-    // bst.insert(5);
-    expect(bst.getInOrder()).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
+    expect(bst.getInOrderValues()).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
+  });
+
+  it('inserts correctly', () => {
+    const bst = new BinarySearchTree();
+    bst.insert(6);
+    bst.insert(3);
+    bst.insert(1);
+    bst.insert(8);
+    bst.insert(4);
+    bst.insert(2);
+    bst.insert(7);
+    bst.insert(5);
+    expect(bst.getInOrderValues()).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
+  });
+
+  it('balances correctly', () => {
+    const bst = new BinarySearchTree();
+    bst.insert(1);
+    bst.insert(2);
+    bst.insert(3);
+    bst.insert(4);
+    bst.insert(5);
+    bst.insert(6);
+    bst.insert(7);
+    bst.insert(8);
+
+    const allRightValues: number[] = [];
+    let pointer = bst.root;
+    while (pointer) {
+      allRightValues.push(pointer.value);
+      pointer = pointer.right;
+    }
+    expect(allRightValues).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
+
+    const balancedRoot = bst.balance();
+    expect(balancedRoot!.value).toEqual(4);
+    expect(balancedRoot!.left!.value).toEqual(2);
+    expect(balancedRoot!.right!.value).toEqual(6);
+    expect(balancedRoot!.left!.left!.value).toEqual(1);
+    expect(balancedRoot!.left!.right!.value).toEqual(3);
+    expect(balancedRoot!.right!.left!.value).toEqual(5);
+    expect(balancedRoot!.right!.right!.value).toEqual(7);
+    expect(balancedRoot!.right!.right!.right!.value).toEqual(8);
   });
 });
--- experiments/binary-search-tree.ts
+++ experiments/binary-search-tree.ts
@@ -6,18 +6,58 @@ export { BinarySearchTree };
 class BinarySearchTree<T = number> {
   root?: Node<T>;
 
+  /** Constructs a tree utilizing an existing root node */
   constructor(root?: Node<T>);
+  /** Constructs a tree from an array of values */
   constructor(values?: T[]);
+  /** Constructs a new copy of a binary search tree */
+  constructor(tree?: BinarySearchTree<T>);
   constructor(firstParam?: any) {
     if (firstParam) {
       if (Array.isArray(firstParam)) {
-        for (let x of firstParam) {
-          this.insert(x);
+        for (let value of firstParam) {
+          this.insert(value);
+        }
+      } else if (firstParam instanceof BinarySearchTree) {
+        const values = firstParam.getPreOrderValues();
+        for (let value of values) {
+          this.insert(value);
         }
       } else {
         this.root = firstParam;
       }
     }
+  }
+
+  /**
+   * Balances the tree in O(n) time
+   * @returns the new root
+   */
+  balance() {
+    const nodes = this.getInOrderNodes();
+
+    // Reset all left and right nodes to undefined
+    for (let node of nodes) {
+      node.left = undefined;
+      node.right = undefined;
+    }
+
+    // Recursively construct a balanced BST
+    function buildTree(start: number, end: number) {
+      if (start > end) return undefined;
+
+      const mid = Math.floor((start + end) / 2);
+      let node = nodes[mid];
+      node.left = buildTree(start, mid - 1);
+      node.right = buildTree(mid + 1, end);
+
+      return node;
+    }
+
+    // set the new root
+    this.root = buildTree(0, nodes.length - 1);
+
+    return this.root;
   }
 
   insert(value: T) {
@@ -35,13 +75,41 @@ class BinarySearchTree<T = number> {
     }
   }
 
-  getInOrder() {
+  getPreOrderValues() {
+    if (!this.root) return [];
+    else {
+      const result: T[] = [];
+      function dfs(root: Node<T>) {
+        result.push(root.value);
+        if (root.left) dfs(root.left);
+        if (root.right) dfs(root.right);
+      }
+      dfs(this.root);
+      return result;
+    }
+  }
+
+  getInOrderValues() {
     if (!this.root) return [];
     else {
       const result: T[] = [];
       function dfs(root: Node<T>) {
         if (root.left) dfs(root.left);
         result.push(root.value);
+        if (root.right) dfs(root.right);
+      }
+      dfs(this.root);
+      return result;
+    }
+  }
+
+  getInOrderNodes() {
+    if (!this.root) return [];
+    else {
+      const result: Node<T>[] = [];
+      function dfs(root: Node<T>) {
+        if (root.left) dfs(root.left);
+        result.push(root);
         if (root.right) dfs(root.right);
       }
       dfs(this.root);
