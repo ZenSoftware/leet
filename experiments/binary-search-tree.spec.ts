a25a3e987aab7e537be06f23d4de3cddf4b25170
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-30 19:23:52 -0700

    skeleton

---
+++ experiments/binary-search-tree.spec.ts
@@ -0,0 +1,7 @@
+import { BinarySearchTree } from './binary-search-tree';
+
+describe('Binary Search Tree', () => {
+  it('evaluates correctly', () => {
+    expect(false).toEqual(true);
+  });
+});
---
+++ experiments/binary-search-tree.ts
@@ -0,0 +1,6 @@
+/**
+ * [YouTube - Learn Binary Search Tree](https://www.youtube.com/watch?v=Gt2yBZAhsGM&t=1051s)
+ */
+export { BinarySearchTree };
+
+class BinarySearchTree {}
