fcaeeed37cfa0004d2844c50cc98a6d868c8d336
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-01 02:10:23 -0700

    better memory

--- work/populating-next-right-pointers-in-each-node.ts
+++ work/populating-next-right-pointers-in-each-node.ts
@@ -6,28 +6,19 @@ export { _Node, connect };
 function connect(root: _Node | null): _Node | null {
   if (!root) return null;
 
-  const map = new Map<number, _Node[]>();
+  const lastNodeByLevel: Record<number, _Node> = {};
 
   function dfs(node: _Node, level: number) {
-    let levelNodes = map.get(level) as _Node[];
-    if (!levelNodes) {
-      levelNodes = [];
-      map.set(level, levelNodes);
-    }
+    const last = lastNodeByLevel[level];
 
-    levelNodes.push(node);
+    if (last) last.next = node;
 
+    lastNodeByLevel[level] = node;
     if (node.left) dfs(node.left, level + 1);
     if (node.right) dfs(node.right, level + 1);
   }
 
   dfs(root, 1);
-
-  for (let nodes of map.values()) {
-    for (let i = 0; i < nodes.length - 1; i++) {
-      nodes[i].next = nodes[i + 1];
-    }
-  }
 
   return root;
 }
