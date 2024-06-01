18db889b90e6170f49062af40fb493c3ab1539f3
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-01 02:16:41 -0700

    skeleton

---
+++ work/pascals-triangle.spec.ts
@@ -0,0 +1,7 @@
+import { generate } from './pascals-triangle';
+
+describe("Pascal's Triangle", () => {
+  it('evaluates correctly', () => {
+    expect(false).toEqual(true);
+  });
+});
---
+++ work/pascals-triangle.ts
@@ -0,0 +1,8 @@
+/**
+ * https://leetcode.com/problems/pascals-triangle/
+ */
+export { generate };
+
+function generate(numRows: number): number[][] {
+  return [];
+}
