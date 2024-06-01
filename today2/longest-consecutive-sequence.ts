5f561261e51cfd75e3f34bbf2735277850797308
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-01 14:26:16 -0700

    solution

--- today2/longest-consecutive-sequence.ts
+++ today2/longest-consecutive-sequence.ts
@@ -4,5 +4,18 @@
 export { longestConsecutive };
 
 function longestConsecutive(nums: number[]): number {
-  return -1;
+  const set = new Set(nums);
+  let longest = 0;
+
+  for (let n of set) {
+    if (!set.has(n - 1)) {
+      let j = n + 1;
+      while (set.has(j)) {
+        j++;
+      }
+      longest = Math.max(longest, j - n);
+    }
+  }
+
+  return longest;
 }
