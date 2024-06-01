8f1cca2471c7ee21bb831e92ca9cc672d53342d6
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-01 02:24:50 -0700

    solution

--- work/pascals-triangle.ts
+++ work/pascals-triangle.ts
@@ -4,5 +4,19 @@
 export { generate };
 
 function generate(numRows: number): number[][] {
-  return [];
+  if (numRows === 1) return [[1]];
+
+  const result: number[][] = [[1]];
+  for (let i = 1; i < numRows; i++) {
+    let next: number[] = Array(i + 1);
+    result.push(next);
+    next[0] = 1;
+    next[next.length - 1] = 1;
+    for (let j = 1; j < next.length - 1; j++) {
+      const lastRow = result[i - 1];
+      next[j] = lastRow[j - 1] + lastRow[j];
+    }
+  }
+
+  return result;
 }
