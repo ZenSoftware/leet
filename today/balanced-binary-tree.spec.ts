3cdc94e43f01de1149190bd1acb385147f3694cc
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-01 00:03:37 -0700

    added test

--- today/balanced-binary-tree.spec.ts
+++ today/balanced-binary-tree.spec.ts
@@ -13,4 +13,21 @@ describe('Balanced Binary Tree', () => {
     n20.right = n7;
     expect(isBalanced(n3)).toEqual(true);
   });
+
+  it('evaluates correctly 2', () => {
+    const n1 = new TreeNode(1);
+    const n2 = new TreeNode(2);
+    const n3 = new TreeNode(3);
+    const n4 = new TreeNode(4);
+    const n5 = new TreeNode(5);
+    const n6 = new TreeNode(6);
+    const n7 = new TreeNode(7);
+    n1.right = n2;
+    n1.left = n3;
+    n3.right = n4;
+    n3.left = n5;
+    n5.right = n6;
+    n5.left = n7;
+    expect(isBalanced(n1)).toEqual(false);
+  });
 });
