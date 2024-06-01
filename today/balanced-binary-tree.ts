573b0fd3939204ad610a44cf5191e13cd571166f
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-01 01:23:19 -0700

    solution

--- today/balanced-binary-tree.ts
+++ today/balanced-binary-tree.ts
@@ -4,7 +4,34 @@
 export { isBalanced, TreeNode };
 
 function isBalanced(root: TreeNode | null): boolean {
-  return false;
+  if (!root) return true;
+
+  function dfs(node: TreeNode): Info {
+    let leftInfo: Info = { balanced: true, height: 1 };
+    if (node.left) {
+      leftInfo = dfs(node.left);
+    }
+
+    let rightInfo: Info = { balanced: true, height: 1 };
+    if (node.right) {
+      rightInfo = dfs(node.right);
+    }
+
+    const isBalanced = Math.abs(leftInfo.height - rightInfo.height) <= 1;
+
+    return {
+      balanced: leftInfo.balanced && rightInfo.balanced && isBalanced,
+      height: Math.max(leftInfo.height, rightInfo.height) + 1,
+    };
+  }
+
+  const result = dfs(root);
+  return result.balanced;
+}
+
+interface Info {
+  balanced: boolean;
+  height: number;
 }
 
 class TreeNode {
