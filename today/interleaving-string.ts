b147ac5672524f038f2496655e575ab8127c0c84
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-31 20:26:46 -0700

    solution

--- today/interleaving-string.ts
+++ today/interleaving-string.ts
@@ -4,7 +4,16 @@
 export { isInterleave };
 
 function isInterleave(s1: string, s2: string, s3: string): boolean {
-  function search(i1: number, i2: number, i3: number): boolean {}
+  function search(i: number, j: number): boolean {
+    if (i === s1.length && j === s2.length) {
+      return true;
+    }
 
-  return search(0, 0, 0);
+    const result1 = s1[i] === s3[i + j] && search(i + 1, j);
+    const result2 = s2[j] === s3[i + j] && search(i, j + 1);
+
+    return result1 || result2;
+  }
+
+  return search(0, 0);
 }
