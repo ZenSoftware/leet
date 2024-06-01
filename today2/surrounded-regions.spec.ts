16e6e67ab603b8e4b3a8cfbc149c09a1ac9a2ad8
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-01 14:40:23 -0700

    added tests

--- today2/surrounded-regions.spec.ts
+++ today2/surrounded-regions.spec.ts
@@ -1,7 +1,25 @@
 import { solve } from './surrounded-regions';
 
 describe('Surrounded Regions', () => {
-  it('evaluates correctly', () => {
-    expect(true).toEqual(true);
+  it('evaluates correctly 1', () => {
+    const input = [['X']];
+    solve(input);
+    expect(input).toEqual([['X']]);
+  });
+
+  it('evaluates correctly 2', () => {
+    const input = [
+      ['X', 'X', 'X', 'X'],
+      ['X', 'O', 'O', 'X'],
+      ['X', 'X', 'O', 'X'],
+      ['X', 'O', 'X', 'X'],
+    ];
+    solve(input);
+    expect(input).toEqual([
+      ['X', 'X', 'X', 'X'],
+      ['X', 'O', 'O', 'X'],
+      ['X', 'X', 'O', 'X'],
+      ['X', 'O', 'X', 'X'],
+    ]);
   });
 });
