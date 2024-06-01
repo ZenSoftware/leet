6169439428ae593c9f427cd1e4b72b94ba6d84d1
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-01 13:51:34 -0700

    skeleton

---
+++ today2/longest-consecutive-sequence.spec.ts
@@ -0,0 +1,7 @@
+import { longestConsecutive } from './longest-consecutive-sequence';
+
+describe('Longest Consecutive Sequence', () => {
+  it('evaluates correctly', () => {
+    expect(true).toEqual(true);
+  });
+});
---
+++ today2/longest-consecutive-sequence.ts
@@ -0,0 +1,8 @@
+/**
+ * https://leetcode.com/problems/longest-consecutive-sequence/description/
+ */
+export { longestConsecutive };
+
+function longestConsecutive(nums: number[]): number {
+  return -1;
+}
