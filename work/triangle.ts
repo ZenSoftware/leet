f5868e33ce966a61370fa1507fd72348b3f1ab9b
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-01 03:09:10 -0700

    working

--- work/triangle.ts
+++ work/triangle.ts
@@ -4,5 +4,17 @@
 export { minimumTotal };
 
 function minimumTotal(triangle: number[][]): number {
-  return -1;
+  let min = Infinity;
+
+  function dfs(row: number, col: number, sum: number) {
+    if (row === triangle.length && sum < min) min = sum;
+    if (row >= triangle.length || col >= triangle[row].length) return;
+
+    dfs(row + 1, col, sum + triangle[row][col]);
+    dfs(row + 1, col + 1, sum + triangle[row][col]);
+  }
+
+  dfs(0, 0, 0);
+
+  return min;
 }
