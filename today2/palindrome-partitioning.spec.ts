e0e25512cb4171be1026d5c13a9802045f65e208
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-01 16:06:36 -0700

    tests

--- today2/palindrome-partitioning.spec.ts
+++ today2/palindrome-partitioning.spec.ts
@@ -2,6 +2,10 @@ import { partition } from './palindrome-partitioning';
 
 describe('Palindrome Partitioning', () => {
   it('evaluates correctly', () => {
-    expect(true).toEqual(true);
+    expect(partition('aab')).toEqual([
+      ['a', 'a', 'b'],
+      ['aa', 'b'],
+    ]);
+    expect(partition('a')).toEqual([['a']]);
   });
 });
