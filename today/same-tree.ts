4c20013b43a09725ca4c85a9d8eb558b889edcfe
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-31 21:47:57 -0700

    solution

--- today/same-tree.ts
+++ today/same-tree.ts
@@ -4,7 +4,11 @@
 export { isSameTree, TreeNode };
 
 function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
-  return false;
+  if ((p && !q) || (!p && q)) return false;
+  if (!p && !q) return true;
+  if (p!.val !== q!.val) return false;
+
+  return isSameTree(p!.left, q!.left) && isSameTree(p!.right, q!.right);
 }
 
 class TreeNode {
