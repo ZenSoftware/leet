fb559320a04f24776b193c7170548df1fb43cee4
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-01 14:58:44 -0700

    tests

--- today2/surrounded-regions.spec.ts
+++ today2/surrounded-regions.spec.ts
@@ -17,9 +17,27 @@ describe('Surrounded Regions', () => {
     solve(input);
     expect(input).toEqual([
       ['X', 'X', 'X', 'X'],
-      ['X', 'O', 'O', 'X'],
-      ['X', 'X', 'O', 'X'],
+      ['X', 'X', 'X', 'X'],
+      ['X', 'X', 'X', 'X'],
       ['X', 'O', 'X', 'X'],
+    ]);
+  });
+
+  it('evaluates correctly 3', () => {
+    const input = [
+      ['O', 'X', 'X', 'O', 'X'],
+      ['X', 'O', 'O', 'X', 'O'],
+      ['X', 'O', 'X', 'O', 'X'],
+      ['O', 'X', 'O', 'O', 'O'],
+      ['X', 'X', 'O', 'X', 'O'],
+    ];
+    solve(input);
+    expect(input).toEqual([
+      ['O', 'X', 'X', 'O', 'X'],
+      ['X', 'X', 'X', 'X', 'O'],
+      ['X', 'X', 'X', 'O', 'X'],
+      ['O', 'X', 'O', 'O', 'O'],
+      ['X', 'X', 'O', 'X', 'O'],
     ]);
   });
 });
