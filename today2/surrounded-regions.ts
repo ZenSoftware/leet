879ef228391e24c228aa4107c9aa19fc95698609
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-01 15:58:42 -0700

    solution

--- today2/surrounded-regions.ts
+++ today2/surrounded-regions.ts
@@ -4,67 +4,32 @@
 export { solve };
 
 function solve(board: string[][]): void {
-  const ignore = new Set<string>();
+  const lastRowIndex = board.length - 1;
+  const lastColIndex = board[0].length - 1;
 
-  for (let row = 1; row < board.length - 1; row++) {
-    for (let col = 1; col < board[0].length - 1; col++) {
-      if (board[row][col] === 'O' && !ignore.has(`${row},${col}`)) {
-        const connected = connectedSet(board, row, col);
-        if (hasBorderingO(board, connected)) {
-          for (let c of connected.keys()) {
-            ignore.add(c);
-          }
-        } else {
-          for (let c of connected.values()) {
-            board[c.row][c.col] = 'X';
-          }
-        }
-      }
+  for (let y = 0; y <= lastColIndex; y++) {
+    if (board[0][y] === 'O') markIgnore(board, 0, y);
+    if (board[lastRowIndex][y] === 'O') markIgnore(board, lastRowIndex, y);
+  }
+
+  for (let x = 1; x < lastRowIndex; x++) {
+    if (board[x][0] === 'O') markIgnore(board, x, 0);
+    if (board[x][lastColIndex] === 'O') markIgnore(board, x, lastColIndex);
+  }
+
+  for (let x = 0; x <= lastRowIndex; x++) {
+    for (let y = 0; y <= lastColIndex; y++) {
+      if (board[x][y] === 'O') board[x][y] = 'X';
+      if (board[x][y] === 'N') board[x][y] = 'O';
     }
   }
 }
 
-function hasBorderingO(board: string[][], connected: Map<string, Cell>) {
-  const lastRowIndex = board.length - 1;
-  const lastColIndex = board[0].length - 1;
-
-  for (let cell of connected.values()) {
-    if (
-      cell.row === 0 ||
-      cell.row === lastRowIndex ||
-      cell.col === 0 ||
-      cell.col === lastColIndex
-    ) {
-      return true;
-    }
-  }
-
-  return false;
-}
-
-function connectedSet(board: string[][], row: number, col: number): Map<string, Cell> {
-  const lastRowIndex = board.length - 1;
-  const lastColIndex = board[0].length - 1;
-  const connectedSet = new Map<string, Cell>();
-
-  function dfs(i: number, j: number) {
-    if (i < 0 || i > lastRowIndex) return;
-    if (j < 0 || j > lastColIndex) return;
-    if (board[i][j] === 'X') return;
-    let key = `${i},${j}`;
-    if (connectedSet.has(key)) return;
-    connectedSet.set(`${i},${j}`, { row: i, col: j });
-    dfs(i + 1, j);
-    dfs(i - 1, j);
-    dfs(i, j + 1);
-    dfs(i, j - 1);
-  }
-
-  dfs(row, col);
-  return connectedSet;
-}
-
-interface Cell {
-  row: number;
-  col: number;
+function markIgnore(board: string[][], x: number, y: number): void {
+  if (board[x]?.[y] !== 'O') return;
+  board[x][y] = 'N';
+  markIgnore(board, x + 1, y);
+  markIgnore(board, x - 1, y);
+  markIgnore(board, x, y + 1);
+  markIgnore(board, x, y - 1);
 }
