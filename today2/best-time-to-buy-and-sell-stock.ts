9379e0434099fe8c1be00c518e741e42ba1408ff
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-01 12:03:37 -0700

    work

--- today2/best-time-to-buy-and-sell-stock.ts
+++ today2/best-time-to-buy-and-sell-stock.ts
@@ -4,5 +4,13 @@
 export { maxProfit };
 
 function maxProfit(prices: number[]): number {
+  let left = 0;
+  let right = 1;
+
+  for (let i = 0; i < prices.length - 1; i++) {
+    if (prices[i + 1] >= prices[right]) right = i + 1;
+    if (prices[i] < prices[left] && i < right) left = i;
+  }
+
   return -1;
 }
