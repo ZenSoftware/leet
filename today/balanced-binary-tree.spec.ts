a75ed1d053dd77ed9b6ed9a172da45d6b2c11f5d
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-31 23:59:48 -0700

    skeleton

---
+++ today/balanced-binary-tree.spec.ts
@@ -0,0 +1,7 @@
+import { isBalanced, TreeNode } from './balanced-binary-tree';
+
+describe('Balanced Binary Tree', () => {
+  it('evaluates correctly', () => {
+    expect(true).toEqual(true);
+  });
+});
