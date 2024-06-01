93bad7d7108c44405dcca584e7254350f018ff7a
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-31 22:41:54 -0700

    solution

--- today/binary-tree-zigzag-level-order-traversal.ts
+++ today/binary-tree-zigzag-level-order-traversal.ts
@@ -4,7 +4,37 @@
 export { zigzagLevelOrder, TreeNode };
 
 function zigzagLevelOrder(root: TreeNode | null): number[][] {
-  return [];
+  if (!root) return [];
+
+  const map = new Map<number, number[]>();
+
+  function dfs(node: TreeNode, level: number) {
+    let result: number[];
+    if (!map.get(level)) {
+      result = [];
+      map.set(level, result);
+    } else {
+      result = map.get(level) as number[];
+    }
+
+    result.push(node.val);
+
+    if (node.left) dfs(node.left, level + 1);
+    if (node.right) dfs(node.right, level + 1);
+  }
+
+  dfs(root, 0);
+
+  const entries = Array.from(map.entries()).sort(
+    ([aLevel, aArray], [bLevel, bArray]) => aLevel - bLevel
+  );
+
+  const result = entries.map(([aLevel, aArray]) => aArray);
+  for (let i = 1; i < result.length; i += 2) {
+    result[i].reverse();
+  }
+
+  return result;
 }
 
 class TreeNode {
