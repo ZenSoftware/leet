8396a3d2888e7daf55df551c06db4d94d7e92790
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-31 22:17:30 -0700

    cleaned up logic

--- today/same-tree.ts
+++ today/same-tree.ts
@@ -4,8 +4,8 @@
 export { isSameTree, TreeNode };
 
 function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
-  if ((p && !q) || (!p && q)) return false;
   if (!p && !q) return true;
+  if (!p || !q) return false;
   if (p!.val !== q!.val) return false;
 
   return isSameTree(p!.left, q!.left) && isSameTree(p!.right, q!.right);
