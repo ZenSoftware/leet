aa4f26bc6196572cb44351cd83ce1ff21d4565ba
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-31 22:19:34 -0700

    skeleton

---
+++ today/binary-tree-level-order-traversal.spec.ts
@@ -0,0 +1,7 @@
+import { TreeNode, levelOrder } from './binary-tree-level-order-traversal';
+
+describe('Binary Tree Level Order Traversal', () => {
+  it('evaluates correctly', () => {
+    expect(true).toEqual(true);
+  });
+});
