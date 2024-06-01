8b7966a6bb286b0f65f747e3eea7831980fa4276
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-31 19:53:19 -0700

    added tests

--- today/interleaving-string.spec.ts
+++ today/interleaving-string.spec.ts
@@ -2,6 +2,8 @@ import { isInterleave } from './interleaving-string';
 
 describe('Interleaving String', () => {
   it('evaluates correctly', () => {
-    expect(true).toEqual(true);
+    expect(isInterleave('aabcc', 'dbbca', 'aadbbcbcac')).toEqual(true);
+    expect(isInterleave('aabcc', 'dbbca', 'aadbbbaccc')).toEqual(false);
+    expect(isInterleave('', '', '')).toEqual(true);
   });
 });
