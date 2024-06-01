d6c138f96f3c90ee4888eabd0f049662b8282639
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-31 23:47:59 -0700

    solution

--- today/convert-sorted-array-to-binary-search-tree.ts
+++ today/convert-sorted-array-to-binary-search-tree.ts
@@ -4,7 +4,17 @@
 export { sortedArrayToBST, TreeNode };
 
 function sortedArrayToBST(nums: number[]): TreeNode | null {
-  return null;
+  function construct(start: number, end: number): TreeNode | null {
+    if (start > end) return null;
+
+    const mid = Math.floor((start + end) / 2);
+    const node = new TreeNode(nums[mid]);
+    node.left = construct(start, mid - 1);
+    node.right = construct(mid + 1, end);
+    return node;
+  }
+
+  return construct(0, nums.length - 1);
 }
 
 class TreeNode {
