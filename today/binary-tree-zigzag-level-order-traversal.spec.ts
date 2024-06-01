374267f58b8009344d7a0622080c157a326547fe
Zen Software <ZenSoftware@users.noreply.github.com>
2024-05-31 22:36:19 -0700

    skeleton

---
+++ today/binary-tree-zigzag-level-order-traversal.spec.ts
@@ -0,0 +1,7 @@
+import { zigzagLevelOrder, TreeNode } from './binary-tree-zigzag-level-order-traversal';
+
+describe('Binary Tree Zigzag Level Order Traversal', () => {
+  it('evaluates correctly', () => {
+    expect(true).toEqual(true);
+  });
+});
