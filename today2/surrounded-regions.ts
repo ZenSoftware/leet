bcb1341e0cba8848ed084bf432a7cfe26af86dec
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-01 16:02:45 -0700

    rename

--- today2/surrounded-regions.ts
+++ today2/surrounded-regions.ts
@@ -4,21 +4,21 @@
 export { solve };
 
 function solve(board: string[][]): void {
-  const lastRowIndex = board.length - 1;
-  const lastColIndex = board[0].length - 1;
+  const lastRow = board.length - 1;
+  const lastCol = board[0].length - 1;
 
-  for (let y = 0; y <= lastColIndex; y++) {
+  for (let y = 0; y <= lastCol; y++) {
     markIgnore(board, 0, y);
-    markIgnore(board, lastRowIndex, y);
+    markIgnore(board, lastRow, y);
   }
 
-  for (let x = 1; x < lastRowIndex; x++) {
+  for (let x = 1; x < lastRow; x++) {
     markIgnore(board, x, 0);
-    markIgnore(board, x, lastColIndex);
+    markIgnore(board, x, lastCol);
   }
 
-  for (let x = 0; x <= lastRowIndex; x++) {
-    for (let y = 0; y <= lastColIndex; y++) {
+  for (let x = 0; x <= lastRow; x++) {
+    for (let y = 0; y <= lastCol; y++) {
       if (board[x][y] === 'O') board[x][y] = 'X';
       if (board[x][y] === 'N') board[x][y] = 'O';
     }
