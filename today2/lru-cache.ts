6b4b816949777732e61fbb73fe12063edf33cdb3
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-02 02:12:37 -0700

    skeleton

---
+++ today2/lru-cache.ts
@@ -0,0 +1,18 @@
+/**
+ * https://leetcode.com/problems/lru-cache/
+ */
+export { LRUCache };
+
+class LRUCache {
+  constructor(capacity: number) {
+    //
+  }
+
+  get(key: number): number {
+    return -1;
+  }
+
+  put(key: number, value: number): void {
+    //
+  }
+}
