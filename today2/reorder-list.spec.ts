c32b97c72eca1526377a38aa7d19db2ca63b30bd
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-02 00:04:36 -0700

    added tests

--- today2/reorder-list.spec.ts
+++ today2/reorder-list.spec.ts
@@ -1,8 +1,16 @@
 import { reorderList, toArray, toList, ListNode } from './reorder-list';
 
 describe('Reorder List', () => {
-  it('evaluates correctly', () => {
-    expect(true).toEqual(true);
+  it('evaluates correctly 1', () => {
+    const input = toList([1, 2, 3, 4]);
+    reorderList(input);
+    expect(toArray(input)).toEqual([1, 4, 2, 3]);
+  });
+
+  it('evaluates correctly 2', () => {
+    const input = toList([1, 2, 3, 4, 5]);
+    reorderList(input);
+    expect(toArray(input)).toEqual([1, 5, 2, 4, 3]);
   });
 
   it('constructs lists correctly', () => {
