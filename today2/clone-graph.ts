d40aa289f0f6f25a9dd73c686366e39f82382db4
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-01 18:35:54 -0700

    solution

--- today2/clone-graph.ts
+++ today2/clone-graph.ts
@@ -4,7 +4,34 @@
 export { cloneGraph, _Node };
 
 function cloneGraph(node: _Node | null): _Node | null {
-  return null;
+  if (!node) return null;
+
+  const visited = new Set<_Node>();
+
+  function dfs(current: _Node) {
+    visited.add(current);
+    for (const n of current.neighbors) {
+      if (!visited.has(n)) dfs(n);
+    }
+  }
+
+  dfs(node);
+
+  const nodeMap = new Map<_Node, _Node>();
+  for (const n of visited) {
+    const copy = new _Node(n.val);
+    nodeMap.set(n, copy);
+  }
+
+  for (const n of visited) {
+    const parent = nodeMap.get(n) as _Node;
+    for (const a of n.neighbors) {
+      const neighbor = nodeMap.get(a) as _Node;
+      parent.neighbors.push(neighbor);
+    }
+  }
+
+  return nodeMap.get(node) as _Node;
 }
 
 class _Node {
