773958b2a191af84f1b2660ff93b00948d5800a7
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-01 20:06:21 -0700

    skeleton

---
+++ today2/single-number.spec.ts
@@ -0,0 +1,7 @@
+import { singleNumber } from './single-number';
+
+describe('Single Number', () => {
+  it('evaluates correctly', () => {
+    expect(false).toEqual(true);
+  });
+});
---
+++ today2/single-number.ts
@@ -0,0 +1,8 @@
+/**
+ * https://leetcode.com/problems/single-number/description/
+ */
+export { singleNumber };
+
+function singleNumber(nums: number[]): number {
+  return Infinity;
+}
