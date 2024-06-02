c6627db3b3bc06f8e9561dbbdf469f02cd473345
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-01 23:50:19 -0700

    removed string allocations

--- today2/word-break.ts
+++ today2/word-break.ts
@@ -4,28 +4,28 @@
 export { wordBreak };
 
 function wordBreak(s: string, wordDict: string[]): boolean {
-  const memo: Record<string, boolean> = {};
+  const memo: Record<number, boolean> = {};
 
-  function recurse(str: string): boolean {
-    if (str in memo) return memo[str];
-    if (str.length === 0) return true;
+  function recurse(sIndex: number): boolean {
+    if (sIndex in memo) return memo[sIndex];
+    if (sIndex === s.length) return true;
 
     for (let word of wordDict) {
       let matches = true;
       for (let w = 0; w < word.length; w++) {
-        if (str[w] !== word[w]) {
+        if (s[sIndex + w] !== word[w]) {
           matches = false;
           break;
         }
       }
       if (matches) {
-        const result = recurse(str.slice(word.length));
-        if (result) return (memo[str] = true);
+        const result = recurse(sIndex + word.length);
+        if (result) return (memo[sIndex] = true);
       }
     }
 
-    return (memo[str] = false);
+    return (memo[sIndex] = false);
   }
 
-  return recurse(s);
+  return recurse(0);
 }
