a4e8c1ca60bbf94d2c246f8f4b6861feffee2c3a
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-01 18:43:37 -0700

    skeleton

---
+++ today2/gas-station.spec.ts
@@ -0,0 +1,7 @@
+import { canCompleteCircuit } from './gas-station';
+
+describe('Gas Station', () => {
+  it('evaluates correctly', () => {
+    expect(false).toEqual(true);
+  });
+});
---
+++ today2/gas-station.ts
@@ -0,0 +1,8 @@
+/**
+ * https://leetcode.com/problems/gas-station/description/
+ */
+export { canCompleteCircuit };
+
+function canCompleteCircuit(gas: number[], cost: number[]): number {
+  return -1;
+}
