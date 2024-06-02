0893f21e3b6b9da5ce5bdaed209e906981a10eed
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-01 23:26:33 -0700

    solution

--- today2/word-break.ts
+++ today2/word-break.ts
@@ -4,22 +4,25 @@
 export { wordBreak };
 
 function wordBreak(s: string, wordDict: string[]): boolean {
-  function recurse(sIndex: number): boolean {
-    if (sIndex === s.length) return true;
+  function recurse(str: string): boolean {
+    if (str.length === 0) return true;
 
     for (let word of wordDict) {
       let matches = true;
       for (let w = 0; w < word.length; w++) {
-        if (s[sIndex + w] !== word[w]) {
+        if (str[w] !== word[w]) {
           matches = false;
           break;
         }
       }
-      if (matches) return recurse(sIndex + word.length);
+      if (matches) {
+        const ans = recurse(str.slice(word.length));
+        if (ans) return true;
+      }
     }
 
     return false;
   }
 
-  return recurse(0);
+  return recurse(s);
 }
