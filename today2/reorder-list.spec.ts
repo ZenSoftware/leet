3c7578aed78ea650b491692e261a96c9352742b9
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-02 00:03:23 -0700

    utlities

--- today2/reorder-list.spec.ts
+++ today2/reorder-list.spec.ts
@@ -1,7 +1,13 @@
-import { ListNode, reorderList } from './reorder-list';
+import { reorderList, toArray, toList, ListNode } from './reorder-list';
 
 describe('Reorder List', () => {
   it('evaluates correctly', () => {
     expect(true).toEqual(true);
+  });
+
+  it('constructs lists correctly', () => {
+    const list = toList([1, 2, 3, 4]);
+    const array = toArray(list);
+    expect(array).toEqual([1, 2, 3, 4]);
   });
 });
--- today2/reorder-list.ts
+++ today2/reorder-list.ts
@@ -1,7 +1,7 @@
 /**
  * https://leetcode.com/problems/reorder-list/description/
  */
-export { reorderList, ListNode };
+export { reorderList, toArray, toList, ListNode };
 
 function reorderList(head: ListNode | null): void {}
 
@@ -12,4 +12,25 @@ class ListNode {
     this.val = val === undefined ? 0 : val;
     this.next = next === undefined ? null : next;
   }
+}
+
+function toList(elements: number[]): ListNode | null {
+  if (elements.length === 0) return null;
+  let pointer: ListNode | null = null;
+  for (let i = elements.length - 1; i >= 0; i--) {
+    const node = new ListNode(elements[i]);
+    node.next = pointer;
+    pointer = node;
+  }
+  return pointer;
+}
+
+function toArray(list: ListNode | null): number[] {
+  if (!list) return [];
+  const result: number[] = [];
+  while (list) {
+    result.push(list.val);
+    list = list.next;
+  }
+  return result;
 }
