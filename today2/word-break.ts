b9826258bdfc03b3e295ecb05d4b25a3225059ce
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-01 23:03:30 -0700

    progress

--- today2/word-break.ts
+++ today2/word-break.ts
@@ -4,5 +4,22 @@
 export { wordBreak };
 
 function wordBreak(s: string, wordDict: string[]): boolean {
-  return false;
+  function recurse(sIndex: number): boolean {
+    if (sIndex === s.length) return true;
+
+    for (let word of wordDict) {
+      let matches = true;
+      for (let w = 0; w < word.length; w++) {
+        if (s[sIndex + w] !== word[w]) {
+          matches = false;
+          break;
+        }
+      }
+      if (matches) return recurse(sIndex + word.length);
+    }
+
+    return false;
+  }
+
+  return recurse(0);
 }
