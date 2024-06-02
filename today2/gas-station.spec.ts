efc2a06b2ec550d8d501ef5b0a71133d4163e863
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-01 18:44:27 -0700

    tests

--- today2/gas-station.spec.ts
+++ today2/gas-station.spec.ts
@@ -2,6 +2,7 @@ import { canCompleteCircuit } from './gas-station';
 
 describe('Gas Station', () => {
   it('evaluates correctly', () => {
-    expect(false).toEqual(true);
+    expect(canCompleteCircuit([1, 2, 3, 4, 5], [3, 4, 5, 1, 2])).toEqual(3);
+    expect(canCompleteCircuit([2, 3, 4], [3, 4, 3])).toEqual(-1);
   });
 });
