8553561a873c4586c64bcfadc9eb1e1f0a90d24d
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-01 19:04:35 -0700

    slow solution

--- today2/gas-station.ts
+++ today2/gas-station.ts
@@ -4,5 +4,28 @@
 export { canCompleteCircuit };
 
 function canCompleteCircuit(gas: number[], cost: number[]): number {
+  for (let i = 0; i < gas.length; i++) {
+    if (checkCircuit(gas, cost, i)) return i;
+  }
+
   return -1;
+}
+
+function checkCircuit(gas: number[], cost: number[], station: number): boolean {
+  let current = station;
+  let tank = 0;
+
+  do {
+    tank += gas[current];
+    tank -= cost[current];
+    if (tank < 0) return false;
+    current = nextIndex(current, gas.length);
+  } while (current !== station);
+
+  return true;
+}
+
+function nextIndex(current: number, circuitLength: number) {
+  if (current + 1 === circuitLength) return 0;
+  else return current + 1;
 }
