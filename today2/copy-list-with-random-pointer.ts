d44aabb624eac8ef7052243ee0918a48ff63566d
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-01 22:41:40 -0700

    solution

--- today2/copy-list-with-random-pointer.ts
+++ today2/copy-list-with-random-pointer.ts
@@ -4,7 +4,33 @@
 export { copyRandomList, _Node };
 
 function copyRandomList(head: _Node | null): _Node | null {
-  return null;
+  if (!head) return null;
+
+  const originals = new Set<_Node>();
+  let pointer: _Node | null = head;
+
+  while (pointer) {
+    originals.add(pointer);
+    pointer = pointer.next;
+  }
+
+  const nodeMap = new Map<_Node, _Node>();
+
+  for (let node of originals) {
+    let copy = new _Node(node.val);
+    nodeMap.set(node, copy);
+  }
+
+  pointer = head;
+
+  while (pointer) {
+    const copy = nodeMap.get(pointer) as _Node;
+    if (pointer.next) copy.next = nodeMap.get(pointer.next) as _Node;
+    if (pointer.random) copy.random = nodeMap.get(pointer.random) as _Node;
+    pointer = pointer.next;
+  }
+
+  return nodeMap.get(head) as _Node;
 }
 
 class _Node {
