9ee693a729065c3930769c02d6a404f83b2f748c
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-02 22:09:08 -0700

    refactor

--- experiments/binary-search.ts
+++ experiments/binary-search.ts
@@ -1,14 +1,10 @@
-export function binarySearch(numbers: number[], find: number) {
-  function bs(start: number, end: number) {
-    if (numbers[start] === find) return start;
-    if (numbers[end] === find) return end;
-    if (start >= end) return -1;
-
+export function binarySearch(nums: number[], target: number) {
+  function bs(start: number, end: number): number {
     const mid = Math.floor((start + end) / 2);
-
-    if (find <= numbers[mid]) return bs(start, mid);
+    if (nums[mid] === target) return mid;
+    if (start >= end) return -1;
+    if (target <= nums[mid]) return bs(start, mid);
     else return bs(mid + 1, end);
   }
-
-  return bs(0, numbers.length - 1);
+  return bs(0, nums.length - 1);
 }
