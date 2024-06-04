45ad98c7f5aa3dea7c242ab310dead8cc92597a7
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-03 18:57:06 -0700

    solution

--- today3/lru-cache.spec.ts
+++ today3/lru-cache.spec.ts
@@ -1,16 +1,39 @@
-import { LRUCache } from './lru-cache';
+import { LRUCache, CacheItem } from './lru-cache';
 
 describe('LRU Cache', () => {
   it('evaluates correctly', () => {
     const lRUCache = new LRUCache(2);
+
     lRUCache.put(1, 1); // cache is {1=1}
+    expect(lRUCache.getValuesForward()).toEqual(['HEAD', 1, 'TAIL']);
+    expect(lRUCache.getValuesBackwards().reverse()).toEqual(['HEAD', 1, 'TAIL']);
+
     lRUCache.put(2, 2); // cache is {1=1, 2=2}
+    expect(lRUCache.getValuesForward()).toEqual(['HEAD', 2, 1, 'TAIL']);
+    expect(lRUCache.getValuesBackwards().reverse()).toEqual(['HEAD', 2, 1, 'TAIL']);
+
     expect(lRUCache.get(1)).toEqual(1); // return 1
+    expect(lRUCache.getValuesForward()).toEqual(['HEAD', 1, 2, 'TAIL']);
+    expect(lRUCache.getValuesBackwards().reverse()).toEqual(['HEAD', 1, 2, 'TAIL']);
+
     lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+    expect(lRUCache.getValuesForward()).toEqual(['HEAD', 3, 1, 'TAIL']);
+    expect(lRUCache.getValuesBackwards().reverse()).toEqual(['HEAD', 3, 1, 'TAIL']);
+
     expect(lRUCache.get(2)).toEqual(-1); // returns -1 (not found)
+
     lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+    expect(lRUCache.getValuesForward()).toEqual(['HEAD', 4, 3, 'TAIL']);
+    expect(lRUCache.getValuesBackwards().reverse()).toEqual(['HEAD', 4, 3, 'TAIL']);
+
     expect(lRUCache.get(1)).toEqual(-1); // return -1 (not found)
+
     expect(lRUCache.get(3)).toEqual(3); // return 3
+    expect(lRUCache.getValuesForward()).toEqual(['HEAD', 3, 4, 'TAIL']);
+    expect(lRUCache.getValuesBackwards().reverse()).toEqual(['HEAD', 3, 4, 'TAIL']);
+
     expect(lRUCache.get(4)).toEqual(4); // return 4
+    expect(lRUCache.getValuesForward()).toEqual(['HEAD', 4, 3, 'TAIL']);
+    expect(lRUCache.getValuesBackwards().reverse()).toEqual(['HEAD', 4, 3, 'TAIL']);
   });
 });
--- today3/lru-cache.ts
+++ today3/lru-cache.ts
@@ -1,18 +1,101 @@
 /**
  * https://leetcode.com/problems/lru-cache/
  */
-export { LRUCache };
+export { LRUCache, CacheItem };
 
 class LRUCache {
-  constructor(capacity: number) {
-    //
+  dummyHead: CacheItem;
+  dummyTail: CacheItem;
+  cache = new Map<number, CacheItem>();
+
+  constructor(private capacity: number) {
+    if (capacity <= 0) {
+      throw Error(`LRUCache capacity must be a positive integer. Recieved: ${capacity}`);
+    }
+
+    this.dummyHead = {
+      key: 'HEAD' as any,
+      value: 'HEAD' as any,
+      next: undefined as any,
+      previous: undefined as any,
+    };
+    this.dummyTail = {
+      key: 'TAIL' as any,
+      value: 'TAIL' as any,
+      next: undefined as any,
+      previous: undefined as any,
+    };
+    this.dummyHead.next = this.dummyTail;
+    this.dummyTail.previous = this.dummyHead;
   }
 
   get(key: number): number {
+    const item = this.cache.get(key);
+    if (item) {
+      this.moveToFront(item);
+      return item.value;
+    }
+
     return -1;
   }
 
   put(key: number, value: number): void {
-    //
+    let item = this.cache.get(key);
+    if (item) {
+      item.value = value;
+      this.moveToFront(item);
+    } else {
+      if (this.cache.size === this.capacity) {
+        // remove LRU
+        this.cache.delete(this.dummyTail.previous.key);
+        this.dummyTail.previous.previous.next = this.dummyTail;
+        this.dummyTail.previous = this.dummyTail.previous.previous;
+      }
+
+      item = { key, value, next: this.dummyHead.next, previous: this.dummyHead };
+      this.cache.set(key, item);
+      this.dummyHead.next.previous = item;
+      this.dummyHead.next = item;
+    }
   }
+
+  moveToFront(item: CacheItem) {
+    if (item !== this.dummyHead.next) {
+      item.previous.next = item.next;
+      item.next.previous = item.previous;
+
+      item.next = this.dummyHead.next;
+      item.previous = this.dummyHead;
+
+      this.dummyHead.next.previous = item;
+      this.dummyHead.next = item;
+    }
+  }
+
+  getValuesForward(): any[] {
+    let pointer: CacheItem | undefined = this.dummyHead;
+    const result: any[] = [];
+    while (pointer) {
+      result.push(pointer.value);
+      pointer = pointer.next;
+    }
+    return result;
+  }
+
+  getValuesBackwards(): any[] {
+    let pointer: CacheItem | undefined = this.dummyTail;
+    const result: any[] = [];
+    while (pointer) {
+      result.push(pointer.value);
+      pointer = pointer.previous;
+    }
+    return result;
+  }
+}
+
+interface CacheItem {
+  key: number;
+  value: number;
+  previous: CacheItem;
+  next: CacheItem;
 }
