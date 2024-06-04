f3b7ad5de70164787ed242bfe3859d229fc769e5
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-04 04:51:15 -0700

    tests

--- today3/find-minimum-in-rotated-sorted-array.spec.ts
+++ today3/find-minimum-in-rotated-sorted-array.spec.ts
@@ -2,6 +2,8 @@ import { findMin } from './find-minimum-in-rotated-sorted-array';
 
 describe('Find Minimum in Rotated Sorted Array', () => {
   it('evaluates correctly', () => {
-    expect(false).toEqual(true);
+    expect(findMin([3, 4, 5, 1, 2])).toEqual(1);
+    expect(findMin([4, 5, 6, 7, 0, 1, 2])).toEqual(0);
+    expect(findMin([11, 13, 15, 17])).toEqual(11);
   });
 });
