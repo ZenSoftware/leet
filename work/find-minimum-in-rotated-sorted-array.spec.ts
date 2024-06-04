4952a9d830957b3d018322ffa9984d61108fe233
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-04 05:31:27 -0700

    moved

--- work/find-minimum-in-rotated-sorted-array.spec.ts
+++ work/find-minimum-in-rotated-sorted-array.spec.ts
@@ -5,5 +5,7 @@ describe('Find Minimum in Rotated Sorted Array', () => {
     expect(findMin([3, 4, 5, 1, 2])).toEqual(1);
     expect(findMin([4, 5, 6, 7, 0, 1, 2])).toEqual(0);
     expect(findMin([11, 13, 15, 17])).toEqual(11);
+    expect(findMin([2, 1])).toEqual(1);
+    // expect(findMin([3, 1, 2])).toEqual(1);
   });
 });
---
+++ work/find-minimum-in-rotated-sorted-array.ts
@@ -0,0 +1,6 @@
+/**
+ * https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
+ */
+export { findMin };
+
+function findMin(nums: number[]): number {}
