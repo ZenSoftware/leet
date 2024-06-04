2ee287a5e6952cac348dec10ff82673fd1b09bd4
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-04 04:53:52 -0700

    progress

--- today3/find-minimum-in-rotated-sorted-array.ts
+++ today3/find-minimum-in-rotated-sorted-array.ts
@@ -6,6 +6,13 @@ export { findMin };
 function findMin(nums: number[]): number {
   function bs(start: number, end: number): number {
     if (start >= end) return nums[start];
+
+    const mid = Math.floor((start + end) / 2);
+    if (nums[start] <= nums[mid]) {
+      // Left portion of rotation
+    } else {
+      // Right portion of rotation
+    }
   }
   return bs(0, nums.length - 1);
 }
