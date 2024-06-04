a67158619068d027be6882f5b18264d88731eaae
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-04 15:14:29 -0700

    tests

--- csharp/Leet/Problems/IntersectionOfTwoLinkedLists.cs
+++ csharp/Leet/Problems/IntersectionOfTwoLinkedLists.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Leet.Problems
+﻿namespace Leet.Problems
 {
-    internal class IntersectionOfTwoLinkedLists
+    public class IntersectionOfTwoLinkedLists
     {
+        public bool Sample() { return true; }
     }
 }
---
+++ csharp/LeetTests/IntersectionOfTwoLinkedListsTest.cs
@@ -0,0 +1,14 @@
+﻿using Leet.Problems;
+
+namespace LeetTests
+{
+    public class IntersectionOfTwoLinkedListsTest
+    {
+        [Test]
+        public void ShouldPass()
+        {
+            var leet = new IntersectionOfTwoLinkedLists();
+            Assert.True(leet.Sample());
+        }
+    }
+}
