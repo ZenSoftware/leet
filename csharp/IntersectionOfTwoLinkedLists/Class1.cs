60b7e2db33cb5a2e9226d6a66adbb7a211b03527
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-04 14:05:40 -0700

    skeleton

---
+++ csharp/IntersectionOfTwoLinkedLists/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace IntersectionOfTwoLinkedLists
+{
+    public class Solution
+    {
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+}
