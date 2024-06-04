a72c8edce93ed77752afe0bb5cda454fc85dec1e
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-04 04:30:05 -0700

    skeleton

--- work/search-in-rotated-sorted-array-ii.ts
+++ work/search-in-rotated-sorted-array-ii.ts
@@ -2,26 +2,19 @@
  * https://leetcode.com/problems/search-in-rotated-sorted-array-ii/description/
  * [NeetCode Solution](https://www.youtube.com/watch?v=oUnF7o88_Xc&t=359s)
  */
-export { search, binarySearch };
+export { search };
 
 function search(nums: number[], target: number): boolean {
-  return false;
-}
-
-function binarySearch(nums: number[], target: number): number {
-  function bs(start: number, end: number) {
+  function bs(start: number, end: number): boolean {
     const mid = Math.floor((start + end) / 2);
-    if (nums[mid] === target) return mid;
-    if (start >= end) return -1;
+    if (nums[mid] === target) return true;
+    if (start >= end) return false;
 
-    if (nums[start] < nums[mid]) {
+    if (nums[start] <= nums[mid]) {
       // in left rotation
     } else {
       // in right rotation
     }
-
-    if (target <= nums[mid]) return bs(start, mid);
-    else return bs(mid + 1, end);
   }
 
   return bs(0, nums.length - 1);
