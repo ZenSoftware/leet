7b2ba7280d0eb351f78ecc391eedcfd53cf48b2b
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-04 01:30:15 -0700

    solution

--- today3/maximum-product-subarray.ts
+++ today3/maximum-product-subarray.ts
@@ -4,14 +4,20 @@
 export { maxProduct };
 
 function maxProduct(nums: number[]): number {
-  let max = -Infinity;
+  let max = nums.reduce((accum, curr) => Math.max(accum, curr));
+  let curMin = 1;
+  let curMax = 1;
 
-  for (let l = 0; l < nums.length; l++) {
-    let product = 1;
-    for (let r = l; r < nums.length; r++) {
-      product *= nums[r];
-      max = Math.max(max, product);
+  for (const n of nums) {
+    if (n === 0) {
+      curMin = 1;
+      curMax = 1;
     }
+
+    const tmp = curMax * n;
+    curMax = Math.max(curMin * n, curMax * n, n);
+    curMin = Math.min(curMin * n, tmp, n);
+    max = Math.max(max, curMax);
   }
 
   return max;
