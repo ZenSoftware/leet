495492a712792ae8ed44257e93ae7a994353e1ed
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-03 23:41:41 -0700

    solution

--- today3/evaluate-reverse-polish-notation.ts
+++ today3/evaluate-reverse-polish-notation.ts
@@ -4,5 +4,36 @@
 export { evalRPN };
 
 function evalRPN(tokens: string[]): number {
-  return -1;
+  const stack: number[] = [];
+
+  for (let i = 0; i < tokens.length; i++) {
+    if (tokens[i] === '+' || tokens[i] === '-' || tokens[i] === '*' || tokens[i] === '/') {
+      const right = stack.pop() as number;
+      const left = stack.pop() as number;
+      let result: number;
+
+      switch (tokens[i]) {
+        case '+':
+          result = left + right;
+          break;
+        case '-':
+          result = left - right;
+          break;
+        case '*':
+          result = left * right;
+          break;
+        case '/':
+          const divided = left / right;
+          if (divided < 0) result = Math.ceil(divided);
+          else result = Math.floor(divided);
+          break;
+      }
+      stack.push(result!);
+    } else {
+      const int = Number.parseInt(tokens[i]);
+      stack.push(int);
+    }
+  }
+
+  return stack[0];
 }
