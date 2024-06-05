48ec1f5bb7e8fbdee8382cebc1cbc0c0d39d1d26
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-04 21:27:45 -0700

    skeleton

--- csharp/Leet/FindPeakElement.cs
+++ csharp/Leet/FindPeakElement.cs
@@ -4,6 +4,11 @@ namespace Leet.FindPeakElement
     {
         public int FindPeakElement(int[] nums)
         {
+            return BinarySearch(nums, 0, nums.Length - 1);
+        }
+
+        public int BinarySearch(int[] nums, int start, int end)
+        {
             return -1;
         }
     }
