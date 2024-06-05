3e591e445cab9384820d169f89458fb978bac742
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-05 05:14:16 -0700

    skeleton

---
+++ today3/excel-sheet-column-title.spec.ts
@@ -0,0 +1,7 @@
+import { convertToTitle } from './excel-sheet-column-title';
+
+describe('Excel Sheet Column Title', () => {
+  it('evaluates correctly', () => {
+    expect(false).toEqual(true);
+  });
+});
---
+++ today3/excel-sheet-column-title.ts
@@ -0,0 +1,8 @@
+/**
+ * https://leetcode.com/problems/excel-sheet-column-title/description/
+ */
+export { convertToTitle };
+
+function convertToTitle(columnNumber: number): string {
+  return '';
+}
