8cd5f4d60444a8e3e3c7e8ecd05699fb738530c3
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-05 15:33:15 -0700

    solution

--- csharp/Leet/FactorialTrailingZeroes.cs
+++ csharp/Leet/FactorialTrailingZeroes.cs
@@ -2,30 +2,17 @@
 {
     public class Solution
     {
-        private Dictionary<long, long> _factorialCache = new Dictionary<long, long>();
-
         public int TrailingZeroes(int n)
         {
-            var factorial = Factorial(n).ToString();
-            if (factorial == "0") return 0;
-
+            var remaining = n;
+            var power = 1;
             var count = 0;
-            for (var i = factorial.Length - 1; i >= 0; i--)
+            while (Math.Pow(5, power) <= n)
             {
-                if (factorial[i] != '0') break;
-                count++;
+                count += (int)Math.Floor(remaining / Math.Pow(5, power));
+                power++;
             }
             return count;
-        }
-
-        public long Factorial(long n)
-        {
-            if (_factorialCache.ContainsKey(n)) return _factorialCache[n];
-            if (n == 1) return 1;
-            if (n == 0) return 0;
-            var result = Factorial(n - 1) * n;
-            _factorialCache.Add(n, result);
-            return result;
         }
     }
 }
