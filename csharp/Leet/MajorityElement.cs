ad31fca6d3c99920c3f8648d0f0dfa24eae39802
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-05 13:42:57 -0700

    solution

--- csharp/Leet/MajorityElement.cs
+++ csharp/Leet/MajorityElement.cs
@@ -4,7 +4,27 @@
     {
         public int MajorityElement(int[] nums)
         {
+            var count = 0;
+            var element = nums[0];
 
+            foreach (var n in nums)
+            {
+                if (element == n)
+                {
+                    count++;
+                }
+                else
+                {
+                    count--;
+                    if (count < 0)
+                    {
+                        element = n;
+                        count = 1;
+                    }
+                }
+            }
+
+            return element;
         }
     }
 }
--- csharp/LeetTests/MajorityElementTest.cs
+++ csharp/LeetTests/MajorityElementTest.cs
@@ -4,10 +4,12 @@ namespace LeetTests
 {
     internal class MajorityElementTest
     {
+        [Test]
         public void TestMajorityElement()
         {
             var solution = new Solution();
-            Assert.Pass();
+            Assert.That(solution.MajorityElement([3, 2, 3]), Is.EqualTo(3));
+            Assert.That(solution.MajorityElement([2, 2, 1, 1, 1, 2, 2]), Is.EqualTo(2));
         }
     }
 }
