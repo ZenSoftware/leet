f2d64a0ff4951fec96a24acb45513b2411076aa7
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-04 20:59:17 -0700

    solution

--- csharp/Leet/IntersectionOfTwoLinkedLists.cs
+++ csharp/Leet/IntersectionOfTwoLinkedLists.cs
@@ -6,7 +6,23 @@ namespace Leet.IntersectionOfTwoLinkedLists
     {
         public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
         {
-            return null;
+            var nodesA = new HashSet<ListNode>();
+
+            var pointer = headA;
+            while (pointer != null)
+            {
+                nodesA.Add(pointer);
+                pointer = pointer.next;
+            }
+
+            pointer = headB;
+            while (pointer != null)
+            {
+                if (nodesA.Contains(pointer)) break;
+                pointer = pointer.next;
+            }
+
+            return pointer;
         }
     }
 }
--- csharp/LeetTests/IntersectionOfTwoLinkedListsTest.cs
+++ csharp/LeetTests/IntersectionOfTwoLinkedListsTest.cs
@@ -7,9 +7,28 @@ namespace LeetTests
         [Test]
         public void ShouldPass()
         {
+            var rootA = new ListNode(4);
+            var nodeA2 = new ListNode(1);
+
+            var rootB = new ListNode(5);
+            var nodeB2 = new ListNode(6);
+            var nodeB3 = new ListNode(1);
+
+            var shared1 = new ListNode(8);
+            var shared2 = new ListNode(4);
+            var shared3 = new ListNode(5);
+
+            rootA.next = nodeA2;
+            rootB.next = nodeB2;
+            nodeB2.next = nodeB3;
+            nodeA2.next = shared1;
+            nodeB3.next = shared1;
+            shared1.next = shared2;
+            shared2.next = shared3;
+
             var solution = new Solution();
-            var node = solution.GetIntersectionNode(null, null);
-            //Assert.Equals(node, );
+            var result = solution.GetIntersectionNode(rootA, rootB);
+            Assert.That(result, Is.EqualTo(shared1));
         }
     }
 }
