3cf17742b968bc5609084c0e649091c1f51e7163
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-05 05:29:12 -0700

    solution

--- today3/excel-sheet-column-title.spec.ts
+++ today3/excel-sheet-column-title.spec.ts
@@ -2,6 +2,9 @@ import { convertToTitle } from './excel-sheet-column-title';
 
 describe('Excel Sheet Column Title', () => {
   it('evaluates correctly', () => {
-    expect(false).toEqual(true);
+    // expect(convertToTitle(1)).toEqual('A');
+    // expect(convertToTitle(28)).toEqual('AB');
+    // expect(convertToTitle(701)).toEqual('ZY');
+    expect(convertToTitle(52)).toEqual('AZ');
   });
 });
