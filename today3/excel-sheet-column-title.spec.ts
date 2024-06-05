53acc3ad8ad10748ecbf570f27627b495b2f3591
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-05 05:33:05 -0700

    tests

--- today3/excel-sheet-column-title.spec.ts
+++ today3/excel-sheet-column-title.spec.ts
@@ -2,9 +2,9 @@ import { convertToTitle } from './excel-sheet-column-title';
 
 describe('Excel Sheet Column Title', () => {
   it('evaluates correctly', () => {
-    // expect(convertToTitle(1)).toEqual('A');
-    // expect(convertToTitle(28)).toEqual('AB');
-    // expect(convertToTitle(701)).toEqual('ZY');
+    expect(convertToTitle(1)).toEqual('A');
+    expect(convertToTitle(28)).toEqual('AB');
+    expect(convertToTitle(701)).toEqual('ZY');
     expect(convertToTitle(52)).toEqual('AZ');
   });
 });
