6aa4236cff74372cc5d74d93c25ecfcd302cf06d
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-05 00:43:14 -0700

    skeleton

---
+++ csharp/Leet/CompareVersionNumbers.cs
@@ -0,0 +1,10 @@
+﻿namespace Leet.CompareVersionNumbers
+{
+    public class Solution
+    {
+        public int CompareVersion(string version1, string version2)
+        {
+            return -34;
+        }
+    }
+}
---
+++ csharp/LeetTests/CompareVersionNumbersTest.cs
@@ -0,0 +1,13 @@
+﻿using Leet.CompareVersionNumbers;
+
+namespace LeetTests
+{
+    public class CompareVersionNumbersTest
+    {
+        [Test]
+        public void TestCompareVersionNumbers()
+        {
+            Assert.Pass();
+        }
+    }
+}
