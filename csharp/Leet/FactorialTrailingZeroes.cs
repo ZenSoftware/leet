f96016eebe1192bf2efbc062ebc8ce29691c6852
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-05 14:13:14 -0700

    naive

---
+++ csharp/Leet/FactorialTrailingZeroes.cs
@@ -0,0 +1,31 @@
+﻿namespace Leet.FactorialTrailingZeroes
+{
+    public class Solution
+    {
+        private Dictionary<long, long> _factorialCache = new Dictionary<long, long>();
+
+        public int TrailingZeroes(int n)
+        {
+            var factorial = Factorial(n).ToString();
+            if (factorial == "0") return 0;
+
+            var count = 0;
+            for (var i = factorial.Length - 1; i >= 0; i--)
+            {
+                if (factorial[i] != '0') break;
+                count++;
+            }
+            return count;
+        }
+
+        public long Factorial(long n)
+        {
+            if (_factorialCache.ContainsKey(n)) return _factorialCache[n];
+            if (n == 1) return 1;
+            if (n == 0) return 0;
+            var result = Factorial(n - 1) * n;
+            _factorialCache.Add(n, result);
+            return result;
+        }
+    }
+}
---
+++ csharp/LeetTests/FactorialTrailingZeroesTest.cs
@@ -0,0 +1,29 @@
+﻿using Leet.FactorialTrailingZeroes;
+
+namespace LeetTests
+{
+    internal class FactorialTrailingZeroesTest
+    {
+        [Test]
+        public void TestTrailingZeroes()
+        {
+            var soltuion = new Solution();
+            Assert.That(soltuion.TrailingZeroes(3), Is.EqualTo(0));
+            Assert.That(soltuion.TrailingZeroes(5), Is.EqualTo(1));
+            Assert.That(soltuion.TrailingZeroes(0), Is.EqualTo(0));
+            Assert.That(soltuion.TrailingZeroes(13), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void TestFactorial()
+        {
+            var soltuion = new Solution();
+            Assert.That(soltuion.Factorial(13), Is.EqualTo(6227020800));
+            Assert.That(soltuion.Factorial(5), Is.EqualTo(120));
+            Assert.That(soltuion.Factorial(4), Is.EqualTo(24));
+            Assert.That(soltuion.Factorial(3), Is.EqualTo(6));
+            Assert.That(soltuion.Factorial(2), Is.EqualTo(2));
+            Assert.That(soltuion.Factorial(1), Is.EqualTo(1));
+        }
+    }
+}
