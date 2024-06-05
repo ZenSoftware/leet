bdb8114d4e257a403eae08fb32fb4ba212093a43
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-05 01:00:09 -0700

    solution

--- csharp/Leet/CompareVersionNumbers.cs
+++ csharp/Leet/CompareVersionNumbers.cs
@@ -4,7 +4,32 @@
     {
         public int CompareVersion(string version1, string version2)
         {
-            return -34;
+            var split1 = version1.Split('.');
+            var split2 = version2.Split('.');
+
+            var i = 0;
+            var j = 0;
+            while(i < split1.Length || j < split2.Length)
+            {
+                var v1 = 0;
+                if (i < split1.Length)
+                {
+                    v1 = Int32.Parse(split1[i]);
+                    i++;
+                }
+
+                var v2 = 0;
+                if (j < split2.Length)
+                {
+                    v2 = Int32.Parse(split2[j]);
+                    j++;
+                }
+
+                if (v1 < v2) return -1;
+                else if (v1 > v2) return 1;
+            }
+
+            return 0;
         }
     }
 }
--- csharp/LeetTests/CompareVersionNumbersTest.cs
+++ csharp/LeetTests/CompareVersionNumbersTest.cs
@@ -9,8 +9,9 @@ namespace LeetTests
         {
             var solution = new Solution();
             Assert.That(solution.CompareVersion("1.2", "1.10"), Is.EqualTo(-1));
-            Assert.That(solution.CompareVersion("1.01", "1.001"), Is.EqualTo(-1));
-            Assert.That(solution.CompareVersion("1.0", "1.0.0.0"), Is.EqualTo(-1));
+            Assert.That(solution.CompareVersion("1.01", "1.001"), Is.EqualTo(0));
+            Assert.That(solution.CompareVersion("1.0", "1.0.0.0"), Is.EqualTo(0));
+            Assert.That(solution.CompareVersion("1.0.0.1", "1.0"), Is.EqualTo(1));
         }
     }
 }
