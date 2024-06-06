14d4c7f0a371955a30ed42bea2e67ea2b020c16f
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-05 23:40:38 -0700

    close

--- csharp/Leet/LargestNumber.cs
+++ csharp/Leet/LargestNumber.cs
@@ -22,6 +22,7 @@ namespace Leet.LargestNumber
 
         public string[] MergeSort(string[] nums)
         {
+            if (nums.Length == 1) return nums;
             var mid = (Index)Math.Floor(nums.Length / 2d);
             var left = MergeSort(nums[0..mid]);
             var right = MergeSort(nums[mid..]);
@@ -35,10 +36,10 @@ namespace Leet.LargestNumber
 
             while (l < left.Length && r < right.Length)
             {
-                if (left[l] < right[r])
-                    result[i] = left[l++];
-                else
+                if (CompareStrings(left[l], right[r]))
                     result[i] = right[r++];
+                else
+                    result[i] = left[l++];
                 i++;
             }
 
@@ -55,6 +56,29 @@ namespace Leet.LargestNumber
             }
 
             return result;
+        }
+
+        public bool CompareStrings(string a, string b)
+        {
+            // if (a <= b)
+            int iA = 0, iB = 0;
+            while (iA != a.Length - 1 || iB != b.Length - 1)
+            {
+                if (a[iA].CompareTo(b[iB]) < 0)
+                    return true;
+                else if (a[iA].CompareTo(b[iB]) > 0)
+                    return false;
+
+                if (iA < a.Length - 1) iA++;
+                if (iB < b.Length - 1) iB++;
+            }
+
+            if (a[iA].CompareTo(b[iB]) < 0)
+                return true;
+            else if (a[iA].CompareTo(b[iB]) > 0)
+                return false;
+
+            return true;
         }
     }
 }
--- csharp/LeetTests/LargestNumberTest..cs
+++ csharp/LeetTests/LargestNumberTest..cs
@@ -4,19 +4,43 @@ namespace LeetTests
 {
     internal class LargestNumberTest
     {
-        //[Test]
-        //public void TestsLargestNumber()
-        //{
-        //    var solution = new Solution();
-        //    Assert.That(solution.LargestNumber([10, 2]), Is.EqualTo("210"));
-        //    Assert.That(solution.LargestNumber([3, 30, 34, 5, 9]), Is.EqualTo("9534330"));
-        //}
+        [Test]
+        public void TestsLargestNumber()
+        {
+            var solution = new Solution();
+            Assert.That(solution.LargestNumber([10, 2]), Is.EqualTo("210"));
+            Assert.That(solution.LargestNumber([3, 30, 34, 5, 9]), Is.EqualTo("9534330"));
+        }
 
         [Test]
         public void TestsMergeSort()
         {
             var solution = new Solution();
-            Assert.That(solution.MergeSort([5, 3, 1, 2, 4]), Is.EqualTo(new[] { 1, 2, 3, 4, 5 }));
+            Assert.That(
+                solution.MergeSort(["5", "3", "1", "2", "4"]),
+                Is.EqualTo(new[] { "1", "2", "3", "4", "5" })
+            );
+        }
+
+        [Test]
+        public void TestsStringCompare()
+        {
+            var solution = new Solution();
+            Assert.That(solution.CompareStrings("10", "2"), Is.EqualTo(true));
+
+            Assert.That(solution.CompareStrings("3", "4"), Is.EqualTo(true));
+            Assert.That(solution.CompareStrings("33", "34"), Is.EqualTo(true));
+
+            Assert.That(solution.CompareStrings("34", "33"), Is.EqualTo(false));
+            Assert.That(solution.CompareStrings("334", "33"), Is.EqualTo(false));
+
+            Assert.That(solution.CompareStrings("30", "3"), Is.EqualTo(true));
+            Assert.That(solution.CompareStrings("330", "33"), Is.EqualTo(true));
+
+            Assert.That(solution.CompareStrings("3", "30"), Is.EqualTo(false));
+            Assert.That(solution.CompareStrings("33", "330"), Is.EqualTo(false));
+
+            Assert.That(solution.CompareStrings("34323", "3432"), Is.EqualTo(true));
         }
     }
 }
