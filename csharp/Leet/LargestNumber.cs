94536ad17abaaef42b38b395c441ba81f8aeb739
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-05 21:29:45 -0700

    merge sort

--- csharp/Leet/LargestNumber.cs
+++ csharp/Leet/LargestNumber.cs
@@ -19,5 +19,43 @@ namespace Leet.LargestNumber
             }
             return result;
         }
+
+        public int[] MergeSort(int[] nums)
+        {
+            var mid = (Index)Math.Floor(nums.Length / 2d);
+            var left = MergeSort(nums[0..mid]);
+            var right = MergeSort(nums[mid..]);
+            return Merge(left, right);
+        }
+
+        public int[] Merge(int[] left, int[] right)
+        {
+            var result = new int[left.Length + right.Length];
+            var i = 0;
+            var l = 0;
+            var r = 0;
+            while (l < left.Length && r < right.Length)
+            {
+                if (left[l] < right[r])
+                    result[i] = left[l++];
+                else
+                    result[i] = right[r++];
+                i++;
+            }
+
+            while (l < left.Length)
+            {
+                result[i] = left[l++];
+                i++;
+            }
+
+            while (r < right.Length)
+            {
+                result[i] = right[r++];
+                i++;
+            }
+
+            return result;
+        }
     }
 }
--- csharp/LeetTests/LargestNumberTest..cs
+++ csharp/LeetTests/LargestNumberTest..cs
@@ -4,12 +4,19 @@ namespace LeetTests
 {
     internal class LargestNumberTest
     {
+        //[Test]
+        //public void TestsLargestNumber()
+        //{
+        //    var solution = new Solution();
+        //    Assert.That(solution.LargestNumber([10, 2]), Is.EqualTo("210"));
+        //    Assert.That(solution.LargestNumber([3, 30, 34, 5, 9]), Is.EqualTo("9534330"));
+        //}
+
         [Test]
-        public void TestsLargestNumber()
+        public void TestsMergeSort()
         {
             var solution = new Solution();
-            Assert.That(solution.LargestNumber([10, 2]), Is.EqualTo("210"));
-            Assert.That(solution.LargestNumber([3, 30, 34, 5, 9]), Is.EqualTo("9534330"));
+            Assert.That(solution.MergeSort([5, 3, 1, 2, 4]), Is.EqualTo(new[] { 1, 2, 3, 4, 5 }));
         }
     }
 }
