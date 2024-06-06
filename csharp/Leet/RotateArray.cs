074e4d3ceafccde8cac825feb4b7cbf007478006
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-06 03:25:55 -0700

    solution

---
+++ csharp/Leet/RotateArray.cs
@@ -0,0 +1,18 @@
+namespace Leet.RotateArray
+{
+    public class Solution
+    {
+        public void Rotate(int[] nums, int k)
+        {
+            k = k % nums.Length;
+            var left = nums[..^k];
+            var right = nums[(nums.Length - k)..];
+            var result = right.Concat(left).ToArray();
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                nums[i] = result[i];
+            }
+        }
+    }
+}
---
+++ csharp/LeetTests/RotateArrayTest.cs
@@ -0,0 +1,45 @@
+using Leet.RotateArray;
+
+namespace LeetTests
+{
+    internal class RotateArrayTest
+    {
+        [Test]
+        public void TestsRotateArray()
+        {
+            var solution = new Solution();
+            var input = new[] { 1, 2, 3, 4, 5, 6, 7 };
+            solution.Rotate(input, 3);
+            Assert.That(
+                input,
+                Is.EqualTo(new[] { 5, 6, 7, 1, 2, 3, 4 })
+            );
+        }
+
+        [Test]
+        public void TestsRotateArray2()
+        {
+            var solution = new Solution();
+            var input = new[] { -1, -100, 3, 99 };
+            solution.Rotate(input, 2);
+            Assert.That(
+                input,
+                Is.EqualTo(new[] { 3, 99, -1, -100 })
+            );
+        }
+
+        [Test]
+        public void TestsRotateArray3()
+        {
+            var solution = new Solution();
+            var input = new[] { 1, 2, 3, 4, 5 };
+            solution.Rotate(input, 1);
+            Assert.That(
+                input,
+                Is.EqualTo(new[] { 5, 1, 2, 3, 4 })
+            );
+        }
+
+
+    }
+}
