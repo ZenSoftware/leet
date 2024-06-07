c365735721f7cd4529e4019371863f0d9a1487e5
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-06 21:54:33 -0700

    solution

--- csharp/Leet/BinaryTreeRightSideView.cs
+++ csharp/Leet/BinaryTreeRightSideView.cs
@@ -4,9 +4,29 @@ namespace Leet.BinaryTreeRightSideView
 {
     public class Solution
     {
+        Dictionary<int, List<TreeNode>> levelOrder;
+
         public IList<int> RightSideView(TreeNode root)
         {
-            return null;
+            levelOrder = new Dictionary<int, List<TreeNode>>();
+            LevelOrder(root, 0);
+            var result = new List<int>();
+            foreach (var (level, nodes) in levelOrder)
+            {
+                result.Add(nodes[nodes.Count - 1].val);
+            }
+            return result;
+        }
+
+        public void LevelOrder(TreeNode root, int level)
+        {
+            if (root == null) return;
+
+            if (!levelOrder.ContainsKey(level)) levelOrder.Add(level, new List<TreeNode>());
+            levelOrder[level].Add(root);
+
+            LevelOrder(root.left, level + 1);
+            LevelOrder(root.right, level + 1);
         }
     }
 
--- csharp/LeetTests/BinaryTreeRightSideView.cs
+++ csharp/LeetTests/BinaryTreeRightSideView.cs
@@ -8,7 +8,19 @@ namespace LeetTests
         public void TestsBinaryTreeRightSideView()
         {
             var solution = new Solution();
-            Assert.Pass();
+            var n1 = new TreeNode(1);
+            var n2 = new TreeNode(2);
+            var n3 = new TreeNode(3);
+            var n4 = new TreeNode(4);
+            var n5 = new TreeNode(5);
+            n1.left = n2;
+            n1.right = n3;
+            n2.right = n5;
+            n3.right = n4;
+            Assert.That(
+                solution.RightSideView(n1),
+                Is.EqualTo(new[] { 1, 3, 4 })
+            );
         }
     }
 }
