986d4cf43679681a15fae53e7d89fd8690070ca1
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-06 18:30:15 -0700

    skeleton

---
+++ csharp/Leet/HouseRobber.cs
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/house-robber/description/
+namespace Leet.HouseRobber
+{
+    public class Solution
+    {
+
+    }
+}
---
+++ csharp/LeetTests/HouseRobberTest.cs
@@ -0,0 +1,14 @@
+using Leet.HouseRobber;
+
+namespace LeetTests
+{
+    internal class HouseRobberTest
+    {
+        [Test]
+        public void TestsHouseRobber()
+        {
+            var solution = new Solution();
+            Assert.Pass();
+        }
+    }
+}
