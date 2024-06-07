0b50a65dd38f387e1c9eec78658285ffea9cd3c8
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-06 20:05:07 -0700

    solution

---
+++ csharp/Leet/Review/HouseRobber.cs
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/house-robber/description/
+// NeetCode solution: https://www.youtube.com/watch?v=73r3KWiEvyk
+
+namespace Leet.HouseRobber
+{
+    public class Solution
+    {
+        public int Rob(int[] nums)
+        {
+            int rob1 = 0, rob2 = 0;
+            foreach (int n in nums)
+            {
+                var temp = Math.Max(n + rob1, rob2);
+                rob1 = rob2;
+                rob2 = temp;
+            }
+            return rob2;
+        }
+
+    }
+}
--- csharp/LeetTests/HouseRobberTest.cs
+++ csharp/LeetTests/HouseRobberTest.cs
@@ -20,6 +20,14 @@ namespace LeetTests
                 solution.Rob([2, 1, 1, 2]),
                 Is.EqualTo(4)
             );
+            Assert.That(
+                solution.Rob([1, 2, 1, 1]),
+                Is.EqualTo(3)
+            );
+            Assert.That(
+                solution.Rob([104, 209, 137, 52, 158, 67, 213, 86, 141, 110, 151, 127, 238, 147, 169, 138, 240, 185, 246, 225, 147, 203, 83, 83, 131, 227, 54, 78, 165, 180, 214, 151, 111, 161, 233, 147, 124, 143]),
+                Is.EqualTo(3176)
+            );
         }
     }
 }
