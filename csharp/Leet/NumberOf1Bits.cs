456adb0d000d001ae44f16398e488779974ebe97
Zen Software <ZenSoftware@users.noreply.github.com>
2024-06-06 18:18:29 -0700

    skeleton

---
+++ csharp/Leet/NumberOf1Bits.cs
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/number-of-1-bits/description/
+namespace Leet.NumberOf1Bits
+{
+    public class Solution
+    {
+        public int HammingWeight(int n)
+        {
+            return -1;
+        }
+    }
+}
---
+++ csharp/LeetTests/NumberOf1BitsTest.cs
@@ -0,0 +1,14 @@
+using Leet.NumberOf1Bits;
+
+namespace LeetTests
+{
+    internal class NumberOf1BitsTestTest
+    {
+        [Test]
+        public void TestsNumberOf1BitsTest()
+        {
+            var solution = new Solution();
+            Assert.Pass();
+        }
+    }
+}
