5884f910354076d5be1c1c4e1a82ded18ca914a3
Zen Software <ZenSoftware@users.noreply.github.com>
2025-01-26 04:50:52 -0700

    solution

---
+++ returned/best-time-to-buy-and-sell-stock-ii.spec.ts
@@ -0,0 +1,8 @@
+import { maxProfit } from './best-time-to-buy-and-sell-stock-ii';
+
+describe('Best Time to Buy and Sell Stock II', () => {
+  it('evaluates correctly', () => {
+    expect(maxProfit([7, 1, 5, 3, 6, 4])).toEqual(7);
+    expect(maxProfit([1, 2, 3, 4, 5])).toEqual(4);
+  });
+});
