0348b0dce14e24e4804eb202ee1c27b4a16888ac
Zen Software <ZenSoftware@users.noreply.github.com>
2025-01-27 23:14:07 -0700

    solution

---
+++ returned/zigzag-conversion.ts
@@ -0,0 +1,45 @@
+/**
+ * https://leetcode.com/problems/zigzag-conversion/?envType=study-plan-v2&envId=top-interview-150
+ * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ * And then read line by line: "PAHNAPLSIIGYIR"
+ * Write the code that will take a string and make this conversion given a number of rows:
+ */
+
+export { convert };
+
+function convert(s: string, numRows: number): string {
+  if (numRows === 1) return s;
+
+  let result = new Array(numRows).fill('');
+
+  let i = 0;
+  let n = 0;
+  let forward = true;
+
+  while (i < s.length) {
+    result[n] += s[i];
+
+    if (forward) {
+      if (n === numRows - 1) {
+        forward = false;
+        n--;
+      } else {
+        n++;
+      }
+    } else {
+      if (n === 0) {
+        forward = true;
+        n++;
+      } else {
+        n--;
+      }
+    }
+
+    i++;
+  }
+
+  return result.join('');
+}
