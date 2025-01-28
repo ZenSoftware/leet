bd1b02bf9bb93eb35b28321569a3bae1682519a8
Zen Software <ZenSoftware@users.noreply.github.com>
2025-01-27 21:14:28 -0700

    refactor

--- returned/integer-to-roman.ts
+++ returned/integer-to-roman.ts
@@ -6,49 +6,18 @@
 export { intToRoman };
 
 function intToRoman(num: number): string {
+  const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  const roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
   let result = '';
   let remainder = num;
 
   while (remainder > 0) {
-    if (remainder - 1000 >= 0) {
-      result += 'M';
-      remainder -= 1000;
-    } else if (remainder - 900 >= 0) {
-      result += 'CM';
-      remainder -= 900;
-    } else if (remainder - 500 >= 0) {
-      result += 'D';
-      remainder -= 500;
-    } else if (remainder - 400 >= 0) {
-      result += 'CD';
-      remainder -= 400;
-    } else if (remainder - 100 >= 0) {
-      result += 'C';
-      remainder -= 100;
-    } else if (remainder - 90 >= 0) {
-      result += 'XC';
-      remainder -= 90;
-    } else if (remainder - 50 >= 0) {
-      result += 'L';
-      remainder -= 50;
-    } else if (remainder - 40 >= 0) {
-      result += 'XL';
-      remainder -= 40;
-    } else if (remainder - 10 >= 0) {
-      result += 'X';
-      remainder -= 10;
-    } else if (remainder - 9 >= 0) {
-      result += 'IX';
-      remainder -= 9;
-    } else if (remainder - 5 >= 0) {
-      result += 'V';
-      remainder -= 5;
-    } else if (remainder - 4 >= 0) {
-      result += 'IV';
-      remainder -= 4;
-    } else if (remainder - 1 >= 0) {
-      result += 'I';
-      remainder -= 1;
+    for (let i = 0; i < roman.length; i++) {
+      if (remainder - values[i] >= 0) {
+        result += roman[i];
+        remainder -= values[i];
+        break;
+      }
     }
   }
 
