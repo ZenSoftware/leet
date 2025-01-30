c8c31272ff889d3ed7af41eb113bbe8a2ac932b6
Zen Software <ZenSoftware@users.noreply.github.com>
2025-01-30 03:50:12 -0700

    added tests

--- returned/python/add_two_numbers_test.py
+++ returned/python/add_two_numbers_test.py
@@ -9,3 +9,15 @@ def test2():
     l2 = to_nodes([5,6,4])
     result = Solution.addTwoNumbers(None, l1, l2)
     assert to_array(result) == [7,0,8]
+
+def test3():
+    l1 = to_nodes([0])
+    l2 = to_nodes([0])
+    result = Solution.addTwoNumbers(None, l1, l2)
+    assert to_array(result) == [0]
+
+def test4():
+    l1 = to_nodes([9,9,9,9,9,9,9])
+    l2 = to_nodes([9,9,9,9])
+    result = Solution.addTwoNumbers(None, l1, l2)
+    assert to_array(result) == [8,9,9,9,0,0,0,1]
