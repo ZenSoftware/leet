aa785aaab63dc6baf3f26ac4f01c246d1513003c
Zen Software <ZenSoftware@users.noreply.github.com>
2025-01-30 05:00:29 -0700

    refactor

--- returned/python/longest_substring_without_repeating_characters.py
+++ returned/python/longest_substring_without_repeating_characters.py
@@ -5,13 +5,13 @@ class Solution:
         maximum = 1
         i = 0
         while i <= len(s) - 1 - maximum:
-            cache = {s[i]: None}
+            cache = set(s[i])
             j = i+1
             while j <= len(s) - 1:
                 if s[j] in cache:
                     break
                 maximum = max(maximum, j-i+1)
-                cache[s[j]] = None
+                cache.add(s[j])
                 j += 1
             i += 1
         return maximum
