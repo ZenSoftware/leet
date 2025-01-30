112cd6851f1f7b1bb36e38bd1b4a92abf38ee65a
Zen Software <ZenSoftware@users.noreply.github.com>
2025-01-30 14:18:57 -0700

    alternative

--- returned/python/reverse_integer.py
+++ returned/python/reverse_integer.py
@@ -1,16 +1,15 @@
 # https://leetcode.com/problems/reverse-integer/description/
-
 class Solution:
     def reverse(self, x: int) -> int:
         if(x < 0):
-            y = int('-' + str(x)[1:][::-1])
-            if y < (-2)**31:
+            res = int('-' + str(x)[1:][::-1])
+            if res < (-2)**31:
                 return 0
             else:
-                return y
+                return res
         else:
-            y = int(str(x)[::-1])
-            if y > 2**31 - 1:
+            res = int(str(x)[::-1])
+            if res > 2**31 - 1:
                 return 0
             else:
-                return y
+                return res
