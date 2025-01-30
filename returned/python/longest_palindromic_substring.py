fa494e12194df257f8535fcaac13ffaf34393d49
Zen Software <ZenSoftware@users.noreply.github.com>
2025-01-30 13:22:33 -0700

    solution

--- returned/python/longest_palindromic_substring.py
+++ returned/python/longest_palindromic_substring.py
@@ -4,22 +4,16 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         longest = ''
 
-        for i in range(len(s)):
-            # odd length
-            l, r = i, i
+        def calc_pal(l: int, r: int):
+            nonlocal longest
             while l >= 0 and r < len(s) and s[l] == s[r]:
                 if(r-l+1 > len(longest)):
-                    longest = s[l:r+1]
+                   longest = s[l:r+1]
                 l -= 1
                 r += 1
 
-            # even length
-            l, r = i, i+1
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if(r-l+1 > len(longest)):
-                    longest = s[l:r+1]
-                l -= 1
-                r += 1
+        for i in range(len(s)):
+            calc_pal(i,i)   # odd length
+            calc_pal(i,i+1) # even length
 
         return longest
-
