cefaa6427f8d804051a887af68adf40815cae5d6
Zen Software <ZenSoftware@users.noreply.github.com>
2025-01-30 13:16:54 -0700

    solution

--- returned/python/longest_palindromic_substring.py
+++ returned/python/longest_palindromic_substring.py
@@ -3,24 +3,21 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         longest = ''
-        longest_len = 0
 
         for i in range(len(s)):
             # odd length
             l, r = i, i
             while l >= 0 and r < len(s) and s[l] == s[r]:
-                if(r-l+1 > longest_len):
+                if(r-l+1 > len(longest)):
                     longest = s[l:r+1]
-                    longest_len = r-l+1
                 l -= 1
                 r += 1
 
             # even length
             l, r = i, i+1
             while l >= 0 and r < len(s) and s[l] == s[r]:
-                if(r-l+1 > longest_len):
+                if(r-l+1 > len(longest)):
                     longest = s[l:r+1]
-                    longest_len = r-l+1
                 l -= 1
                 r += 1
 
