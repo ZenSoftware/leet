66e73eebe360861bf3ebcc46b4cc5ceb4e6850cc
Zen Software <ZenSoftware@users.noreply.github.com>
2025-02-01 08:31:19 -0700

    solution

---
+++ returned/python/search_insert_position.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/search-insert-position/description/
+from typing import List
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if target == nums[mid]:
+                return mid
+            
+            if target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        return left
---
+++ returned/python/search_insert_position_test.py
@@ -0,0 +1,16 @@
+from search_insert_position import Solution
+
+def test1():
+    assert Solution.searchInsert(None, [1,3,5,6], 5) == 2
+
+def test2():
+    assert Solution.searchInsert(None, [1,3,5,6], 2) == 1
+    
+def test3():
+    assert Solution.searchInsert(None, [1,3,5,6], 7) == 4
+    
+def test4():
+    assert Solution.searchInsert(None, [1], 1) == 0
+
+def test5():
+    assert Solution.searchInsert(None, [1,3], 2) == 1
