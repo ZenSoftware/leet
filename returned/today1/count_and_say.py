12ceaee683284c8f4e45208f6d8f1ece3b02a9b8
Zen Software <ZenSoftware@users.noreply.github.com>
2025-02-01 19:40:00 -0700

    solution

---
+++ returned/today1/count_and_say.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/count-and-say/description/
+
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        def rle(chars: str, s: int) -> str:
+            if s == n:
+                return chars
+
+            output = ''
+            current = chars[0]
+            count = 0
+            for c in chars:
+                if c == current:
+                    count += 1
+                else:
+                    output += str(count) + current
+                    current = c
+                    count = 1
+            output += str(count) + current
+
+            return rle(output, s+1)
+        return rle('1',1)
---
+++ returned/today1/count_and_say_test.py
@@ -0,0 +1,7 @@
+from count_and_say import Solution
+
+def test1():
+    assert Solution.countAndSay(None, 4) == '1211'
+
+def test2():
+    assert Solution.countAndSay(None, 1) == '1'
