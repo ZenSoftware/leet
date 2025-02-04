3173817fa94e440c9fbac1264b3a4ea91e46aefa
Zen Software <ZenSoftware@users.noreply.github.com>
2025-02-03 20:36:51 -0700

    solution

--- returned/python/today3/rotate_image.py
+++ returned/python/today3/rotate_image.py
@@ -3,27 +3,15 @@ from typing import List
 
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-        top = 0
-        bottom = len(matrix) - 1
-        left = 0
-        right = bottom
+        l, r = 0, len(matrix) - 1
 
-        while left < right and top < bottom:
-            i = left
-            while i < right:
-                tmp_right = matrix[top+i][right]
-                matrix[top+i][right] = matrix[top][left+i]
-
-                tmp_bottom = matrix[bottom][right-i]
-                matrix[bottom][right-i] = tmp_right
-
-                tmp_left = matrix[bottom-i][left]
-                matrix[bottom-i][left] = tmp_bottom
-
-                matrix[top][left+i] = tmp_left
-
-                i += 1
-            left += 1
-            right -= 1
-            top += 1
-            bottom -= 1
+        while l < r:
+            for i in range(r-l):
+                t, b = l, r
+                topLeft = matrix[t][l+i]
+                matrix[t][l+i] = matrix[b-i][l]
+                matrix[b-i][l] = matrix[b][r-i]
+                matrix[b][r-i] = matrix[t+i][r]
+                matrix[t+i][r] = topLeft
+            l += 1
+            r -= 1
