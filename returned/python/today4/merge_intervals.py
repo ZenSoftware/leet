bcad400d7977e422779411e6f85b0643c04c4779
Zen Software <ZenSoftware@users.noreply.github.com>
2025-02-04 17:33:58 -0700

    solution

--- returned/python/today4/merge_intervals.py
+++ returned/python/today4/merge_intervals.py
@@ -3,17 +3,10 @@ from typing import List
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort()
         result: List[List[int]] = []
-        prev_min = intervals[0][0]
-        prev_max = intervals[0][1]
-        for interval in intervals:
-            if prev_max >= interval[0] and prev_min <= interval[1]:
-                prev_min = min(prev_min, interval[0])
-                prev_max = max(prev_max, interval[1])
+        for interval in sorted(intervals):
+            if not result or result[-1][1] < interval[0]:
+                result.append(interval)
             else:
-                result.append([prev_min, prev_max])
-                prev_min = interval[0]
-                prev_max = interval[1]
-        result.append([prev_min, prev_max])
+                result[-1][1] = max(result[-1][1], interval[1])
         return result
