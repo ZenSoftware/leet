0924e6c9a1d0552f1c5c12ff4775efffa7533442
Zen Software <ZenSoftware@users.noreply.github.com>
2025-02-04 20:55:15 -0700

    memoized

--- returned/python/today4/unique_paths.py
+++ returned/python/today4/unique_paths.py
@@ -1,16 +1,23 @@
 # https://leetcode.com/problems/unique-paths/description/
+from typing import Dict
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        count = 0
-        def dfs(x: int, y: int) -> None:
-            if x == m - 1 and y == n -1:
-                nonlocal count
-                count += 1
-                return
-            if x > m - 1 or y > n - 1:
-                return
-            dfs(x, y+1)
-            dfs(x+1, y)
-        dfs(0,0)
-        return count
+        memo: Dict[str,int] = {}
+        
+        def dfs(x: int, y: int) -> int:
+            if x == m-1 and y == n-1:
+                return 1
+            if x > m-1 or y > n-1:
+                return 0
+            
+            key = str(x) + '-' + str(y)
+            if key in memo:
+                return memo[key]
+
+            count = dfs(x, y+1)
+            count += dfs(x+1, y)
+
+            memo[key] = count
+            return count
+        return dfs(0,0)
