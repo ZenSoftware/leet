687c0e7ba182e303af3325cf870a70519656a90e
Zen Software <ZenSoftware@users.noreply.github.com>
2025-02-06 20:23:03 -0700

    refactor

--- returned/python/today6/largest_rectangle_in_histogram.py
+++ returned/python/today6/largest_rectangle_in_histogram.py
@@ -11,14 +11,14 @@ class Solution:
                 stack.append((i, h))
             else:
                 while stack and stack[-1][1] > h:
-                    (j, height) = stack.pop()
+                    j, height = stack.pop()
                     area = (i - j) * height
                     if area > largest:
                         largest = area
                 stack.append((j, h))
  
         while stack:
-            (j, height) = stack.pop()
+            j, height = stack.pop()
             area = (len(heights) - j) * height
             if area > largest:
                 largest = area
