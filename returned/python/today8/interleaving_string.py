e9989078a968275f3736fdcaf8d39d60457d0107
Zen Software <ZenSoftware@users.noreply.github.com>
2025-02-08 18:15:30 -0700

    memoized

--- returned/python/today8/interleaving_string.py
+++ returned/python/today8/interleaving_string.py
@@ -5,9 +5,13 @@ class Solution:
         if len(s1) + len(s2) != len(s3):
             return False
 
+        memo = {}
         def dfs(i1: int, i2: int, i3: int) -> bool:
-            if i1 == len(s1) and i2 == len(s2) and i3 == len(s3):
+            if i1 == len(s1) and i2 == len(s2):
                 return True
+            
+            if (i1, i2) in memo:
+                return memo[(i1, i2)]
 
             r1 = False
             if i1 < len(s1) and s1[i1] == s3[i3]:
@@ -17,6 +21,7 @@ class Solution:
             if i2 < len(s2) and s2[i2] == s3[i3]:
                 r2 = dfs(i1, i2+1, i3+1)
             
+            memo[(i1, i2)] = r1 or r2
             return r1 or r2
 
         return dfs(0,0,0)
