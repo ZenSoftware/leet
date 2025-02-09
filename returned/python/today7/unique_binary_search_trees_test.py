835c6b12acce28fabb1ddd78ba40063f87bbf867
Zen Software <ZenSoftware@users.noreply.github.com>
2025-02-08 17:56:44 -0700

    solution

---
+++ returned/python/today7/unique_binary_search_trees_test.py
@@ -0,0 +1,6 @@
+from unique_binary_search_trees import Solution
+
+def test1():
+    assert Solution().numTrees(1) == 1
+    assert Solution().numTrees(2) == 2
+    assert Solution().numTrees(3) == 5
