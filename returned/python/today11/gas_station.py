d0f176d240856dd38d103266352775fe9d31d3c2
Zen Software <ZenSoftware@users.noreply.github.com>
2025-02-11 16:10:06 -0700

    solution

--- returned/python/today11/gas_station.py
+++ returned/python/today11/gas_station.py
@@ -3,17 +3,15 @@ from typing import List
 
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        if sum(gas) >= sum(cost):
+        if sum(gas) < sum(cost):
             return -1
         
-        for i in range(len(gas) - 1):
-            tank = gas[i]
-            for j in range(i, len(gas) - 1):
-                if tank - cost[j] >= 0:
-                    tank = tank - cost[j] + gas[j+1]
-                else:
-                    break
-                if j == len(gas) - 2:
-                    return i
+        total = 0
+        start = 0
+        for i in range(len(gas)):
+            total += (gas[i] - cost[i])
+            if total < 0:
+                total = 0
+                start = i+1
         
-        return len(gas) - 1
+        return start
