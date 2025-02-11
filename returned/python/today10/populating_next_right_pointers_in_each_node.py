e58aca1cdeb3a63a7db043a8d306cb7c55cd649b
Zen Software <ZenSoftware@users.noreply.github.com>
2025-02-10 17:59:47 -0700

    solution

---
+++ returned/python/today10/populating_next_right_pointers_in_each_node.py
@@ -0,0 +1,29 @@
+from collections import deque
+from typing import Optional
+
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root:
+            return None
+        
+        stack = deque([root])
+        while stack:
+            level_size = len(stack)
+
+            for i in range(level_size - 1):
+                stack[i].next = stack[i+1]
+
+            for _ in range(level_size):
+                node = stack.popleft()
+                if node.left:
+                    stack.append(node.left)
+                if node.right:
+                    stack.append(node.right)
+        return root
