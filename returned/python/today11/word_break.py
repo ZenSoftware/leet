286f28eaef7bd2a742832e3e7b847bae1cfeacad
Zen Software <ZenSoftware@users.noreply.github.com>
2025-02-11 18:46:45 -0700

    solution

---
+++ returned/python/today11/word_break.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/word-break/description/
+from typing import List
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        memo = {}
+
+        def dfs(l: int, r: int) -> bool:
+            if l == len(s) and r == len(s):
+                return True
+            if r >= len(s):
+                return False
+            if (l,r) in memo:
+                return memo[(l,r)]
+        
+            segment = s[l:r+1]
+            
+            result = False
+            if segment in wordDict:
+                result = dfs(r+1, r+1)
+
+            memo[(l,r)] = result or dfs(l, r+1)
+            return memo[(l,r)]
+
+        return dfs(0,0)
