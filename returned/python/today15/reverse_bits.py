28ce0fe4bb361cf45b132ba875c89d31cad36d4f
Zen Software <ZenSoftware@users.noreply.github.com>
2025-02-16 15:28:22 -0700

    solution

---
+++ returned/python/today15/reverse_bits.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/reverse-bits/
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        result = 0
+        for i in range(32):
+            place = 1 << i
+            digit = n & place
+            if i <= 15:
+                shifted = digit << (31-i*2)
+            else:
+                shifted = digit >> (i-15)*2-1
+            result = result | shifted
+            print(format(result, '032b'))
+        return result
