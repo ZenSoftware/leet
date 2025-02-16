5e1f68d4919824e251790d4e78b6496a94d2b5fe
Zen Software <ZenSoftware@users.noreply.github.com>
2025-02-16 15:35:01 -0700

    cleaner

--- returned/python/today15/reverse_bits.py
+++ returned/python/today15/reverse_bits.py
@@ -4,11 +4,7 @@ class Solution:
     def reverseBits(self, n: int) -> int:
         result = 0
         for i in range(32):
-            place = 1 << i
-            digit = n & place
-            if i <= 15:
-                shifted = digit << (31-i*2)
-            else:
-                shifted = digit >> (i-15)*2-1
-            result = result | shifted
+            bit = n & 1
+            result = (result << 1) | bit
+            n = n >> 1
         return result
