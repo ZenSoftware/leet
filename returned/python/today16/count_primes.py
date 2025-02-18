df3766eb334f0cf1c12cd23ac4375e8f2e89316f
Zen Software <ZenSoftware@users.noreply.github.com>
2025-02-17 20:14:54 -0700

    solution

--- returned/python/today16/count_primes.py
+++ returned/python/today16/count_primes.py
@@ -2,20 +2,12 @@
 
 class Solution:
     def countPrimes(self, n: int) -> int:
-        count = 0
-
-        for x in range(2,n):
-            if self.isPrime(x):
-                count += 1
-        
-        return count
-
-    def isPrime(self, n: int) -> bool:
         if n < 2:
-            return False
-        
-        for divisor in range(2, int(n**0.5)+1):
-            if n % divisor == 0:
-                return False
-            
-        return True
+            return 0
+
+        primes = [True] * n
+        primes[0], primes[1] = False, False
+        for p in range(2, int(n**0.5)+1):
+            for i in range(p**2, n, p):
+                primes[i] = False
+        return sum(primes)
