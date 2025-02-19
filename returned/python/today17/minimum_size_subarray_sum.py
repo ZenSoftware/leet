1f3c84772561c7bb0514d82c8c86183c77f625e1
Zen Software <ZenSoftware@users.noreply.github.com>
2025-02-18 23:10:49 -0700

    refactor

--- returned/python/today17/minimum_size_subarray_sum.py
+++ returned/python/today17/minimum_size_subarray_sum.py
@@ -3,15 +3,15 @@ from typing import List
 
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        min_len = float('inf')
+        ans = float('inf')
         total = 0
         l = 0
-        
+
         for r, num in enumerate(nums):
             total += num
             while total >= target:
-                min_len = min(min_len, r-l+1)
+                ans = min(ans, r-l+1)
                 total -= nums[l]
                 l += 1
 
-        return 0 if min_len == float('inf') else min_len
+        return 0 if ans == float('inf') else ans
