4f910966c87d597d74995272c468b681e52b069d
Zen Software <ZenSoftware@users.noreply.github.com>
2025-02-19 02:30:40 -0700

    solution

---
+++ returned/typescript-types/00018-easy-tuple-length.ts
@@ -0,0 +1,11 @@
+{
+  type tesla = ['tesla', 'model 3', 'model X', 'model Y'];
+  type spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT'];
+  const tesla2 = ['tesla', 'model 3', 'model X', 'model Y'] as const;
+
+  type Length<T extends readonly unknown[]> = T['length'];
+
+  type teslaLength = Length<tesla>; // expected 4
+  type spaceXLength = Length<spaceX>; // expected 5
+  type tesla2Length = Length<typeof tesla2>; // expected 5
+}
