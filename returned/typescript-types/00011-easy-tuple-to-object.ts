a8b065af2909b6c0ed62d1d69349859d75fe34cc
Zen Software <ZenSoftware@users.noreply.github.com>
2025-02-19 02:00:09 -0700

    renamed

--- returned/typescript-types/00011-easy-tuple-to-object.ts
+++ returned/typescript-types/00011-easy-tuple-to-object.ts
@@ -2,7 +2,7 @@
   const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const;
 
   type TupleToObject<T extends readonly (keyof any)[]> = {
-    [P in T[number]]: P;
+    [Key in T[number]]: Key;
   };
 
   type result = TupleToObject<typeof tuple>; // expected { 'tesla': 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
