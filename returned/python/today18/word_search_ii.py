deae120ed0fe8b5966cd783ebae1b296cfc552dd
Zen Software <ZenSoftware@users.noreply.github.com>
2025-02-20 07:46:13 -0700

    brute

---
+++ returned/python/today18/word_search_ii.py
@@ -0,0 +1,42 @@
+# https://leetcode.com/problems/word-search-ii/
+from typing import List
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        rows = len(board)
+        cols = len(board[0])
+        def exists(r: int, c: int, word: str, i: int, visited: set) -> bool:
+            if r not in range(rows):
+                return False
+            if c not in range(cols):
+                return False
+            if (r,c) in visited:
+                return False
+            if word[i] != board[r][c]:
+                return False
+            if i == len(word) - 1:
+                return True
+            
+            visited.add((r,c))
+
+            up =    exists(r-1, c, word, i+1, visited)
+            down =  exists(r+1, c, word, i+1, visited)
+            left =  exists(r, c-1, word, i+1, visited)
+            right = exists(r, c+1, word, i+1, visited)
+            
+            visited.remove((r,c))
+
+            return right or left or up or down
+        
+        result = []
+        def check_exists(word):
+            for r in range(rows):
+                for c in range(cols):
+                    if exists(r, c, word, 0, set()):
+                        result.append(word)
+                        return
+                    
+        for word in words:
+            check_exists(word)
+                        
+        return result
---
+++ returned/python/today18/word_search_ii_test.py
@@ -0,0 +1,21 @@
+from word_search_ii import Solution
+
+def test1():
+    board = [["o","a","a","n"],["e","t","a","e"],["i","h","k","r"]]
+    words = ["oath","pea","eat","rain"]
+    assert Solution().findWords(board, words) == ['oath', 'eat']
+
+def test2():
+    board = [["a","b"],["c","d"]]
+    words = ["abcb"]
+    assert Solution().findWords(board, words) == []
+
+def test3():
+    board = [["a","b","c"],["a","e","d"],["a","f","g"]]
+    words = ["eaabcdgfa"]
+    assert Solution().findWords(board, words) == ["eaabcdgfa"]
+
+def test4():
+    board = [["a","b"],["a","a"]]
+    words = ["bab"]
+    assert Solution().findWords(board, words) == []
