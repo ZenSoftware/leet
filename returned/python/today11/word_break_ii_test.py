7568c38fe823a6d361166c71f57f8821d7a66c2f
Zen Software <ZenSoftware@users.noreply.github.com>
2025-02-19 21:25:43 -0700

    fixed tests

--- returned/python/today11/word_break_ii_test.py
+++ returned/python/today11/word_break_ii_test.py
@@ -2,11 +2,11 @@ from word_break_ii import Solution
 
 def test1():
     result = Solution().wordBreak("catsanddog", ["cat","cats","and","sand","dog"])
-    assert result == ["cats and dog","cat sand dog"]
-
+    assert result == ['cat sand dog','cats and dog']
+                     
 def test2():
     result = Solution().wordBreak("pineapplepenapple", ["apple","pen","applepen","pine","pineapple"])
-    assert result ==["pine apple pen apple","pineapple pen apple","pine applepen apple"]
+    assert result == ['pine apple pen apple', 'pine applepen apple', 'pineapple pen apple']
 
 def test3():
     result = Solution().wordBreak("catsandog", ["cats","dog","sand","and","cat"])
