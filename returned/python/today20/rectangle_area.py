d6b31ce30443e3ef06260cd3b9ea5cb8801ad2be
Zen Software <ZenSoftware@users.noreply.github.com>
2025-02-21 23:04:28 -0700

    solution

--- returned/python/today20/rectangle_area.py
+++ returned/python/today20/rectangle_area.py
@@ -4,9 +4,11 @@ class Solution:
     def computeArea(self, ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int) -> int:
         intersection_x = min(ax2, bx2) - max(ax1, bx1)
         intersection_y = min(ay2, by2) - max(ay1, by1)
-        intersection_area = intersection_x * intersection_y
-        intersection_area = max(0, intersection_area)
-
+        if intersection_x <= 0 or intersection_y <= 0:
+            intersection_area = 0
+        else:
+            intersection_area = intersection_x * intersection_y
+        
         def area(cx1, cy1, cx2, cy2):
             return (cx2 - cx1) * (cy2 - cy1)
         
