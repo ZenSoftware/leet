1613d109d5ba3d78441ff40372cd3bd856de78f9
Zen Software <ZenSoftware@users.noreply.github.com>
2025-02-22 21:43:55 -0700

    solution

---
+++ returned/python/today21/palindrome_linked_list_const_space.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/palindrome-linked-list/
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        self.curr = head
+        return self.solve(head)
+
+    def solve(self, head: Optional[ListNode]) -> bool:
+        if not head:
+            return True
+        ans = self.solve(head.next) and self.curr.val == head.val
+        self.curr = self.curr.next
+        return ans
---
+++ returned/python/today21/palindrome_linked_list_const_space_test.py
@@ -0,0 +1,35 @@
+from palindrome_linked_list_const_space import Solution, ListNode
+from typing import List, Optional
+
+def to_nodes(elements: List[int]) -> Optional[ListNode]:
+    if not elements:
+        return None
+    prev = None
+    for e in reversed(elements):
+        node = ListNode(e)
+        node.next = prev
+        prev = node
+    return prev
+
+def to_array(head: Optional[ListNode]) -> List[int]:
+    if not head:
+        return []
+    result = []
+    while head:
+        result.append(head.val)
+        head = head.next
+    return result
+
+def test1():
+    nodes = to_nodes([1,2,3,4,5])
+    assert to_array(nodes) == [1,2,3,4,5]
+    nodes = to_nodes([])
+    assert to_array(nodes) == []
+
+def test2():
+    nodes = to_nodes([1,2,2,1])
+    assert Solution().isPalindrome(nodes) == True
+
+def test3():
+    nodes = to_nodes([1,2])
+    assert Solution().isPalindrome(nodes) == False
