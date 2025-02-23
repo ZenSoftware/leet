22ccf8f7babcf8d31e82fd37693d58d9677460f8
Zen Software <ZenSoftware@users.noreply.github.com>
2025-02-23 08:49:03 -0700

    refactor

--- returned/python/today22/valid_anagram_array_count.py
+++ returned/python/today22/valid_anagram_array_count.py
@@ -3,10 +3,13 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         ascii_a = ord('a')
+        
         count_s = [0] * 26
-        count_t = [0] * 26
         for c in s:
             count_s[ord(c)-ascii_a] += 1
+        
+        count_t = [0] * 26
         for c in t:
             count_t[ord(c)-ascii_a] += 1
+        
         return count_s == count_t
