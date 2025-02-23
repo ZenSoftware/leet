0f045ebadb1147bd8bca85a6c6752aaf0b585a34
Zen Software <ZenSoftware@users.noreply.github.com>
2025-02-22 17:39:56 -0700

    refactor

--- returned/python/today21/kth_smallest_element_in_a_bst.py
+++ returned/python/today21/kth_smallest_element_in_a_bst.py
@@ -11,8 +11,7 @@ class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         result = []
         def dfs(node: Optional[TreeNode]):
-            if not node:
-                return
+            if not node: return
             dfs(node.left)
             result.append(node.val)
             dfs(node.right)
