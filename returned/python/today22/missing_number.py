85c0788e376e5bdd52df41a8253bca0706cfe0e7
Zen Software <ZenSoftware@users.noreply.github.com>
2025-02-23 22:53:03 -0700

    refactor

--- returned/python/today22/missing_number.py
+++ returned/python/today22/missing_number.py
@@ -4,7 +4,7 @@ from typing import List
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         rollcall = [False] * (len(nums)+1)
-        
+
         for n in nums:
             rollcall[n] = True
 
