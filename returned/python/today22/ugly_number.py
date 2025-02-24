09b5e563e7cf5fe9d579f01ac266ee823d58ed66
Zen Software <ZenSoftware@users.noreply.github.com>
2025-02-23 21:42:06 -0700

    solution

--- returned/python/today22/ugly_number.py
+++ returned/python/today22/ugly_number.py
@@ -5,14 +5,8 @@ class Solution:
         if n <= 0:
             return False
         
-        while n != 1:
-            if n % 2 == 0:
-                n //= 2
-            elif n % 3 == 0:
-                n //= 3
-            elif n % 5 == 0:
-                n //= 5
-            else:
-                break
-        
+        for p in [2, 3, 5]:
+            while n % p == 0:
+                n //= p
+
         return n == 1
