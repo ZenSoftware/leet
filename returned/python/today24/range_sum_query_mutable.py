3f7ea86b8b9a6ce8779f3e58bc9f56de3c5b1491
Zen Software <ZenSoftware@users.noreply.github.com>
2025-02-26 21:30:48 -0700

    solution

--- returned/python/today24/range_sum_query_mutable.py
+++ returned/python/today24/range_sum_query_mutable.py
@@ -1,19 +1,38 @@
 # https://leetcode.com/problems/range-sum-query-mutable/description/
+# https://www.youtube.com/watch?v=T_xe9p7-zwQ
+# https://www.youtube.com/watch?v=uSFzHCZ4E-8
 from typing import List
+
+class BinaryIndexTree:
+    def __init__(self, arr: List[int]):
+        self.tree = [0] * (len(arr) + 1)
+        for i in range(len(arr)):
+            self.update(i, arr[i])
+        
+    def update(self, i: int, val: int):
+        i += 1
+        while i < len(self.tree):
+            self.tree[i] += val
+            i += i & -i
+    
+    def get_sum(self, i: int):
+        i += 1
+        result = 0
+        while i > 0:
+            result += self.tree[i]
+            i -= i & -i
+        return result
 
 class NumArray:
 
     def __init__(self, nums: List[int]):
         self.nums = nums
-        self.prefix = [0]
-        for i in range(len(nums)):
-            self.prefix.append(nums[i] + self.prefix[i])
+        self.bit = BinaryIndexTree(nums)
 
     def update(self, index: int, val: int) -> None:
-        if self.nums[index] != val:
-            self.nums[index] = val
-            for i in range(index, len(self.nums)):
-                self.prefix[i+1] = self.nums[i] + self.prefix[i]
+        delta = val - self.nums[index]
+        self.nums[index] = val
+        self.bit.update(index, delta)
 
     def sumRange(self, left: int, right: int) -> int:
-        return self.prefix[right+1] - self.prefix[left]
+        return self.bit.get_sum(right) - self.bit.get_sum(left-1)
--- returned/python/today24/range_sum_query_mutable_test.py
+++ returned/python/today24/range_sum_query_mutable_test.py
@@ -1,4 +1,19 @@
-from range_sum_query_mutable import NumArray
+from range_sum_query_mutable import NumArray, BinaryIndexTree
+
+def test_bit():
+    bit = BinaryIndexTree([1,2,3,4,5])
+    assert bit.get_sum(0) == 1
+    assert bit.get_sum(1) == 3
+    assert bit.get_sum(2) == 6
+    assert bit.get_sum(3) == 10
+    assert bit.get_sum(4) == 15
+
+    bit.update(0,1)
+    assert bit.get_sum(0) == 2
+    assert bit.get_sum(1) == 4
+    assert bit.get_sum(2) == 7
+    assert bit.get_sum(3) == 11
+    assert bit.get_sum(4) == 16
 
 def test1():
     numArray = NumArray([1, 3, 5])
