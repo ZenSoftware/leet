e0fbf503362ef0652e3a9ceccf57435a4ac6a5be
Zen Software <ZenSoftware@users.noreply.github.com>
2025-02-27 00:48:54 -0700

    brute force

---
+++ returned/python/today24/minimum_height_trees.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/minimum-height-trees/
+from typing import List
+from collections import defaultdict, deque
+
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        tree = defaultdict(set)
+        for a, b in edges:
+            tree[a].add(b)
+            tree[b].add(a)
+        
+        heights = defaultdict(lambda: [])
+        
+        for i in range(n):
+            visited = set([i])
+            queue = deque([i])
+            height = 1
+            while queue:
+                for _ in range(len(queue)):
+                    node = queue.popleft()
+                    adjacent = tree[node].difference(visited)
+                    queue.extend(adjacent)
+                    visited.update(tree[node])
+                height += 1
+            heights[height].append(i)
+
+        return heights[min(heights)]
---
+++ returned/python/today24/minimum_height_trees_test.py
@@ -0,0 +1,7 @@
+from minimum_height_trees import Solution
+
+def test1():
+    assert Solution().findMinHeightTrees(4, [[1,0],[1,2],[1,3]]) == [1]
+
+def test2():
+    assert Solution().findMinHeightTrees(6, [[3,0],[3,1],[3,2],[3,4],[5,4]]) == [3,4]
