64939fa64d43103411444037aa0e994616115a62
Zen Software <ZenSoftware@users.noreply.github.com>
2025-02-28 01:23:31 -0700

    reversed

--- returned/python/today24/count_of_smaller_numbers_after_self.py
+++ returned/python/today24/count_of_smaller_numbers_after_self.py
@@ -4,20 +4,11 @@ from typing import List
 
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
-        counts = []
-        sorted_nums = []
-        for n in reversed(nums):
-            index = bisect_left(sorted_nums, n)
-            counts.append(index)
-            sorted_nums.insert(index, n)
-        return counts[::-1]
+        arr, ans = sorted(nums), []
 
-    def binarySearch(self, sorted: List[int], insertion: int) -> int:
-        l, r = 0, len(sorted)-1
-        while l <= r:
-            m = (l + r) // 2
-            if insertion > sorted[m]:
-                l = m + 1
-            else:
-                r = m - 1
-        return l
+        for num in nums:
+            i = bisect_left(arr,num)
+            ans.append(i)
+            del arr[i]
+
+        return ans
--- returned/python/today24/count_of_smaller_numbers_after_self_test.py
+++ returned/python/today24/count_of_smaller_numbers_after_self_test.py
@@ -13,27 +13,27 @@ def test3():
 def test3():
     assert Solution().countSmaller([-1,-1]) == [0,0]
 
-def test_binary_search():
-    assert Solution().binarySearch([0,1,2], 3) == 3
-    assert bisect_left([0,1,2], 3) == 3
-    assert bisect_right([0,1,2], 3) == 3
+# def test_binary_search():
+#     assert Solution().binarySearch([0,1,2], 3) == 3
+#     assert bisect_left([0,1,2], 3) == 3
+#     assert bisect_right([0,1,2], 3) == 3
 
-    assert Solution().binarySearch([0,1,2], 2) == 2
-    assert bisect_left([0,1,2], 2) == 2
-    assert bisect_right([0,1,2], 2) == 3
+#     assert Solution().binarySearch([0,1,2], 2) == 2
+#     assert bisect_left([0,1,2], 2) == 2
+#     assert bisect_right([0,1,2], 2) == 3
 
-    assert Solution().binarySearch([0,1,2], -1) == 0
-    assert bisect_left([0,1,2], -1) == 0
+#     assert Solution().binarySearch([0,1,2], -1) == 0
+#     assert bisect_left([0,1,2], -1) == 0
 
-    assert Solution().binarySearch([0,1,2,4], 3) == 3
-    assert bisect_left([0,1,2,4], 3) == 3
-    assert bisect_right([0,1,2,4], 3) == 3
+#     assert Solution().binarySearch([0,1,2,4], 3) == 3
+#     assert bisect_left([0,1,2,4], 3) == 3
+#     assert bisect_right([0,1,2,4], 3) == 3
 
-    assert Solution().binarySearch([], 9) == 0
-    assert bisect_left([], 9) == 0
+#     assert Solution().binarySearch([], 9) == 0
+#     assert bisect_left([], 9) == 0
 
-    assert Solution().binarySearch([-1,-1], -1) == 0
-    assert Solution().binarySearch([-1,-1,-1], -1) == 0
+#     assert Solution().binarySearch([-1,-1], -1) == 0
+#     assert Solution().binarySearch([-1,-1,-1], -1) == 0
 
-    assert bisect_right([-1,-1,-1], -1) == 3
-    assert bisect_left([-1,-1,-1], -1) == 0
+#     assert bisect_right([-1,-1,-1], -1) == 3
+#     assert bisect_left([-1,-1,-1], -1) == 0
