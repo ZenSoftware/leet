2b5699f5b8e10b5f8b1e64dc693dfd74c64eca22
Zen Software <ZenSoftware@users.noreply.github.com>
2025-02-28 02:19:49 -0700

    binary index tree

--- returned/python/today24/count_of_smaller_numbers_after_self.py
+++ returned/python/today24/count_of_smaller_numbers_after_self.py
@@ -1,14 +1,32 @@
 # https://leetcode.com/problems/count-of-smaller-numbers-after-self/
-from bisect import bisect_left
 from typing import List
+
+class BinaryIndexTree:
+    def __init__(self, size: int):
+        self.tree = [0] * (size+1)
+
+    def update(self, i: int, value: int):
+        i += 1
+        while i < len(self.tree):
+            self.tree[i] += value
+            i += i & -i
+        
+    def query(self, i: int):
+        i += 1
+        result = 0
+        while i > 0:
+            result += self.tree[i]
+            i -= i & -i
+        return result
 
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
-        arr, ans = sorted(nums), []
-
-        for num in nums:
-            i = bisect_left(arr,num)
-            ans.append(i)
-            del arr[i]
-
-        return ans
+        e = { v: i for i, v in enumerate(sorted(set(nums))) }
+        bit = BinaryIndexTree(len(e))
+        indexes = [e[n] for n in nums]
+        result = []
+        for i in reversed(indexes):
+            count = bit.query(i)
+            result.append(count)
+            bit.update(i+1, 1)
+        return result[::-1]
--- returned/python/today24/count_of_smaller_numbers_after_self_test.py
+++ returned/python/today24/count_of_smaller_numbers_after_self_test.py
@@ -1,5 +1,4 @@
-from bisect import bisect_left, bisect_right
-from count_of_smaller_numbers_after_self import Solution
+from count_of_smaller_numbers_after_self import Solution, BinaryIndexTree
 
 def test1():
     assert Solution().countSmaller([5,2,6,1]) == [2,1,1,0]
@@ -13,27 +12,20 @@ def test3():
 def test3():
     assert Solution().countSmaller([-1,-1]) == [0,0]
 
-# def test_binary_search():
-#     assert Solution().binarySearch([0,1,2], 3) == 3
-#     assert bisect_left([0,1,2], 3) == 3
-#     assert bisect_right([0,1,2], 3) == 3
-
-#     assert Solution().binarySearch([0,1,2], 2) == 2
-#     assert bisect_left([0,1,2], 2) == 2
-#     assert bisect_right([0,1,2], 2) == 3
-
-#     assert Solution().binarySearch([0,1,2], -1) == 0
-#     assert bisect_left([0,1,2], -1) == 0
-
-#     assert Solution().binarySearch([0,1,2,4], 3) == 3
-#     assert bisect_left([0,1,2,4], 3) == 3
-#     assert bisect_right([0,1,2,4], 3) == 3
-
-#     assert Solution().binarySearch([], 9) == 0
-#     assert bisect_left([], 9) == 0
-
-#     assert Solution().binarySearch([-1,-1], -1) == 0
-#     assert Solution().binarySearch([-1,-1,-1], -1) == 0
-
-#     assert bisect_right([-1,-1,-1], -1) == 3
-#     assert bisect_left([-1,-1,-1], -1) == 0
+def test_bit():
+    bit = BinaryIndexTree(4)
+    # [1,2,3,4]
+    bit.update(0,1)
+    bit.update(1,2)
+    bit.update(2,3)
+    bit.update(3,4)
+    # [1,3,6,10]
+    assert bit.query(0) == 1
+    assert bit.query(1) == 3
+    assert bit.query(2) == 6
+    assert bit.query(3) == 10
+    bit.update(0,1)
+    assert bit.query(0) == 2
+    assert bit.query(1) == 4
+    assert bit.query(2) == 7
+    assert bit.query(3) == 11
