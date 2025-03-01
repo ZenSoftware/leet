1748369296bae7c4b223652b0a98f79798cd117d
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-01 01:21:53 -0700

    solution

---
+++ returned/python/today25/coin_change_bottom_up_test.py
@@ -0,0 +1,13 @@
+from coin_change_bottom_up import Solution
+
+def test1():
+    assert Solution().coinChange([1,2,5], 11) == 3
+
+def test2():
+    assert Solution().coinChange([2], 3) == -1
+
+def test3():
+    assert Solution().coinChange([1], 0) == 0
+
+def test4():
+    assert Solution().coinChange([1,3,4,5], 7) == 2
