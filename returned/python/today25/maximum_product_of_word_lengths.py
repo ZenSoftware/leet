50738fb607d8d98f1cb49225a6d4cb7ab96ef701
Zen Software <ZenSoftware@users.noreply.github.com>
2025-02-28 17:47:20 -0700

    solution

--- returned/python/today25/maximum_product_of_word_lengths.py
+++ returned/python/today25/maximum_product_of_word_lengths.py
@@ -3,16 +3,10 @@ from typing import List
 
 class Solution:
     def maxProduct(self, words: List[str]) -> int:
-        wordSets = {i: set(word) for i, word in enumerate(words)}
+        char_sets = [set(words[i]) for i in range(len(words))]
         largest = 0
         for i in range(len(words)):
             for j in range(i+1, len(words)):
-                if self.allUnique(wordSets[i], wordSets[j]):
+                if not char_sets[i] & char_sets[j]:
                     largest = max(largest, len(words[i]) * len(words[j]))
         return largest
-
-    def allUnique(self, a: set, b: set) -> bool:
-        for a_char in a:
-            if a_char in b:
-                return False
-        return True
