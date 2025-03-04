10921a29903b1cc086b2315ca6f762d0fbb244e6
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-03 18:35:05 -0700

    tries

---
+++ returned/python/work/implement_trie2.py
@@ -0,0 +1,35 @@
+# https://neetcode.io/problems/implement-prefix-tree
+
+class Node:
+    def __init__(self, val: str):
+        self.val = val
+        self.children = dict()
+        self.is_terminal = False
+
+class Trie:
+    def __init__(self):
+        self.root = Node(None)
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                node.children[c] = Node(c)
+            node = node.children[c]
+        node.is_terminal = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+        return node.is_terminal
+    
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for c in prefix:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+        return True
---
+++ returned/python/work/implement_trie2_test.py
@@ -0,0 +1,5 @@
+from implement_trie2 import Trie
+
+def test1():
+    tree = Trie()
+    assert tree.startsWith('a') == False
