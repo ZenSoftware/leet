6a1341ca6e4d2206713aeee99da3f6c53e1fac61
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-03 20:48:55 -0700

    memoized

--- returned/python/today26/longest_increasing_path_in_a_matrix.py
+++ returned/python/today26/longest_increasing_path_in_a_matrix.py
@@ -5,6 +5,8 @@ class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         ROWS = len(matrix)
         COLS = len(matrix[0])
+        memo = {}
+        
         def dfs(r: int, c: int, prev: int) -> int:
             if r not in range(ROWS):
                 return 0
@@ -12,13 +14,18 @@ class Solution:
                 return 0
             if prev >= matrix[r][c]:
                 return 0
+            key = (r,c,prev)
+            if key in memo:
+                return memo[key]
 
             prev = matrix[r][c]
             right = dfs(r, c+1, prev) + 1
             left = dfs(r, c-1, prev) + 1
             up = dfs(r+1, c, prev) + 1
             down = dfs(r-1, c, prev) + 1
-            return max(right, left, up, down)
+
+            memo[key] = max(right, left, up, down)
+            return memo[key]
     
         largest = 0
         for r in range(ROWS):
