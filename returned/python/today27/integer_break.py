92bab79f3f23f502c5f7ef2f0934494e1f0a7539
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-05 05:55:50 -0700

    memoized

--- returned/python/today27/integer_break.py
+++ returned/python/today27/integer_break.py
@@ -2,14 +2,17 @@
 
 class Solution:
     def integerBreak(self, n: int) -> int:
+        memo = { 1: 1 }
+        
         def dfs(num: int) -> int:
-            if num == 1:
-                return 1
-            
+            if num in memo:
+                return memo[num]
+                
             res = 0 if num == n else num
             for i in range(1, num):
                 val = dfs(i) * dfs(num-i)
                 res = max(res, val)
+            memo[num] = res
             return res
         
         return dfs(n)
