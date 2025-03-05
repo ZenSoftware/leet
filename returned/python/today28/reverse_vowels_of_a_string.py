f3515550302edd44bd9cc720f665e9a4d5ea8b5f
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-05 07:42:47 -0700

    refactor

--- returned/python/today28/reverse_vowels_of_a_string.py
+++ returned/python/today28/reverse_vowels_of_a_string.py
@@ -1,17 +1,18 @@
 # https://leetcode.com/problems/reverse-vowels-of-a-string/description/
 
 class Solution:
+    VOWELS = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
     def reverseVowels(self, s: str) -> str:
         s = list(s)
         l, r = 0, len(s)-1
-        VOWELS = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
         while l < r:
-            if s[l] in VOWELS and s[r] in VOWELS:
+            if s[l] in self.VOWELS and s[r] in self.VOWELS:
                 s[l], s[r] = s[r], s[l]
                 l += 1
                 r -= 1
-            elif s[l] not in VOWELS:
+            elif s[l] not in self.VOWELS:
                 l += 1
-            elif s[r] not in VOWELS:
+            elif s[r] not in self.VOWELS:
                 r -= 1
         return ''.join(s)
--- returned/python/today28/reverse_vowels_of_a_string_test.py
+++ returned/python/today28/reverse_vowels_of_a_string_test.py
@@ -2,3 +2,6 @@ from reverse_vowels_of_a_string import Solution
 
 def test1():
     assert Solution().reverseVowels('IceCreAm') == 'AceCreIm'
+
+def test2():
+    assert Solution().reverseVowels('a.') == 'a.'
