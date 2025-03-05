583d80a978dc3d739e2ff6090c1415c9d49f652b
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-04 18:49:22 -0700

    refactor

--- returned/python/today27/self_crossing.py
+++ returned/python/today27/self_crossing.py
@@ -2,14 +2,14 @@
 from typing import List
 
 class Solution:
-    def isSelfCrossing(self, distance: List[int]) -> bool:
-        for i in range(3, len(distance)):
-            if distance[i] >= distance[i-2] and distance[i-1] <= distance[i-3]:
+    def isSelfCrossing(self, d: List[int]) -> bool:
+        for i in range(3, len(d)):
+            if d[i] >= d[i-2] and d[i-1] <= d[i-3]:
                 return True
             if i >= 4:
-                if (distance[i] + distance[i-4]) >= distance[i-2] and distance[i-3] == distance[i-1]:
+                if (d[i] + d[i-4]) >= d[i-2] and d[i-3] == d[i-1]:
                     return True
             if i >= 5:
-                if (distance[i-4] + distance[i]) >= distance[i-2] and distance[i-2] > distance[i-4] and (distance[i-1] + distance[i-5]) >= distance[i-3] and distance[i-1] < distance[i-3]:
+                if (d[i-4] + d[i]) >= d[i-2] and d[i-2] > d[i-4] and (d[i-1] + d[i-5]) >= d[i-3] and d[i-1] < d[i-3]:
                     return True
         return False
