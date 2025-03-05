de3e2deaa70c445cfaa5fc3ed9322184f8071a7a
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-05 06:45:41 -0700

    refactor

--- returned/python/today27/reverse_string.py
+++ returned/python/today27/reverse_string.py
@@ -4,7 +4,6 @@ from typing import List
 class Solution:
     def reverseString(self, s: List[str]) -> None:
         l, r = 0, len(s)-1
-
         while l < r:
             s[l], s[r] = s[r], s[l]
             l += 1
