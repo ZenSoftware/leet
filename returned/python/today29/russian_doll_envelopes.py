4a007139fcee145b6b8939991a8db729017645c2
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-08 07:18:15 -0700

    brute

--- returned/python/today29/russian_doll_envelopes.py
+++ returned/python/today29/russian_doll_envelopes.py
@@ -3,24 +3,10 @@ from typing import List
 
 class Solution:
     def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
-        envelopes.sort()
-        memo = {}
-        
-        def dfs(i, prev):
-            if i >= len(envelopes):
-                return 0
-            
-            key = (i, tuple(prev))
-            if key in memo:
-                return memo[key]
-
-            include = 0
-            if prev[0] < envelopes[i][0] and prev[1] < envelopes[i][1]:
-                include = dfs(i+1, envelopes[i]) + 1
-            
-            exclude = dfs(i+1, prev)
-            
-            memo[key] = max(include, exclude)
-            return memo[key]
-        
-        return dfs(0, [float('-inf'), float('-inf')])
+        envelopes.sort(key=lambda x: (x[0], -x[1]))
+        LIS = [1] * len(envelopes)
+        for i in reversed(range(len(envelopes))):
+            for j in range(i+1, len(envelopes)):
+                if envelopes[i][1] < envelopes[j][1]:
+                    LIS[i] = max(LIS[i], 1 + LIS[j])
+        return max(LIS)
