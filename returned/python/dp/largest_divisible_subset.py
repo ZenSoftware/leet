deb4aa6f1762aa736c7d6dcd5dd44bbbda9335f7
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-09 03:15:36 -0600

    simplified

--- returned/python/dp/largest_divisible_subset.py
+++ returned/python/dp/largest_divisible_subset.py
@@ -4,26 +4,14 @@ from typing import List
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
         nums.sort()
-        memo = {}
-
-        def dfs(i: int) -> List[int]:
-            if i >= len(nums):
-                return []
-            if i in memo:
-                return memo[i]
-            
-            res = [nums[i]]
+        
+        dp = [[n] for n in nums]
+        longest = []
+        for i in reversed(range(len(nums))):
             for j in range(i+1, len(nums)):
                 if nums[j] % nums[i] == 0:
-                    tmp = [nums[i]] + dfs(j)
-                    if len(tmp) > len(res):
-                        res = tmp
-            memo[i] = res
-            return res
-        
-        res = []
-        for i in range(len(nums)):
-            tmp = dfs(i)
-            if len(tmp) > len(res):
-                res = tmp
-        return res
+                    tmp = [nums[i]] + dp[j]
+                    dp[i] = tmp if len(tmp) > len(dp[i]) else dp[i]
+            if len(dp[i]) > len(longest):
+                longest = dp[i]    
+        return longest
