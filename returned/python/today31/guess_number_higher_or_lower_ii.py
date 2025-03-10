04ac2cbb7943370d562c172b6826926c3453d383
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-09 22:27:01 -0600

    refactor

--- returned/python/today31/guess_number_higher_or_lower_ii.py
+++ returned/python/today31/guess_number_higher_or_lower_ii.py
@@ -1,5 +1,16 @@
 # https://leetcode.com/problems/guess-number-higher-or-lower-ii/description/
+from functools import cache
 
 class Solution:
     def getMoneyAmount(self, n: int) -> int:
-        pass
+        @cache
+        def dp(left: int, right: int):
+            if left >= right:
+                return 0    
+            ans = float('inf')
+            for guess in range((left+right)//2, right+1):
+                cost = guess + max(dp(left, guess-1), dp(guess+1, right))
+                ans = min(ans, cost)
+            return ans
+        
+        return dp(1, n)
