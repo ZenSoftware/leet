3e822f38ee5b001165e54a4f6b4a1f47b019aa42
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-14 02:36:23 -0600

    refactor

--- returned/python/today34/rotate_function.py
+++ returned/python/today34/rotate_function.py
@@ -5,11 +5,11 @@ class Solution:
     def maxRotateFunction(self, nums: List[int]) -> int:
         ans = float('-inf')
         for rotations in range(len(nums)):
-            start = len(nums) - rotations
+            offset = len(nums) - rotations
             total = 0
             for i in range(len(nums)):
-                if start + i >= len(nums):
-                    start = -i
-                total += nums[start + i] * i
+                if offset + i >= len(nums):
+                    offset = -i
+                total += nums[offset + i] * i
             ans = max(ans, total)
         return ans
