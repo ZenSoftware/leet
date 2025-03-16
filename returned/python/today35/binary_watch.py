083223bd7511d067536bacb91b71b887b5c52610
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-15 21:57:44 -0600

    solution

--- returned/python/today35/binary_watch.py
+++ returned/python/today35/binary_watch.py
@@ -1,18 +1,14 @@
 # https://leetcode.com/problems/binary-watch/description/
-from typing import Counter, List
+from typing import List
 
 class Solution:
     def readBinaryWatch(self, turnedOn: int) -> List[str]:
         result = []
         for h in range(12):
             for m in range(60):
-                hour = '{0:b}'.format(h)
-                hour_bits = Counter(hour)['1']
-
-                minutes = '{0:b}'.format(m)
-                minutes_bits = Counter(minutes)['1']
-                
-                if hour_bits + minutes_bits == turnedOn:
+                hour_bits = bin(h).count('1')
+                minute_bits = bin(m).count('1')
+                if hour_bits + minute_bits == turnedOn:
                     time = '{0}:{1:02d}'.format(h, m)
                     result.append(time)
         return result
