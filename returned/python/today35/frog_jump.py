0d9b79b41b0211b8b9ff31f3e603eda910c60a7b
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-15 23:54:08 -0600

    solution

--- returned/python/today35/frog_jump.py
+++ returned/python/today35/frog_jump.py
@@ -1,14 +1,14 @@
 # https://leetcode.com/problems/frog-jump/
 from typing import List
+from functools import cache
 
 class Solution:
     def canCross(self, stones: List[int]) -> bool:
+        @cache
         def dfs(i: int, k: int) -> bool:
             if i == len(stones)-1:
                 return True
             
-            result = False
-
             for kth in range(k-1, k+2):
                 if kth > 0:
                     for j in range(i+1, len(stones)):
@@ -16,7 +16,7 @@ class Solution:
                         if kth < distance:
                             break
                         elif kth == distance:
-                            result |= dfs(j, kth)
-                            
-            return result
+                            if dfs(j, kth):
+                                return True
+            return False
         return dfs(0, 0)
