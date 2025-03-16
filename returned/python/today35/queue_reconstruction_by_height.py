b61fed2322305d3c5549a22f3630a1d44bdb496d
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-16 03:09:52 -0600

    greedy solution

--- returned/python/today35/queue_reconstruction_by_height.py
+++ returned/python/today35/queue_reconstruction_by_height.py
@@ -3,29 +3,8 @@ from typing import List
 
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
-        result = None
-        
-        def backtrack(remaining: List[List[int]], current: List[List[int]]) -> List[List[int]]:
-            nonlocal result
-            if len(current) == len(people):
-                result = current.copy()
-                return True
-            
-            for i, r in enumerate(remaining):
-                if self.countAhead(current, r) == r[1]:
-                    current.append(r)
-                    if backtrack(remaining[:i] + remaining[i+1:], current):
-                        return True
-                    current.pop()
-            
-            return False
-            
-        backtrack(people, [])
-        return result
-    
-    def countAhead(self, current: List[List[int]], person: List[int]) -> int:
-        count = 0
-        for c in current:
-            if person[0] <= c[0]:
-                count += 1
-        return count
+        people.sort(key=lambda p: (-p[0], p[1]))
+        res = []
+        for p in people:
+            res.insert(p[1], p)
+        return res
