1fa2dc0385e0f0a080c96963acc1d9e86f23ad0a
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-16 02:51:42 -0600

    test

--- returned/python/today35/queue_reconstruction_by_height_test.py
+++ returned/python/today35/queue_reconstruction_by_height_test.py
@@ -1,7 +1,9 @@
 from queue_reconstruction_by_height import Solution
 
 def test1():
-    assert Solution().reconstructQueue([[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]) == [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]]
+    result = Solution().reconstructQueue([[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]])
+    assert result == [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]]
 
 def test2():
-    assert Solution().reconstructQueue([[6,0],[5,0],[4,0],[3,2],[2,2],[1,4]]) == [[4,0],[5,0],[2,2],[3,2],[1,4],[6,0]]
+    result = Solution().reconstructQueue([[6,0],[5,0],[4,0],[3,2],[2,2],[1,4]])
+    assert result == [[4,0],[5,0],[2,2],[3,2],[1,4],[6,0]]
