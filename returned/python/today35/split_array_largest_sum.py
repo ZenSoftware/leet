2903c85520726e86381fc76d413e899a7f8b2081
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-16 15:05:14 -0600

    brute

---
+++ returned/python/today35/split_array_largest_sum.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/split-array-largest-sum/description/
+from typing import List
+
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+        def dfs(i: int, m: int):
+            if m == 1:
+                return sum(nums[i:])
+            
+            current = 0
+            res = float('inf')
+            for j in range(i, len(nums)-m+1):
+                current += nums[j]
+                maxSum = max(current, dfs(j+1, m-1))
+                res = min(res, maxSum)
+                if current > res:
+                    break
+            return res
+        return dfs(0, k)
---
+++ returned/python/today35/split_array_largest_sum_test.py
@@ -0,0 +1,7 @@
+from split_array_largest_sum import Solution
+
+def test1():
+    assert Solution().splitArray([7,2,5,10,8], 2) == 18
+
+def test2():
+    assert Solution().splitArray([1,2,3,4,5], 2) == 9
