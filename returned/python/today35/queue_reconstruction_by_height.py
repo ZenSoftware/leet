7f92606f833c4c6112ac0479398e752332d4f2a4
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-16 02:39:25 -0600

    brute force

---
+++ returned/python/today35/queue_reconstruction_by_height.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/queue-reconstruction-by-height/description/
+from typing import List
+
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        result = None
+        
+        def backtrack(remaining: List[List[int]], current: List[List[int]]) -> List[List[int]]:
+            nonlocal result
+            if len(current) == len(people):
+                result = current.copy()
+                return
+            
+            for i, r in enumerate(remaining):
+                if self.countAhead(current, r) == r[1]:
+                    current.append(r)
+                    backtrack(remaining[:i] + remaining[i+1:], current)
+                    current.pop()
+        
+        backtrack(people, [])
+        return result
+    
+    def countAhead(self, current: List[List[int]], person: List[int]) -> int:
+        count = 0
+        for c in current:
+            if person[0] <= c[0]:
+                count += 1
+        return count
