17231d320c0e1789b3f4a18f31c231e1a2d0ec9e
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-17 01:31:48 -0600

    refactor

--- returned/python/today36/add_strings.py
+++ returned/python/today36/add_strings.py
@@ -7,15 +7,17 @@ class Solution:
         result = []
         carry = 0
         while i1 >= 0 or i2 >= 0 or carry > 0:
-            d1 = 0
             if i1 >= 0:
-                d1 = int(num1[i1])
+                digit1 = int(num1[i1])
+            else:
+                digit1 = 0
             
-            d2 = 0
             if i2 >= 0:
-                d2 = int(num2[i2])
+                digit2 = int(num2[i2])
+            else:
+                digit2 = 0
             
-            place = d1 + d2 + carry
+            place = digit1 + digit2 + carry
             if place >= 10:
                 result.append(str(place - 10))
                 carry = 1
