4fa8f7122b6e9a481734b2f42370e0d76c19944b
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-17 00:44:17 -0600

    smallest time compelxity

--- returned/python/today36/third_maximum_number.py
+++ returned/python/today36/third_maximum_number.py
@@ -1,11 +1,17 @@
 # https://leetcode.com/problems/third-maximum-number/description/
 from typing import List
+from heapq import heapify, heappop
 
 class Solution:
+    '''
+    Time: O(n)
+    Space: O(n)
+    '''
     def thirdMax(self, nums: List[int]) -> int:
-        distinct = list(set(nums))
-        if len(distinct) < 3:
-            return max(distinct)
-        
-        distinct.sort(reverse=True)
-        return distinct[2]
+        max_heap = [-x for x in set(nums)]
+        heapify(max_heap)
+        if len(max_heap) < 3:
+            return -max_heap[0]
+        heappop(max_heap)
+        heappop(max_heap)
+        return -heappop(max_heap)
