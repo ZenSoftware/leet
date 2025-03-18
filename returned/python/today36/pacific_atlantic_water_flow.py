e49ac0d3c18b3e9bfdffd9014e66c9adcbe06e9b
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-17 23:20:53 -0600

    brute force

---
+++ returned/python/today36/pacific_atlantic_water_flow.py
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/pacific-atlantic-water-flow/
+from typing import List
+
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        ROWS, COLS = len(heights), len(heights[0])
+        pacific, atlantic = set(), set()
+
+        def bfs(r, c, h, ocean, visited):
+            if r not in range(ROWS) or c not in range(COLS) or (r,c) in visited:
+                return
+            
+            if heights[r][c] >= h:
+                ocean.add((r,c))
+            else:
+                return
+            
+            visited.add((r,c))
+            for i, j in [[0,1],[0,-1],[1,0],[-1,0]]:
+                bfs(r+i, c+j, heights[r][c], ocean, visited)
+            visited.pop()
+
+        for r in range(ROWS):
+            bfs(r, 0, -1, pacific, set())
+            bfs(r, COLS-1, -1, atlantic, set())
+            
+        for c in range(COLS):
+            bfs(0, c, -1, pacific, set())
+            bfs(ROWS-1, c, -1, atlantic, set())
+
+        result = []
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (r,c) in pacific and (r,c) in atlantic:
+                    result.append([r,c])
+        return result
