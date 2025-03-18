9ffc00bf552ee5f838cf5f4afda0f929b528a753
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-18 05:43:57 -0600

    solution

--- returned/python/today37/reconstruct_original_digits_from_english.py
+++ returned/python/today37/reconstruct_original_digits_from_english.py
@@ -1,32 +1,43 @@
 # https://leetcode.com/problems/reconstruct-original-digits-from-english/
 from collections import Counter
+from typing import Dict
 
 class Solution:
-    NUMBERS = {
-        '0': Counter('zero'),
-        '1': Counter('one'),
-        '2': Counter('two'),
-        '3': Counter('three'),
-        '4': Counter('four'),
-        '5': Counter('five'),
-        '6': Counter('six'),
-        '7': Counter('seven'),
-        '8': Counter('eight'),
-        '9': Counter('nine'),
-    }
+    def countNums(self, s: str) -> Dict[int, int]:
+        char_occ = Counter(s)
+        num_occ = {}
+        
+        # z only appears in 'zero'
+        num_occ['0'] = char_occ['z']
+
+        # w only appears in 'two'
+        num_occ['2'] = char_occ['w']
+
+        # u only appears in 'four'
+        num_occ['4'] = char_occ['u']
+
+        # x only appears in 'six'
+        num_occ['6'] = char_occ['x']
+
+        # g only appears in 'eight'
+        num_occ['8'] = char_occ['g']
+
+        # o appears in 'one', 'two' and 'four'
+        num_occ['1'] = char_occ['o'] - num_occ['0'] - num_occ['2'] - num_occ['4']
+
+        # h appears in 'three' and 'eight'
+        num_occ['3'] = char_occ['h'] - num_occ['8']
+
+        # f appears in 'five' and 'four'
+        num_occ['5'] = char_occ['f'] - num_occ['4']
+
+        # v appears in 'seven' and 'five'
+        num_occ['7'] = char_occ['v'] - num_occ['5']
+
+        # n appears in 'nine', 'one' and 'seven'
+        num_occ['9'] = char_occ['n'] - num_occ['1'] - num_occ['7']
+
+        return num_occ
 
     def originalDigits(self, s: str) -> str:
-        counts = Counter(s)
-        result = ''
-        used = 0
-        while used < len(s):
-            for num, letters in self.NUMBERS.items():
-                for c in letters:
-                    if counts.get(c, 0) < letters[c]:
-                        break
-                else:
-                    result += num
-                    for c in letters:
-                        counts[c] -= letters[c]
-                        used += letters[c]
-        return result
+        return ''.join([digit*count for digit, count in sorted(self.countNums(s).items())])
