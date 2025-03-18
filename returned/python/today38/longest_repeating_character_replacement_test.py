6f46ae1caad03a79de2175891c0e7a57e3fa8a03
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-18 15:20:08 -0600

    solution

---
+++ returned/python/today38/longest_repeating_character_replacement_test.py
@@ -0,0 +1,13 @@
+from longest_repeating_character_replacement import Solution
+
+def test1():
+    assert Solution().characterReplacement('ABAB', 2) == 4
+
+def test2():
+    assert Solution().characterReplacement('AABABBA', 1) == 4
+
+def test3():
+    assert Solution().characterReplacement('AAAA', 0) == 4
+
+def test4():
+    assert Solution().characterReplacement('BAAAA', 0) == 4
