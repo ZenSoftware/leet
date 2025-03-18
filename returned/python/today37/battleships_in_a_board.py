257c3385f27e80050b120b668135e34a42e11127
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-18 00:36:31 -0600

    refactor

--- returned/python/today37/battleships_in_a_board.py
+++ returned/python/today37/battleships_in_a_board.py
@@ -6,8 +6,8 @@ class Solution:
         count = 0
         for i, row in enumerate(board):
             for j, cell in enumerate(row):
-                if cell == 'X':
-                    if (i == 0 or board[i-1][j] == '.') and\
-                       (j == 0 or board[i][j-1] == '.'):
-                        count += 1
+                if cell == 'X' and\
+                   (i == 0 or board[i-1][j] == '.') and\
+                   (j == 0 or board[i][j-1] == '.'):
+                    count += 1
         return count
