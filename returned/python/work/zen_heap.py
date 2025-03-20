6c5e8c8032c07190f15b09c4d9efd1c35fb5aaee
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-20 01:00:29 -0600

    simplified

--- returned/python/work/zen_heap.py
+++ returned/python/work/zen_heap.py
@@ -7,7 +7,7 @@ class MinHeap:
 
     def __len__(self):
         return len(self.heap)
-        
+
     def heapify(self):
         for i in reversed(range(len(self.heap))):
             self.sift_down(i)
@@ -18,27 +18,20 @@ class MinHeap:
             i = parent_index
 
     def sift_down(self, i: int):
-        while True:
-            size = len(self.heap)
-            l = self.left_index(i)
-            r = self.right_index(i)
-            if l < size and r < size:
-                if self.heap[l] < self.heap[r] and self.heap[i] > self.heap[l]:
-                    self.heap[l], self.heap[i] = self.heap[i], self.heap[l]
-                    i = l
-                elif self.heap[l] >= self.heap[r] and self.heap[i] > self.heap[r]:
-                    self.heap[r], self.heap[i] = self.heap[i], self.heap[r]
-                    i = r
-                else:
-                    break
-            elif l < size:
-                if self.heap[i] > self.heap[l]:
-                    self.heap[l], self.heap[i] = self.heap[i], self.heap[l]
-                    i = l
-                else:
-                    break
-            else:
-                break
+        size = len(self.heap)
+        l = self.left_index(i)
+        r = self.right_index(i)
+        if l < size and r < size:
+            if self.heap[l] < self.heap[r] and self.heap[i] > self.heap[l]:
+                self.heap[l], self.heap[i] = self.heap[i], self.heap[l]
+                self.sift_down(l)
+            elif self.heap[l] >= self.heap[r] and self.heap[i] > self.heap[r]:
+                self.heap[r], self.heap[i] = self.heap[i], self.heap[r]
+                self.sift_down(r)
+        elif l < size:
+            if self.heap[i] > self.heap[l]:
+                self.heap[l], self.heap[i] = self.heap[i], self.heap[l]
+                self.sift_down(l)
 
     def push(self, val):
         self.heap.append(val)
@@ -58,38 +51,31 @@ class MinHeap:
 
     def parent_index(self, i: int) -> int:
         return (i-1)//2
-    
+
     def left_index(self, i: int) -> int:
         return 2*i+1
-    
+
     def right_index(self, i: int) -> int:
         return 2*i+2
 
-class MaxHeap(MinHeap):    
+class MaxHeap(MinHeap):
     def sift_up(self, i: int):
         while i != 0 and self.heap[i] > self.heap[parent_index := self.parent_index(i)]:
             self.heap[parent_index], self.heap[i] = self.heap[i], self.heap[parent_index]
             i = parent_index
 
     def sift_down(self, i: int):
-        while True:
-            size = len(self.heap)
-            l = self.left_index(i)
-            r = self.right_index(i)
-            if l < size and r < size:
-                if self.heap[l] > self.heap[r] and self.heap[i] < self.heap[l]:
-                    self.heap[l], self.heap[i] = self.heap[i], self.heap[l]
-                    i = l
-                elif self.heap[l] <= self.heap[r] and self.heap[i] < self.heap[r]:
-                    self.heap[r], self.heap[i] = self.heap[i], self.heap[r]
-                    i = r
-                else:
-                    break
-            elif l < size:
-                if self.heap[i] < self.heap[l]:
-                    self.heap[l], self.heap[i] = self.heap[i], self.heap[l]
-                    i = l
-                else:
-                    break
-            else:
-                break
+        size = len(self.heap)
+        l = self.left_index(i)
+        r = self.right_index(i)
+        if l < size and r < size:
+            if self.heap[l] > self.heap[r] and self.heap[i] < self.heap[l]:
+                self.heap[l], self.heap[i] = self.heap[i], self.heap[l]
+                self.sift_down(l)
+            elif self.heap[l] <= self.heap[r] and self.heap[i] < self.heap[r]:
+                self.heap[r], self.heap[i] = self.heap[i], self.heap[r]
+                self.sift_down(r)
+        elif l < size:
+            if self.heap[i] < self.heap[l]:
+                self.heap[l], self.heap[i] = self.heap[i], self.heap[l]
+                self.sift_down(l)
