955593e5c8c01d41e236547904d7295add419313
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-20 00:26:45 -0600

    format

--- returned/python/work/quick_sort.py
+++ returned/python/work/quick_sort.py
@@ -1,21 +1,21 @@
 from typing import List
 
-def quick_sort(elements: List) -> List:
+def quick_sort(arr: List) -> List:
     def sort(start: int, end: int):
         if start >= end:
             return
 
         i = start-1
         for j in range(start,end):
-            if elements[j] < elements[end]:
+            if arr[j] < arr[end]:
                 i += 1
-                elements[i], elements[j] = elements[j], elements[i]
+                arr[i], arr[j] = arr[j], arr[i]
 
         i = i+1
-        elements[i], elements[end] = elements[end], elements[i]
+        arr[i], arr[end] = arr[end], arr[i]
 
         sort(start, i-1)
         sort(i+1, end)
-        
-    sort(0, len(elements)-1)
-    return elements
+
+    sort(0, len(arr)-1)
+    return arr
