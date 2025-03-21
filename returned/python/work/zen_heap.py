51f23f13db781ebea7e9d8eeece3bc51041298a0
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-20 21:36:12 -0600

    simplified

--- returned/python/work/zen_heap.py
+++ returned/python/work/zen_heap.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 class MinHeap:
     def __init__(self, elements: List = [], key=lambda x: x):
         self.heap = elements
@@ -14,40 +15,35 @@ class MinHeap:
             self.sift_down(i)
 
     def sift_up(self, i: int):
-        def val(x):
-            return self.key(self.heap[x])
-        
-        while i != 0 and val(i) < val(p := self.parent_index(i)):
+        while i != 0 and self.val(i) < self.val(p := (i - 1) // 2):
             self.heap[p], self.heap[i] = self.heap[i], self.heap[p]
             i = p
-        return i
 
     def sift_down(self, i: int):
-        def val(x):
-            return self.key(self.heap[x])
-        
-        def swap(a,b):
+        def swap(a, b):
             self.heap[a], self.heap[b] = self.heap[b], self.heap[a]
 
         size = len(self.heap)
-        l = self.left_index(i)
-        r = self.right_index(i)
+        l = 2 * i + 1
+        r = 2 * i + 2
         if l < size and r < size:
-            if val(l) < val(r) and val(i) > val(l):
+            if self.val(l) < self.val(r) and self.val(i) > self.val(l):
                 swap(i, l)
-                return self.sift_down(l)
-            elif val(l) >= val(r) and val(i) > val(r):
+                self.sift_down(l)
+            elif self.val(l) >= self.val(r) and self.val(i) > self.val(r):
                 swap(i, r)
-                return self.sift_down(r)
+                self.sift_down(r)
         elif l < size:
-            if val(i) > val(l):
+            if self.val(i) > self.val(l):
                 swap(i, l)
-                return self.sift_down(l)
-        return i
+                self.sift_down(l)
+
+    def val(self, i: int):
+        return self.key(self.heap[i])
 
     def push(self, val):
         self.heap.append(val)
-        self.sift_up(len(self.heap)-1)
+        self.sift_up(len(self.heap) - 1)
 
     def peek(self):
         return self.heap[0] if len(self.heap) > 0 else None
@@ -55,50 +51,34 @@ class MinHeap:
     def pop(self):
         if not self.heap:
             return None
-        last_index = len(self.heap)-1
+        last_index = len(self.heap) - 1
         self.heap[0], self.heap[last_index] = self.heap[last_index], self.heap[0]
         result = self.heap.pop()
         self.sift_down(0)
         return result
-    
-    def parent_index(self, i: int) -> int:
-        return (i-1)//2
 
-    def left_index(self, i: int) -> int:
-        return 2*i+1
-
-    def right_index(self, i: int) -> int:
-        return 2*i+2
 
 class MaxHeap(MinHeap):
     def sift_up(self, i: int):
-        def val(x):
-            return self.key(self.heap[x])
-        
-        while i != 0 and val(i) > val(p := self.parent_index(i)):
+        while i != 0 and self.val(i) > self.val(p := (i - 1) // 2):
             self.heap[p], self.heap[i] = self.heap[i], self.heap[p]
             i = p
-        return i
 
     def sift_down(self, i: int):
-        def val(x):
-            return self.key(self.heap[x])
-        
-        def swap(a,b):
+        def swap(a, b):
             self.heap[a], self.heap[b] = self.heap[b], self.heap[a]
 
         size = len(self.heap)
-        l = self.left_index(i)
-        r = self.right_index(i)
+        l = 2 * i + 1
+        r = 2 * i + 2
         if l < size and r < size:
-            if val(l) > val(r) and val(i) < val(l):
+            if self.val(l) > self.val(r) and self.val(i) < self.val(l):
                 swap(i, l)
-                return self.sift_down(l)
-            elif val(l) <= val(r) and val(i) < val(r):
+                self.sift_down(l)
+            elif self.val(l) <= self.val(r) and self.val(i) < self.val(r):
                 swap(i, r)
-                return self.sift_down(r)
+                self.sift_down(r)
         elif l < size:
-            if val(i) < val(l):
+            if self.val(i) < self.val(l):
                 swap(i, l)
-                return self.sift_down(l)
-        return i
+                self.sift_down(l)
