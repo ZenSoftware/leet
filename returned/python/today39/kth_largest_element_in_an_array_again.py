f330d8bbb868f6dcdbe9b68e5e914689f8c41bb0
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-20 23:12:07 -0600

    3 partition

--- returned/python/today39/kth_largest_element_in_an_array_again.py
+++ returned/python/today39/kth_largest_element_in_an_array_again.py
@@ -1,28 +1,39 @@
 # https://leetcode.com/problems/kth-largest-element-in-an-array/
 from typing import List
-from random import randint
 
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         goal = len(nums) - k
 
-        def quick_select(start: int, end: int) -> int:
+        def quick_select(start, end):
+            # 1st patition such that all elements that are less than
+            # the pivot are shuffled to the front
             i = start - 1
-            new_pivot_index = randint(start, end)
-            nums[new_pivot_index], nums[end] = nums[end], nums[new_pivot_index]
+            pivot = nums[end]
             for j in range(start, end):
-                if nums[j] < nums[end]:
+                if nums[j] < pivot:
                     i += 1
                     nums[i], nums[j] = nums[j], nums[i]
-            i += 1
-            nums[i], nums[end] = nums[end], nums[i]
+            dup_start = i + 1
+            nums[dup_start], nums[end] = nums[end], nums[dup_start]
 
-            if i > goal:
-                return quick_select(start, i - 1)
-            elif i < goal:
-                return quick_select(i + 1, end)
+            # 2nd partition will contains all duplicates of pivot
+            # 3rd partition will contain all elements greater than the pivot
+            i = dup_start
+            for j in range(dup_start + 1, end + 1):
+                if nums[j] == pivot:
+                    i += 1
+                    nums[i], nums[j] = nums[j], nums[i]
+            dup_end = i
+
+            # Recurse on the 1st and 3rd partitions
+            # Skip the 2nd partition due to it containing only duplicates
+            if goal < dup_start:
+                return quick_select(start, dup_start - 1)
+            elif dup_end < goal:
+                return quick_select(dup_end + 1, end)
             else:
-                return nums[i]
+                return nums[dup_start]
 
         return quick_select(0, len(nums) - 1)
