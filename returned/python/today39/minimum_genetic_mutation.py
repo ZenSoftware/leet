be1a9f1d57bdbade872d21cd349cfceb893028b0
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-21 03:23:04 -0600

    docs

--- returned/python/today39/minimum_genetic_mutation.py
+++ returned/python/today39/minimum_genetic_mutation.py
@@ -5,34 +5,37 @@ from collections import defaultdict, deque
 
 class Solution:
     def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
-        # Construct unweighted bi-directional graph
+        # A gene is adjacent if it differs by 1 character.
+        # startGene may not be in bank, thus start by addinging it to the graph.
         graph = defaultdict(set)
         for gene in bank:
             if self.differ_by(startGene, gene) == 1:
                 graph[startGene].add(gene)
 
+        # Construct the rest of the unweighted bi-directional graph
         for i in range(len(bank) - 1):
             for j in range(i + 1, len(bank)):
                 if self.differ_by(bank[i], bank[j]) == 1:
                     graph[bank[i]].add(bank[j])
                     graph[bank[j]].add(bank[i])
 
-        # Breadth first search
+        # Breadth first search, level order traversal.
+        # After each level, increment mutation_count by 1
         queue = deque([startGene])
         visited = set([startGene])
-        result = 0
+        mutation_count = 0
         while queue:
             for _ in range(len(queue)):
                 gene = queue.popleft()
                 if gene == endGene:
-                    return result
+                    return mutation_count
                 for adj in graph[gene]:
                     if adj not in visited:
                         queue.append(adj)
                         visited.add(adj)
-            result += 1
+            mutation_count += 1
 
-        # We've exhausted our search thus there is no path to endGene
+        # We've exhausted our search, thus there is no path to endGene
         return -1
 
     def differ_by(self, g1: str, g2: str) -> int:
