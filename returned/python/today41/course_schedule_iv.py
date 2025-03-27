88cbc622bece7573dd33ebadcae70082cd7ca420
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-27 01:36:10 -0600

    refactor

--- returned/python/today41/course_schedule_iv.py
+++ returned/python/today41/course_schedule_iv.py
@@ -1,24 +1,22 @@
 # https://leetcode.com/problems/course-schedule-iv/description/
 from typing import List
 from collections import defaultdict
+from functools import cache
 
 
 class Solution:
     def checkIfPrerequisite(
         self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]
     ) -> List[bool]:
-        cache = defaultdict(set)
         graph = defaultdict(set)
         for a, b in prerequisites:
             graph[b].add(a)
 
+        @cache
         def dfs(course):
-            if course in cache:
-                return cache[course]
             prereqs = set([course])
             for adj in graph[course]:
                 prereqs.update(dfs(adj))
-            cache[course] = prereqs
             return prereqs
 
         result = []
