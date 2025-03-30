c8f59d67d1139c8b01fc26ebb0c8622d9ce6ca5a
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-30 06:26:40 -0600

    binary search

--- returned/python/today42/arranging_coins.py
+++ returned/python/today42/arranging_coins.py
@@ -3,12 +3,14 @@
 
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        total = 0
-        rows = 0
-        for i in range(1, n + 1):
-            total += i
-            if total <= n:
-                rows += 1
+        left, right = 0, n
+        while left <= right:
+            mid = (left + right) // 2
+            cur = mid * (mid + 1) // 2
+            if cur < n:
+                left = mid + 1
+            elif cur > n:
+                right = mid - 1
             else:
-                break
-        return rows
+                return mid
+        return right
