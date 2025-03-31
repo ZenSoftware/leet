59d555569f839d2b27d1a9e932d345f0176f7086
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-31 00:48:31 -0600

    solution

--- returned/python/today43/add_two_numbers_ii.py
+++ returned/python/today43/add_two_numbers_ii.py
@@ -9,23 +9,48 @@ class ListNode:
 
 
 class Solution:
+    """
+    Time: O(5n)
+    Space: O(1)
+    """
+
     def addTwoNumbers(
         self, l1: Optional[ListNode], l2: Optional[ListNode]
     ) -> Optional[ListNode]:
-        def get_num(l: Optional[ListNode]) -> int:
-            res = 0
+        def reverse_list(l: Optional[ListNode]) -> Optional[ListNode]:
+            dummy = ListNode(None)
             while l:
-                res *= 10
-                res += l.val
-                l = l.next
-            return res
+                next_node = l.next
+                l.next = dummy.next
+                dummy.next = l
+                l = next_node
+            return dummy.next
 
-        n1 = get_num(l1)
-        n2 = get_num(l2)
+        l1 = reverse_list(l1)
+        l2 = reverse_list(l2)
 
-        dummy = ListNode()
-        head = dummy
-        for c in str(n1 + n2):
-            head.next = ListNode(int(c))
-            head = head.next
-        return dummy.next
+        res = ListNode(None)
+        carry = 0
+        while l1 or l2:
+            d1, d2 = 0, 0
+            if l1:
+                d1 = l1.val
+            if l2:
+                d2 = l2.val
+
+            total = d1 + d2 + carry
+            if total >= 10:
+                digit_node = ListNode(total - 10)
+                carry = 1
+            else:
+                digit_node = ListNode(total)
+                carry = 0
+            digit_node.next = res.next
+            res.next = digit_node
+
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+
+        return res.next
