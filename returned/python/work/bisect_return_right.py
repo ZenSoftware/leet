0cae98518bd4c2e38d7b684f55da8175b267a6ee
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-30 22:11:16 -0600

    refactor

--- returned/python/work/bisect_return_right.py
+++ returned/python/work/bisect_return_right.py
@@ -5,10 +5,10 @@ def bisect_return_right(arr: List[int], target: int) -> int:
     left, right = 0, len(arr) - 1
     while left <= right:
         mid = (left + right) // 2  # (right-left)//2 + left
-        if target < arr[mid]:
-            right = mid - 1
-        elif arr[mid] < target:
+        if target > arr[mid]:
             left = mid + 1
+        elif target < arr[mid]:
+            right = mid - 1
         else:
             return mid
     return right
