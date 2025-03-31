d6b387c55859047ea5e24f30cbfd619b58f29a33
Zen Software <ZenSoftware@users.noreply.github.com>
2025-03-31 04:47:31 -0600

    refactor

--- returned/python/today43/number_of_boomerangs.py
+++ returned/python/today43/number_of_boomerangs.py
@@ -15,6 +15,6 @@ class Solution:
             for x2, y2 in points:
                 dist = (x2 - x1) ** 2 + (y2 - y1) ** 2
                 counts[dist] = counts.get(dist, 0) + 1
-            for count in counts.values():
-                res += count * (count - 1)
+            for n in counts.values():
+                res += n * (n - 1)
         return res
