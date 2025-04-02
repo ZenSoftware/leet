a0adc73cebea58fe70857bbd5a1f218c70d22494
Zen Software <ZenSoftware@users.noreply.github.com>
2025-04-02 00:04:19 -0600

    time complexity

--- returned/python/today44/minimum_number_of_arrows_to_burst_balloons.py
+++ returned/python/today44/minimum_number_of_arrows_to_burst_balloons.py
@@ -3,6 +3,11 @@ from typing import List
 
 
 class Solution:
+    """
+    Time: O(nlogn)
+    Space: O(1)
+    """
+
     def findMinArrowShots(self, points: List[List[int]]) -> int:
         points.sort(key=lambda x: x[1])
         res = 0
