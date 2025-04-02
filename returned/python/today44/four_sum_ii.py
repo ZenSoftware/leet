7ea18e2877700f69b58d4ac59ee1e327ecfbb12a
Zen Software <ZenSoftware@users.noreply.github.com>
2025-04-02 03:04:10 -0600

    efficiency

--- returned/python/today44/four_sum_ii.py
+++ returned/python/today44/four_sum_ii.py
@@ -1,17 +1,20 @@
 # https://leetcode.com/problems/4sum-ii/description/
 from typing import List
+from collections import defaultdict
 
 
 class Solution:
     def fourSumCount(
         self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]
     ) -> int:
-        n = len(nums1)
-        count = 0
-        for i in range(n):
-            for j in range(n):
-                for k in range(n):
-                    for l in range(n):
-                        if nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0:
-                            count += 1
-        return count
+        memo = defaultdict(int)
+        for i in nums1:
+            for j in nums2:
+                memo[i + j] += 1
+
+        res = 0
+        for i in nums3:
+            for j in nums4:
+                res += memo[-i - j]
+
+        return res
