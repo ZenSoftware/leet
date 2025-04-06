3d8caee969f75a8862f7bbe55f30e99de31eb4d1
Zen Software <ZenSoftware@users.noreply.github.com>
2025-04-06 15:51:45 -0600

    median

--- returned/python/today46/minimum_moves_to_equal_array_elements_ii.py
+++ returned/python/today46/minimum_moves_to_equal_array_elements_ii.py
@@ -4,17 +4,14 @@ from typing import List
 
 class Solution:
     """
-    Time: O(n^2)
+    Time: O(nlogn)
     Space: O(1)
     """
 
     def minMoves2(self, nums: List[int]) -> int:
-        res = float("inf")
-        for n1 in nums:
-            count = 0
-            for n2 in nums:
-                count += abs(n1 - n2)
-                if count >= res:
-                    break
-            res = min(res, count)
+        nums.sort()
+        median = nums[(len(nums) - 1) // 2]
+        res = 0
+        for n in nums:
+            res += abs(median - n)
         return res
