6a55dfb38f18d92b9de63a0316b8f44cba6fce05
Zen Software <ZenSoftware@users.noreply.github.com>
2025-04-07 02:33:37 -0600

    faster solution

--- returned/python/today46/unique_substrings_in_wraparound_string.py
+++ returned/python/today46/unique_substrings_in_wraparound_string.py
@@ -1,18 +1,15 @@
 # https://leetcode.com/problems/unique-substrings-in-wraparound-string/description/
-from collections import defaultdict
 
 
 class Solution:
     def findSubstringInWraproundString(self, s: str) -> int:
-        d = defaultdict(int)
-        i = 0
-        while i < len(s):
-            d[s[i]] = max(d[s[i]], 1)
-            j = i + 1
-            while j < len(s) and (
-                ord(s[j]) - ord(s[j - 1]) == 1 or ord(s[j]) - ord(s[j - 1]) == -25
-            ):
-                d[s[j]] = max(d[s[j]], j - i + 1)
-                j += 1
-            i = j
-        return sum(d.values())
+        char_dict = {char: 1 for char in s}
+        count = 1
+        for i in range(1, len(s)):
+            diff = ord(s[i]) - ord(s[i - 1])
+            if diff == 1 or diff == -25:
+                count += 1
+            else:
+                count = 1
+            char_dict[s[i]] = max(char_dict[s[i]], count)
+        return sum(char_dict.values())
