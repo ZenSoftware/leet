783a156496c0ef54b65e6e9ce6cf0b1db28a45d0
Zen Software <ZenSoftware@users.noreply.github.com>
2025-04-06 23:12:53 -0600

    solution

--- returned/python/today46/can_i_win.py
+++ returned/python/today46/can_i_win.py
@@ -1,5 +1,4 @@
 # https://leetcode.com/problems/can-i-win/description/
-from typing import Tuple
 from functools import cache
 
 
@@ -12,21 +11,19 @@ class Solution:
     def canIWin(self, maxChoosableInteger: int, desiredTotal: int) -> bool:
         if desiredTotal <= 0:
             return True
-        elif maxChoosableInteger * (maxChoosableInteger + 1) / 2 < desiredTotal:
+        elif (maxChoosableInteger) * (maxChoosableInteger + 1) // 2 < desiredTotal:
             return False
 
-        choices = tuple(range(1, maxChoosableInteger + 1))
-        return self.compute_win(choices, desiredTotal)
+        @cache
+        def doesP1Win(total, bitmask):
+            if total <= 0:
+                return False
 
-    @cache
-    def compute_win(self, choices: Tuple[int], target: int) -> bool:
-        if choices[-1] >= target:
-            return True
+            for i in range(1, maxChoosableInteger + 1):
+                i_bit = 1 << i
+                if bitmask & i_bit == 0:
+                    if not doesP1Win(total - i, bitmask | i_bit):
+                        return True
+            return False
 
-        for i in range(len(choices)):
-            next_choices = choices[:i] + choices[i + 1 :]
-            next_target = target - choices[i]
-            if not self.compute_win(next_choices, next_target):
-                return True
-
-        return False
+        return doesP1Win(desiredTotal, 0)
