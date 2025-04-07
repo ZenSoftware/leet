a122a727b64370308c6ad5dea3f5d9fb2b76ac15
Zen Software <ZenSoftware@users.noreply.github.com>
2025-04-07 03:14:02 -0600

    solution

--- returned/python/today46/validate_ip_address.py
+++ returned/python/today46/validate_ip_address.py
@@ -3,30 +3,28 @@
 
 class Solution:
     def validIPAddress(self, queryIP: str) -> str:
-        neither = "Neither"
-
         if "." in queryIP:
             split_ip = queryIP.split(".")
             if len(split_ip) != 4:
-                return neither
+                return "Neither"
             for segment in split_ip:
-                if not segment.isnumeric():
-                    return neither
-                if len(segment) > 1 and segment[0] == "0":
-                    return neither
-                if not (0 <= int(segment) <= 255):
-                    return neither
+                if (
+                    not segment.isnumeric()
+                    or (len(segment) > 1 and segment[0] == "0")
+                    or not (0 <= int(segment) <= 255)
+                ):
+                    return "Neither"
             return "IPv4"
         elif ":" in queryIP:
             split_ip = queryIP.split(":")
             if len(split_ip) != 8:
-                return neither
+                return "Neither"
             for segment in split_ip:
                 if not self._is_hex(segment):
-                    return neither
+                    return "Neither"
             return "IPv6"
 
-        return neither
+        return "Neither"
 
     def _is_hex(self, s: str) -> bool:
         if not (1 <= len(s) <= 4):
