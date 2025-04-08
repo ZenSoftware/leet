5d532a261b5887deb203972df113507ea33229ca
Zen Software <ZenSoftware@users.noreply.github.com>
2025-04-07 23:25:03 -0600

    trie solution

--- returned/python/today46/concatenated_words.py
+++ returned/python/today46/concatenated_words.py
@@ -2,33 +2,43 @@
 from typing import List
 
 
+class TrieNode:
+    def __init__(self):
+        self.terminal = False
+        self.children = {}
+
+
 class Solution:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str):
+        cur = self.root
+        for char in word:
+            if char not in cur.children:
+                cur.children[char] = TrieNode()
+            cur = cur.children[char]
+        cur.terminal = True
+
+    def dfs(self, key, index, count):
+        if index >= len(key):
+            return count > 1
+        curr = self.root
+        for i in range(index, len(key)):
+            if key[i] not in curr.children:
+                return False
+            curr = curr.children[key[i]]
+            if curr.terminal:
+                if self.dfs(key, i + 1, count + 1):
+                    return True
+        return False
+
     def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
-        words = set(words)
+        for word in words:
+            self.insert(word)
 
-        def isConcatenatedWord(word: str) -> bool:
-            i = 0
-            while i < len(word):
-                j = i + 1
-                while j <= len(word):
-                    substr = word[i:j]
-                    if substr in words:
-                        words.remove(substr)
-                        if isConcatenatedWord(word[i:]):
-                            words.add(substr)
-                            return True
-                        words.add(substr)
-                        break
-                    j += 1
-                else:
-                    return False
-                i = j
-            return True
-
-        result = []
-        for word in words.copy():
-            words.remove(word)
-            if isConcatenatedWord(word):
-                result.append(word)
-            words.add(word)
-        return result
+        ans = []
+        for i in range(len(words)):
+            if self.dfs(words[i], 0, 0):
+                ans.append(words[i])
+        return ans
