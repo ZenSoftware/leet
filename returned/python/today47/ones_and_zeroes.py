11b6c1ee0e485d590cc320fb9523e9939667d9b4
Zen Software <ZenSoftware@users.noreply.github.com>
2025-04-08 08:06:12 -0600

    refactor

--- returned/python/today47/ones_and_zeroes.py
+++ returned/python/today47/ones_and_zeroes.py
@@ -1,16 +1,22 @@
 # https://leetcode.com/problems/ones-and-zeroes/description/
 from typing import List
-from collections import Counter
+from functools import cache
 
 
 class Solution:
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
-        strs.sort(key=len)
-        res = 0
-        for s in strs:
-            counts = Counter(s)
-            if counts["0"] <= m and counts["1"] <= n:
-                res += 1
-                m -= counts["0"]
-                n -= counts["1"]
-        return res
+        counts = [[s.count("0"), s.count("1")] for s in strs]
+
+        @cache
+        def dfs(i: int, m_left: int, n_left: int) -> int:
+            if m_left < 0 or n_left < 0:
+                return float("-inf")
+
+            if i == len(strs):
+                return 0
+
+            with_i = 1 + dfs(i + 1, m_left - counts[i][0], n_left - counts[i][1])
+            without_i = dfs(i + 1, m_left, n_left)
+            return max(with_i, without_i)
+
+        return dfs(0, m, n)
