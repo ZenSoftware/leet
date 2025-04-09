db7b05dd982ba517eb6a74788884ab77ec54806e
Zen Software <ZenSoftware@users.noreply.github.com>
2025-04-09 07:35:30 -0600

    quick select

--- returned/python/today47/sliding_window_median.py
+++ returned/python/today47/sliding_window_median.py
@@ -9,15 +9,63 @@ class Solution:
         mid_index = k // 2
         if k % 2 == 1:
             while i + k <= len(nums):
-                nums_sorted = nums[i : i + k]
-                nums_sorted.sort()
-                result.append(nums_sorted[mid_index])
+                median = self.quick_select_odd(nums[i : i + k], mid_index)
+                result.append(median)
                 i += 1
         else:
             while i + k <= len(nums):
-                nums_sorted = nums[i : i + k]
-                nums_sorted.sort()
-                median = (nums_sorted[mid_index - 1] + nums_sorted[mid_index]) / 2
+                mid_left, mid_right = self.quick_select_even(nums[i : i + k], mid_index)
+                median = (mid_left + mid_right) / 2
                 result.append(median)
                 i += 1
         return result
+
+    def quick_select_even(
+        self, nums: List[int], mid_right_index: int
+    ) -> tuple[int, int]:
+        def helper(left: int, right: int, target: int):
+            if left == right:
+                return nums[left]
+
+            i = left - 1
+            for j in range(left, right):
+                if nums[j] < nums[right]:
+                    i += 1
+                    nums[i], nums[j] = nums[j], nums[i]
+
+            i += 1
+            nums[i], nums[right] = nums[right], nums[i]
+
+            if i < target:
+                return helper(i + 1, right, target)
+            elif target < i:
+                return helper(left, i - 1, target)
+            else:
+                return nums[i]
+
+        mid_right = helper(0, len(nums) - 1, mid_right_index)
+        mid_left = helper(0, mid_right_index - 1, mid_right_index - 1)
+        return (mid_right, mid_left)
+
+    def quick_select_odd(self, nums: List[int], target_index: int) -> int:
+        def helper(left: int, right: int):
+            if left == right:
+                return nums[left]
+
+            i = left - 1
+            for j in range(left, right):
+                if nums[j] < nums[right]:
+                    i += 1
+                    nums[i], nums[j] = nums[j], nums[i]
+
+            i += 1
+            nums[i], nums[right] = nums[right], nums[i]
+
+            if i < target_index:
+                return helper(i + 1, right)
+            elif target_index < i:
+                return helper(left, i - 1)
+            else:
+                return nums[i]
+
+        return helper(0, len(nums) - 1)
