04ec367c25e2bf605cc60ff8288cb7b9d11d6fb7
Zen Software <ZenSoftware@users.noreply.github.com>
2025-04-09 01:09:16 -0600

    solution

--- returned/python/today47/total_hamming_distance.py
+++ returned/python/today47/total_hamming_distance.py
@@ -1,11 +1,17 @@
 # https://leetcode.com/problems/total-hamming-distance/description/
 from typing import List
+import math
 
 
 class Solution:
     def totalHammingDistance(self, nums: List[int]) -> int:
+        n = len(nums)
+        bit_count = math.floor(math.log2(max(nums))) + 1
         result = 0
-        for i in range(len(nums) - 1):
-            for j in range(i + 1, len(nums)):
-                result += (nums[i] ^ nums[j]).bit_count()
+        for i in range(bit_count):
+            ones = 0
+            for num in nums:
+                if (num >> i) & 1:
+                    ones += 1
+            result += ones * (n - ones)
         return result
