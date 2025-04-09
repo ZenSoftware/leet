daad9ed2ccd9c02dd2735c3432fc595bcbae15b5
Zen Software <ZenSoftware@users.noreply.github.com>
2025-04-08 23:04:49 -0600

    docs

--- returned/python/today47/number_complement.py
+++ returned/python/today47/number_complement.py
@@ -4,6 +4,29 @@ import math
 
 class Solution:
     def findComplement(self, num: int) -> int:
-        num_bits = math.floor(math.log2(num)) + 1
-        mask = (1 << num_bits) - 1
+        """
+        Time: O(1)
+        Space: O(1)
+        ---------------------
+        0   1   0   0   0
+           2^3 2^2 2^1 2^0
+        assert log 8 == 3
+        bit_length = logn + 1
+        we add 1 due to the LSB being 0th indexed
+        ---------------------
+        10000 - 1
+        01111
+        mask = (1 << bit_length) - 1
+        ---------------------
+        0 xor 0 = 0
+        0 xor 1 = 1
+        1 xor 0 = 1
+        1 xor 1 = 0
+        mask     = 1111
+        num      = 1011
+        num^mask = 0100
+        """
+
+        bit_length = math.floor(math.log2(num)) + 1
+        mask = (1 << bit_length) - 1
         return num ^ mask
