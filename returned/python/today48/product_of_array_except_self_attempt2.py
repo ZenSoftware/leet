697f4a807dbbb9f8baef0f5ce49fb544b20d8b4b
Zen Software <ZenSoftware@users.noreply.github.com>
2025-04-09 22:19:28 -0600

    simplified

--- returned/python/today48/product_of_array_except_self_attempt2.py
+++ returned/python/today48/product_of_array_except_self_attempt2.py
@@ -4,26 +4,21 @@ from typing import List
 
 class Solution:
     """
-    Time: O(3n)
-    Space: O(2n)
+    Time: O(n)
+    Space: O(n)
     """
 
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        accum = 1
-        forward = []
-        for i in range(len(nums)):
-            accum *= nums[i]
-            forward.append(accum)
+        result = [1] * len(nums)
 
-        accum = 1
-        reverse = [None] * len(nums)
+        left_product = 1
+        for i in range(len(nums)):
+            result[i] = left_product
+            left_product *= nums[i]
+
+        right_product = 1
         for i in reversed(range(len(nums))):
-            accum *= nums[i]
-            reverse[i] = accum
+            result[i] *= right_product
+            right_product *= nums[i]
 
-        result = []
-        for i in range(len(nums)):
-            f = 1 if i - 1 not in range(len(nums)) else forward[i - 1]
-            r = 1 if i + 1 not in range(len(nums)) else reverse[i + 1]
-            result.append(f * r)
         return result
