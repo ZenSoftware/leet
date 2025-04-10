db09c37ae98f1cdc80c8698de59ec33e00e0e1ef
Zen Software <ZenSoftware@users.noreply.github.com>
2025-04-10 02:35:45 -0600

    solution

---
+++ returned/python/today48/container_with_most_water_attempt2.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/container-with-most-water/description/
+from typing import List
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left, right = 0, len(height) - 1
+        result = 0
+        while left < right:
+            h = min(height[left], height[right])
+            w = right - left
+            result = max(result, w * h)
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return result
---
+++ returned/python/today48/container_with_most_water_attempt2_test.py
@@ -0,0 +1,9 @@
+from container_with_most_water_attempt2 import Solution
+
+
+def test1():
+    assert Solution().maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]) == 49
+
+
+def test2():
+    assert Solution().maxArea([1, 1]) == 1
