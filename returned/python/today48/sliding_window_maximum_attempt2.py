97040c6a3237d58180e6683f2ea062b860d6448d
Zen Software <ZenSoftware@users.noreply.github.com>
2025-04-10 03:08:44 -0600

    efficiency attempt

--- returned/python/today48/sliding_window_maximum_attempt2.py
+++ returned/python/today48/sliding_window_maximum_attempt2.py
@@ -4,10 +4,19 @@ from typing import List
 
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        cur_max_index = 0
+        for i in range(1, k):
+            if nums[i] > nums[cur_max_index]:
+                cur_max_index = i
+
         result = []
         for i in range(len(nums) - k + 1):
-            cur_max = nums[i]
-            for j in range(1, k):
-                cur_max = max(cur_max, nums[i + j])
-            result.append(cur_max)
+            if nums[i + k - 1] >= nums[cur_max_index]:
+                cur_max_index = i + k - 1
+            elif cur_max_index < i:
+                cur_max_index = i
+                for j in range(1, k):
+                    if nums[i + j] > nums[cur_max_index]:
+                        cur_max_index = i + j
+            result.append(nums[cur_max_index])
         return result
