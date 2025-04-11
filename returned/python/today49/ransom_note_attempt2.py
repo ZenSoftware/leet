15458126112e9f18b3d13afcb6b89931e200159b
Zen Software <ZenSoftware@users.noreply.github.com>
2025-04-11 01:30:49 -0600

    simplified

--- returned/python/today49/ransom_note_attempt2.py
+++ returned/python/today49/ransom_note_attempt2.py
@@ -5,7 +5,7 @@ from collections import Counter
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         magazine_counts = Counter(magazine)
-        for char, count in Counter(ransomNote).items():
-            if magazine_counts.get(char, 0) - count < 0:
+        for char, ransom_count in Counter(ransomNote).items():
+            if magazine_counts.get(char, 0) - ransom_count < 0:
                 return False
         return True
