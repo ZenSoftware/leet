ce9b5775edef9f2de73b659acfbf6f4b2cda3596
Zen Software <ZenSoftware@users.noreply.github.com>
2025-04-10 23:37:12 -0600

    efficient

--- returned/python/today49/minimum_window_substring.py
+++ returned/python/today49/minimum_window_substring.py
@@ -1,5 +1,5 @@
 # https://leetcode.com/problems/minimum-window-substring/description/
-from collections import Counter, defaultdict
+from collections import defaultdict
 
 
 class Solution:
@@ -7,34 +7,29 @@ class Solution:
         if len(s) < len(t):
             return ""
 
-        t_counts = Counter(t)
+        map = defaultdict(int)
+        remaining = len(t)
+        start = 0
+        end = 0
+        min_len = float("inf")
+        start_index = 0
+        for char in t:
+            map[char] += 1
 
-        def is_valid(s_counts: Counter):
-            for char, count in t_counts.items():
-                if s_counts[char] < count:
-                    return False
-            return True
+        while end < len(s):
+            if map[s[end]] > 0:
+                remaining -= 1
+            map[s[end]] -= 1
+            end += 1
 
-        def get_min_window(start: int):
-            s_counts = defaultdict(int)
-            for end in range(len(t) - 1):
-                s_counts[s[start + end]] += 1
+            while remaining == 0:
+                if end - start < min_len:
+                    start_index = start
+                    min_len = end - start
 
-            for end in range(start + len(t) - 1, len(s)):
-                s_counts[s[end]] += 1
-                if is_valid(s_counts):
-                    return s[start : end + 1]
+                if map[s[start]] == 0:
+                    remaining += 1
+                map[s[start]] += 1
+                start += 1
 
-            return ""
-
-        result = ""
-        for start in range(len(s) - len(t) + 1):
-            if s[start] not in t_counts:
-                continue
-
-            window = get_min_window(start)
-            if window != "" and (result == "" or len(window) < len(result)):
-                result = window
-                if len(result) == len(t):
-                    return result
-        return result
+        return "" if min_len == float("inf") else s[start_index : start_index + min_len]
