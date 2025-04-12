27c566566ac4b67aece1c03bfde351867939e221
Zen Software <ZenSoftware@users.noreply.github.com>
2025-04-12 01:56:05 -0600

    brute force

--- returned/python/today50/combinations_attempt2.py
+++ returned/python/today50/combinations_attempt2.py
@@ -4,23 +4,20 @@ from typing import List
 
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        def combinations(elements: List[int]) -> List[List[int]]:
-            if not elements:
+        def combinations(remaining: int) -> List[List[int]]:
+            if remaining == 0:
                 return [[]]
 
-            first_el = elements[0]
-            without_first = elements[1:]
-            combs_without_first = combinations(without_first)
+            combs_without_first = combinations(remaining - 1)
             combs_with_first = []
 
             for comb in combs_without_first:
-                combs_with_first.append([first_el] + comb)
+                combs_with_first.append(comb + [remaining])
             combs_with_first.extend(combs_without_first)
             return combs_with_first
 
-        result = combinations(list(range(1, n + 1)))
-        print(result)
-        return result
+        result = combinations(n)
+        return [comb for comb in result if len(comb) == k]
 
 
-# Solution().combine(3, 2)
+print(Solution().combine(3, 2))
