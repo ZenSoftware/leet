6f7db1349caf976c512f8dc4a7c043be029ce1a1
Zen Software <ZenSoftware@users.noreply.github.com>
2025-04-12 22:57:10 -0600

    simplified

--- returned/python/today50/combinations_attempt2.py
+++ returned/python/today50/combinations_attempt2.py
@@ -11,15 +11,19 @@ class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         result = []
 
-        def combinations(start: int, path: List[int]) -> List[List[int]]:
+        def combinations(i: int, path: List[int]) -> List[List[int]]:
             if len(path) == k:
                 result.append(path.copy())
                 return
 
-            for i in range(start, n + 1):
-                path.append(i)
-                combinations(i + 1, path)
-                path.pop()
+            if i > n:
+                return
+
+            path.append(i)
+            combinations(i + 1, path)
+            path.pop()
+
+            combinations(i + 1, path)
 
         combinations(1, [])
         return result
