2cbdc4726214816b6f595982350b6c56eecbc56f
Zen Software <ZenSoftware@users.noreply.github.com>
2025-04-14 02:45:54 -0600

    solution

--- returned/python/today52/set_matrix_zeroes_attempt2.py
+++ returned/python/today52/set_matrix_zeroes_attempt2.py
@@ -3,18 +3,20 @@ from typing import List
 
 
 class Solution:
+    """
+    Time: O(n * m)
+    Space: O(n + m)
+    """
+
     def setZeroes(self, matrix: List[List[int]]) -> None:
         n, m = len(matrix), len(matrix[0])
-        for i in range(n):
-            for j in range(m):
-                if matrix[i][j] == 0:
-                    for k in range(m):
-                        if matrix[i][k] != 0:
-                            matrix[i][k] = None
-                    for k in range(n):
-                        if matrix[k][j] != 0:
-                            matrix[k][j] = None
-        for i in range(n):
-            for j in range(m):
-                if matrix[i][j] is None:
-                    matrix[i][j] = 0
+        row = [False] * n
+        col = [False] * m
+        for r in range(n):
+            for c in range(m):
+                if matrix[r][c] == 0:
+                    row[r] = col[c] = True
+        for r in range(n):
+            for c in range(m):
+                if row[r] or col[c]:
+                    matrix[r][c] = 0
