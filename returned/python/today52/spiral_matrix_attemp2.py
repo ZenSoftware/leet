811188b333cef230681dbc4af9760459f5614b23
Zen Software <ZenSoftware@users.noreply.github.com>
2025-04-14 04:43:14 -0600

    refactor

--- returned/python/today52/spiral_matrix_attemp2.py
+++ returned/python/today52/spiral_matrix_attemp2.py
@@ -16,25 +16,25 @@ class Solution:
         while len(result) < total_elements:
             for c in range(left, right + 1):
                 result.append(matrix[top][c])
+            top += 1
 
             if len(result) >= total_elements:
                 return result
 
-            top += 1
             for r in range(top, bottom + 1):
                 result.append(matrix[r][right])
+            right -= 1
 
             if len(result) >= total_elements:
                 return result
 
-            right -= 1
             for c in range(right, left - 1, -1):
                 result.append(matrix[bottom][c])
+            bottom -= 1
 
             if len(result) >= total_elements:
                 return result
 
-            bottom -= 1
             for r in range(bottom, top - 1, -1):
                 result.append(matrix[r][left])
             left += 1
