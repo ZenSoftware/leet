605e7349e295c976ebdfe2cae153b21111ae3ee9
Zen Software <ZenSoftware@users.noreply.github.com>
2025-04-17 01:16:06 -0600

    faster

--- csharp/Leet/Today1/BinaryTreeLevelOrderTraversal.cs
+++ csharp/Leet/Today1/BinaryTreeLevelOrderTraversal.cs
@@ -1,40 +1,63 @@
 // https://leetcode.com/problems/binary-tree-level-order-traversal/description/
 using Leet.Tree;
+namespace Leet.BinaryTreeLevelOrderTraversal;
 
-namespace Leet.BinaryTreeLevelOrderTraversal
+public class Solution
 {
-    public class Solution
+    public IList<IList<int>> LevelOrder(TreeNode root)
     {
-        public IList<IList<int>> LevelOrder(TreeNode root)
+        if (root == null)
+            return [];
+
+        var result = new List<IList<int>>();
+        var queue = new LinkedList<TreeNode>([root]);
+
+        while (queue.Count > 0)
         {
-            if (root == null)
-                return [];
+            var size = queue.Count;
+            var level = new List<int>(size);
 
-            var result = new List<IList<int>>();
-            var queue = new LinkedList<TreeNode>([root]);
-
-            while (queue.Count > 0)
+            for (int i = 0; i < size; i++)
             {
-                var size = queue.Count;
-                var level = new List<int>(size);
+                var node = queue.First.Value;
+                queue.RemoveFirst();
+                level.Add(node.val);
 
-                for (int i = 0; i < size; i++)
-                {
-                    var node = queue.First.Value;
-                    queue.RemoveFirst();
-                    level.Add(node.val);
+                if (node.left != null)
+                    queue.AddLast(node.left);
 
-                    if (node.left != null)
-                        queue.AddLast(node.left);
-
-                    if (node.right != null)
-                        queue.AddLast(node.right);
-                }
-
-                result.Add(level);
+                if (node.right != null)
+                    queue.AddLast(node.right);
             }
 
-            return result;
+            result.Add(level);
         }
+
+        return result;
+    }
+}
+
+public class Solution2
+{
+    public IList<IList<int>> LevelOrder(TreeNode root)
+    {
+        var levels = new List<IList<int>>();
+        DFS(root, levels, 0);
+        return levels;
+    }
+
+    private void DFS(TreeNode root, IList<IList<int>> levels, int level)
+    {
+        if (root == null)
+            return;
+
+        if (levels.Count - 1 < level)
+        {
+            levels.Add([]);
+        }
+        levels[level].Add(root.val);
+
+        DFS(root.left, levels, level + 1);
+        DFS(root.right, levels, level + 1);
     }
 }
--- csharp/LeetTests/BinaryTreeLevelOrderTraversalTest.cs
+++ csharp/LeetTests/BinaryTreeLevelOrderTraversalTest.cs
@@ -7,7 +7,6 @@ namespace LeetTests
         [Test]
         public void TestBinaryTreeLevelOrderTraversal()
         {
-            var solution = new Solution();
             var n3 = new TreeNode(3);
             var n9 = new TreeNode(9);
             var n20 = new TreeNode(20);
@@ -17,6 +16,28 @@ namespace LeetTests
             n3.right = n20;
             n20.left = n15;
             n20.right = n7;
+
+            var solution = new Solution();
+            Assert.That(
+                solution.LevelOrder(n3),
+                Is.EquivalentTo(new[] { new[] { 3 }, new[] { 9, 20 }, new[] { 15, 7 } })
+            );
+        }
+
+        [Test]
+        public void TestBinaryTreeLevelOrderTraversal2()
+        {
+            var n3 = new TreeNode(3);
+            var n9 = new TreeNode(9);
+            var n20 = new TreeNode(20);
+            var n15 = new TreeNode(15);
+            var n7 = new TreeNode(7);
+            n3.left = n9;
+            n3.right = n20;
+            n20.left = n15;
+            n20.right = n7;
+
+            var solution = new Solution2();
             Assert.That(
                 solution.LevelOrder(n3),
                 Is.EquivalentTo(new[] { new[] { 3 }, new[] { 9, 20 }, new[] { 15, 7 } })
