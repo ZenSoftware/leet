5e20b1e819dd850057753a3e98fac6c667f5af13
Zen Software <ZenSoftware@users.noreply.github.com>
2025-04-21 04:34:41 -0600

    solution

---
+++ csharp/Leet/Today1/DesignCircularQueue.cs
@@ -0,0 +1,66 @@
+// https://leetcode.com/problems/design-circular-queue/description/
+namespace Leet.DesignCircularQueue;
+
+public class MyCircularQueue
+{
+    int capacity;
+    int[] elements;
+    int start = 0;
+    int end = -1;
+    int count = 0;
+
+    public MyCircularQueue(int k)
+    {
+        capacity = k;
+        elements = new int[k];
+    }
+
+    public bool EnQueue(int value)
+    {
+        if (count == capacity)
+            return false;
+        count++;
+        end = (end + 1) % capacity;
+        elements[end] = value;
+        return true;
+    }
+
+    public bool DeQueue()
+    {
+        if (count == 0)
+            return false;
+        else if (count == 1)
+        {
+            start = 0;
+            end = -1;
+        }
+        else
+            start = (start + 1) % capacity;
+        count--;
+        return true;
+    }
+
+    public int Front()
+    {
+        if (count == 0)
+            return -1;
+        return elements[start];
+    }
+
+    public int Rear()
+    {
+        if (count == 0)
+            return -1;
+        return elements[end];
+    }
+
+    public bool IsEmpty()
+    {
+        return count == 0;
+    }
+
+    public bool IsFull()
+    {
+        return count == capacity;
+    }
+}
---
+++ csharp/LeetTests/DesignCircularQueueTest.cs
@@ -0,0 +1,21 @@
+using Leet.DesignCircularQueue;
+
+namespace LeetTests;
+
+internal class DesignCircularQueueTest
+{
+    [Test]
+    public void TestDesignCircularQueue()
+    {
+        MyCircularQueue myCircularQueue = new MyCircularQueue(3);
+        Assert.That(myCircularQueue.EnQueue(1), Is.EqualTo(true));  // return True
+        Assert.That(myCircularQueue.EnQueue(2), Is.EqualTo(true));  // return True
+        Assert.That(myCircularQueue.EnQueue(3), Is.EqualTo(true));  // return True
+        Assert.That(myCircularQueue.EnQueue(4), Is.EqualTo(false)); // return False
+        Assert.That(myCircularQueue.Rear(), Is.EqualTo(3));         // return 3
+        Assert.That(myCircularQueue.IsFull(), Is.EqualTo(true));    // return True
+        Assert.That(myCircularQueue.DeQueue(), Is.EqualTo(true));   // return True
+        Assert.That(myCircularQueue.EnQueue(4), Is.EqualTo(true));  // return True
+        Assert.That(myCircularQueue.Rear(), Is.EqualTo(4));         // return 4
+    }
+}
