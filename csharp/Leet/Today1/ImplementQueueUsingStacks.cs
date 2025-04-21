4d13fcd6ff34221c759dcf83e8bb11a20cbc839d
Zen Software <ZenSoftware@users.noreply.github.com>
2025-04-21 07:27:23 -0600

    refactor

--- csharp/Leet/Today1/ImplementQueueUsingStacks.cs
+++ csharp/Leet/Today1/ImplementQueueUsingStacks.cs
@@ -3,53 +3,41 @@ namespace Leet.ImplementQueueUsingStacks;
 
 public class MyQueue
 {
-    List<int> left = new List<int>();
-    List<int> right = new List<int>();
+    Stack<int> left = new Stack<int>();
+    Stack<int> right = new Stack<int>();
 
     public MyQueue() { }
 
     public void Push(int x)
     {
-        left.Add(x);
+        left.Push(x);
     }
 
     public int Pop()
     {
         if (right.Count > 0)
-        {
-            var left_pop = right[right.Count - 1];
-            right.RemoveAt(right.Count - 1);
-            return left_pop;
-        }
+            return right.Pop();
         else
         {
             while (left.Count > 1)
             {
-                var left_pop = left[left.Count - 1];
-                left.RemoveAt(left.Count - 1);
-                right.Add(left_pop);
+                right.Push(left.Pop());
             }
-            var res = left[0];
-            left.RemoveAt(0);
-            return res;
+            return left.Pop();
         }
     }
 
     public int Peek()
     {
         if (right.Count > 0)
-        {
-            return right[^1];
-        }
+            return right.Peek();
         else
         {
             while (left.Count > 0)
             {
-                var left_pop = left[left.Count - 1];
-                left.RemoveAt(left.Count - 1);
-                right.Add(left_pop);
+                right.Push(left.Pop());
             }
-            return right[^1];
+            return right.Peek();
         }
     }
 
