c1f19c1f302d603f70f9d0430abb708f4f723fb7
Zen Software <ZenSoftware@users.noreply.github.com>
2025-04-21 01:50:47 -0600

    solution

---
+++ csharp/Leet/Today1/ImplementQueueUsingStacks.cs
@@ -0,0 +1,59 @@
+namespace Leet.ImplementQueueUsingStacks;
+
+public class MyQueue
+{
+    List<int> left = new List<int>();
+    List<int> right = new List<int>();
+
+    public MyQueue() { }
+
+    public void Push(int x)
+    {
+        left.Add(x);
+    }
+
+    public int Pop()
+    {
+        if (right.Count > 0)
+        {
+            var left_pop = right[right.Count - 1];
+            right.RemoveAt(right.Count - 1);
+            return left_pop;
+        }
+        else
+        {
+            while (left.Count > 1)
+            {
+                var left_pop = left[left.Count - 1];
+                left.RemoveAt(left.Count - 1);
+                right.Add(left_pop);
+            }
+            var res = left[0];
+            left.RemoveAt(0);
+            return res;
+        }
+    }
+
+    public int Peek()
+    {
+        if (right.Count > 0)
+        {
+            return right[^1];
+        }
+        else
+        {
+            while (left.Count > 0)
+            {
+                var left_pop = left[left.Count - 1];
+                left.RemoveAt(left.Count - 1);
+                right.Add(left_pop);
+            }
+            return right[^1];
+        }
+    }
+
+    public bool Empty()
+    {
+        return left.Count == 0 && right.Count == 0;
+    }
+}
---
+++ csharp/LeetTests/ImplementQueueUsingStacksTest.cs
@@ -0,0 +1,29 @@
+using Leet.ImplementQueueUsingStacks;
+namespace LeetTests;
+
+internal class ImplementQueueUsingStacksTest
+{
+    [Test]
+    public void TestImplementQueueUsingStacks()
+    {
+        MyQueue myQueue = new MyQueue();
+        myQueue.Push(1); // queue is: [1]
+        myQueue.Push(2); // queue is: [1, 2] (leftmost is front of the queue)
+        Assert.That(myQueue.Peek(), Is.EqualTo(1)); // return 1
+        Assert.That(myQueue.Pop(), Is.EqualTo(1)); // return 1, queue is [2]
+        Assert.That(myQueue.Empty(), Is.EqualTo(false)); // return false
+    }
+
+    [Test]
+    public void TestImplementQueueUsingStacks2()
+    {
+        //["MyQueue","push","push","peek","push","peek"]
+        //[    [],     [1],   [2],   [],    [3],   []]
+        MyQueue myQueue = new MyQueue();
+        myQueue.Push(1);
+        myQueue.Push(2);
+        Assert.That(myQueue.Peek(), Is.EqualTo(1));
+        myQueue.Push(3);
+        Assert.That(myQueue.Peek(), Is.EqualTo(1));
+    }
+}
