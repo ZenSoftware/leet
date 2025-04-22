04087ab796ee93dd9788154f11a0a71cc91270f7
Zen Software <ZenSoftware@users.noreply.github.com>
2025-04-22 14:56:36 -0600

    solution

--- csharp/Leet/Today2/InsertInterval.cs
+++ csharp/Leet/Today2/InsertInterval.cs
@@ -5,38 +5,28 @@ public class Solution
 {
     public int[][] Insert(int[][] intervals, int[] newInterval)
     {
-        var insertionIndex = GetInsertionIndex(intervals, newInterval);
-        intervals = [.. intervals[..insertionIndex], newInterval, .. intervals[insertionIndex..]];
         var result = new List<int[]>();
         var i = 0;
+        while (i < intervals.Length && intervals[i][1] < newInterval[0])
+        {
+            result.Add(intervals[i]);
+            i++;
+        }
+
+        while (i < intervals.Length && newInterval[1] >= intervals[i][0])
+        {
+            newInterval[0] = Math.Min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.Max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        result.Add(newInterval);
+
         while (i < intervals.Length)
         {
-            var largest = intervals[i][1];
-            var j = i + 1;
-            while (j < intervals.Length && largest >= intervals[j][0])
-            {
-                largest = Math.Max(largest, intervals[j][1]);
-                j++;
-            }
-            result.Add([intervals[i][0], largest]);
-            i = j;
+            result.Add(intervals[i]);
+            i++;
         }
-        return result.ToArray();
-    }
 
-    public int GetInsertionIndex(int[][] intervals, int[] newInterval)
-    {
-        int left = 0, right = intervals.Length - 1;
-        while (left <= right)
-        {
-            var mid = left + ((right - left) / 2);
-            if (newInterval[0] < intervals[mid][0])
-                right = mid - 1;
-            else if (intervals[mid][0] < newInterval[0])
-                left = mid + 1;
-            else
-                return mid;
-        }
-        return left;
+        return result.ToArray();
     }
 }
