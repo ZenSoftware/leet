5d24a404e593bd0812bf1f832a3688b77ec32d38
Zen Software <ZenSoftware@users.noreply.github.com>
2025-04-23 01:37:53 -0600

    solution

--- csharp/Leet/Today3/SubtreeOfAnotherTree.cs
+++ csharp/Leet/Today3/SubtreeOfAnotherTree.cs
@@ -6,29 +6,43 @@ public class Solution
 {
     public bool IsSubtree(TreeNode root, TreeNode subRoot)
     {
-        TreeNode? FindSubtree(TreeNode root)
-        {
-            if (root == null || root.val == subRoot.val)
-                return root;
-            var left = FindSubtree(root.left);
-            if (left != null)
-                return left;
-            return FindSubtree(root.right);
-        }
-
-        var s = FindSubtree(root);
-        if (s == null)
+        var possibleSubTrees = GetPossibleSubTrees(root, subRoot);
+        if (possibleSubTrees.Count == 0)
             return false;
 
-        bool TreesEqual(TreeNode a, TreeNode b)
+        foreach (var tree in possibleSubTrees)
         {
-            if (a == null && b == null)
+            if (TreesEqual(tree, subRoot))
                 return true;
-            if (a?.val != b?.val)
-                return false;
-            return TreesEqual(a.left, b.left) && TreesEqual(a.right, b.right);
         }
 
-        return TreesEqual(s, subRoot);
+        return false;
+    }
+
+    private bool TreesEqual(TreeNode a, TreeNode b)
+    {
+        if (a == null && b == null)
+            return true;
+        if (a?.val != b?.val)
+            return false;
+        return TreesEqual(a.left, b.left) && TreesEqual(a.right, b.right);
+    }
+
+    private List<TreeNode> GetPossibleSubTrees(TreeNode root, TreeNode subRoot)
+    {
+        var trees = new List<TreeNode>();
+
+        void FindSubtree(TreeNode root)
+        {
+            if (root == null)
+                return;
+            if (root.val == subRoot.val)
+                trees.Add(root);
+            FindSubtree(root.left);
+            FindSubtree(root.right);
+        }
+
+        FindSubtree(root);
+        return trees;
     }
 }
