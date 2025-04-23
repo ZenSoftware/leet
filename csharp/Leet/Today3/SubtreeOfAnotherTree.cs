aac27c44f1ed64ab5de3a55f1fa239fcd7e92ebb
Zen Software <ZenSoftware@users.noreply.github.com>
2025-04-23 01:44:51 -0600

    simplified

--- csharp/Leet/Today3/SubtreeOfAnotherTree.cs
+++ csharp/Leet/Today3/SubtreeOfAnotherTree.cs
@@ -6,17 +6,11 @@ public class Solution
 {
     public bool IsSubtree(TreeNode root, TreeNode subRoot)
     {
-        var possibleSubTrees = GetPossibleSubTrees(root, subRoot);
-        if (possibleSubTrees.Count == 0)
+        if (root == null)
             return false;
-
-        foreach (var tree in possibleSubTrees)
-        {
-            if (TreesEqual(tree, subRoot))
-                return true;
-        }
-
-        return false;
+        if (TreesEqual(root, subRoot))
+            return true;
+        return IsSubtree(root.left, subRoot) || IsSubtree(root.right, subRoot);
     }
 
     private bool TreesEqual(TreeNode a, TreeNode b)
@@ -26,23 +20,5 @@ public class Solution
         if (a?.val != b?.val)
             return false;
         return TreesEqual(a.left, b.left) && TreesEqual(a.right, b.right);
-    }
-
-    private List<TreeNode> GetPossibleSubTrees(TreeNode root, TreeNode subRoot)
-    {
-        var trees = new List<TreeNode>();
-
-        void FindSubtree(TreeNode root)
-        {
-            if (root == null)
-                return;
-            if (root.val == subRoot.val)
-                trees.Add(root);
-            FindSubtree(root.left);
-            FindSubtree(root.right);
-        }
-
-        FindSubtree(root);
-        return trees;
     }
 }
