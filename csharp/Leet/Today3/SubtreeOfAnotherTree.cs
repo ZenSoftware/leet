b3e9846877f73d34a538f2a08651b14ba7d616cb
Zen Software <ZenSoftware@users.noreply.github.com>
2025-04-23 02:01:56 -0600

    simplified

--- csharp/Leet/Today3/SubtreeOfAnotherTree.cs
+++ csharp/Leet/Today3/SubtreeOfAnotherTree.cs
@@ -8,17 +8,15 @@ public class Solution
     {
         if (root == null)
             return false;
-        if (IsSameTree(root, subRoot))
-            return true;
-        return IsSubtree(root.left, subRoot) || IsSubtree(root.right, subRoot);
+        return IsSameTree(root, subRoot)
+            || IsSubtree(root.left, subRoot)
+            || IsSubtree(root.right, subRoot);
     }
 
     private bool IsSameTree(TreeNode a, TreeNode b)
     {
-        if (a?.val != b?.val)
-            return false;
-        if (a == null && b == null)
-            return true;
-        return IsSameTree(a.left, b.left) && IsSameTree(a.right, b.right);
+        if (a == null && b == null) return true;
+        if (a == null || b == null) return false;
+        return a.val == b.val && IsSameTree(a.left, b.left) && IsSameTree(a.right, b.right);
     }
 }
