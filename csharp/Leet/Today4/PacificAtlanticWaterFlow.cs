c9f0182389ad9cca382641f791490b8b5dafeaf0
Zen Software <ZenSoftware@users.noreply.github.com>
2025-04-25 01:23:05 -0600

    solution

---
+++ csharp/Leet/Today4/PacificAtlanticWaterFlow.cs
@@ -0,0 +1,66 @@
+// https://leetcode.com/problems/pacific-atlantic-water-flow/description/
+namespace Today4.PacificAtlanticWaterFlow;
+
+public class Solution
+{
+    public IList<IList<int>> PacificAtlantic(int[][] heights)
+    {
+        var ROWS = heights.Length;
+        var COLS = heights[0].Length;
+        var result = new List<IList<int>>();
+        var visited = new HashSet<(int Row, int Col)>();
+
+        bool IsPacificFlow(int row, int col, int prev)
+        {
+            if (row < 0 || col < 0)
+                return true;
+            if (row >= ROWS || col >= COLS || heights[row][col] > prev || visited.Contains((row, col)))
+                return false;
+
+            visited.Add((row, col));
+            var cur = heights[row][col];
+            if (IsPacificFlow(row - 1, col, cur)
+                || IsPacificFlow(row + 1, col, cur)
+                || IsPacificFlow(row, col - 1, cur)
+                || IsPacificFlow(row, col + 1, cur))
+            {
+                visited.Remove((row, col));
+                return true;
+            }
+            visited.Remove((row, col));
+            return false;
+        }
+
+        bool IsAtlanticFlow(int row, int col, int prev)
+        {
+            if (row >= ROWS || col >= COLS)
+                return true;
+            if (row < 0 || col < 0 || heights[row][col] > prev || visited.Contains((row, col)))
+                return false;
+
+            visited.Add((row, col));
+            var cur = heights[row][col];
+            if (IsAtlanticFlow(row - 1, col, cur)
+                || IsAtlanticFlow(row + 1, col, cur)
+                || IsAtlanticFlow(row, col - 1, cur)
+                || IsAtlanticFlow(row, col + 1, cur))
+            {
+                visited.Remove((row, col));
+                return true;
+            }
+            visited.Remove((row, col));
+            return false;
+        }
+
+        for (int i = 0; i < ROWS; i++)
+        {
+            for (int j = 0; j < COLS; j++)
+            {
+                if (IsPacificFlow(i, j, int.MaxValue) && IsAtlanticFlow(i, j, int.MaxValue))
+                    result.Add([i, j]);
+            }
+        }
+
+        return result;
+    }
+}
---
+++ csharp/LeetTests/Today4/PacificAtlanticWaterFlowTest.cs
@@ -0,0 +1,25 @@
+using Today4.PacificAtlanticWaterFlow;
+namespace Today4;
+
+internal class PacificAtlanticWaterFlowTest()
+{
+    [Test]
+    public void Test1()
+    {
+        var solution = new Solution();
+        int[][] heights = [[1, 2, 2, 3, 5], [3, 2, 3, 4, 4], [2, 4, 5, 3, 1], [6, 7, 1, 4, 5], [5, 1, 1, 2, 4]];
+        var result = solution.PacificAtlantic(heights);
+        int[][] answer = [[0, 4], [1, 3], [1, 4], [2, 2], [3, 0], [3, 1], [4, 0]];
+        Assert.That(result, Is.EqualTo(answer));
+    }
+
+    [Test]
+    public void Test2()
+    {
+        var solution = new Solution();
+        int[][] heights = [[1]];
+        var result = solution.PacificAtlantic(heights);
+        int[][] answer = [[0, 0]];
+        Assert.That(result, Is.EqualTo(answer));
+    }
+}
