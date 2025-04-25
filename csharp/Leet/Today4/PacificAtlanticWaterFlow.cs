b247867f2fce397ae668708ddcfe5321ee58ea2d
Zen Software <ZenSoftware@users.noreply.github.com>
2025-04-25 02:11:44 -0600

    efficiency

--- csharp/Leet/Today4/PacificAtlanticWaterFlow.cs
+++ csharp/Leet/Today4/PacificAtlanticWaterFlow.cs
@@ -8,22 +8,26 @@ public class Solution
         var ROWS = heights.Length;
         var COLS = heights[0].Length;
         var result = new List<IList<int>>();
-        var visited = new HashSet<(int, int)>();
+        var visited = new bool[ROWS][];
+        for (int r = 0; r < ROWS; r++)
+        {
+            visited[r] = new bool[COLS];
+        }
 
         bool IsPacificFlow(int row, int col, int prev)
         {
             if (row < 0 || col < 0)
                 return true;
-            if (row >= ROWS || col >= COLS || heights[row][col] > prev || visited.Contains((row, col)))
+            if (row >= ROWS || col >= COLS || heights[row][col] > prev || visited[row][col])
                 return false;
 
-            visited.Add((row, col));
+            visited[row][col] = true;
             var cur = heights[row][col];
             var result = IsPacificFlow(row - 1, col, cur)
                 || IsPacificFlow(row, col - 1, cur)
                 || IsPacificFlow(row + 1, col, cur)
                 || IsPacificFlow(row, col + 1, cur);
-            visited.Remove((row, col));
+            visited[row][col] = false;
             return result;
         }
 
@@ -31,16 +35,16 @@ public class Solution
         {
             if (row >= ROWS || col >= COLS)
                 return true;
-            if (row < 0 || col < 0 || heights[row][col] > prev || visited.Contains((row, col)))
+            if (row < 0 || col < 0 || heights[row][col] > prev || visited[row][col])
                 return false;
 
-            visited.Add((row, col));
+            visited[row][col] = true;
             var cur = heights[row][col];
             var result = IsAtlanticFlow(row + 1, col, cur)
                 || IsAtlanticFlow(row, col + 1, cur)
                 || IsAtlanticFlow(row - 1, col, cur)
                 || IsAtlanticFlow(row, col - 1, cur);
-            visited.Remove((row, col));
+            visited[row][col] = false;
             return result;
         }
 
