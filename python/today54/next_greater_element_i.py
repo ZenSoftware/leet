d708acf32b52d54c3f1a208d9e904a1c880d8480
Zen Software <ZenSoftware@users.noreply.github.com>
2025-09-15 00:54:34 -0600

    effecient

--- python/today54/next_greater_element_i.py
+++ python/today54/next_greater_element_i.py
@@ -4,14 +4,19 @@ from typing import List
 
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        stack = []
+        ans = {}
+
+        for r in range(len(nums2) - 1, -1, -1):
+            while stack and stack[-1] <= nums2[r]:
+                stack.pop()
+            if not stack:
+                ans[nums2[r]] = -1
+            else:
+                ans[nums2[r]] = stack[-1]
+            stack.append(nums2[r])
+
         result = []
         for n in nums1:
-            i = nums2.index(n) + 1
-            next_greater = -1
-            while i < len(nums2):
-                if nums2[i] > n:
-                    next_greater = nums2[i]
-                    break
-                i += 1
-            result.append(next_greater)
+            result.append(ans[n])
         return result
