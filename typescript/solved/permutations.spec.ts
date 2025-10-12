9206b2dd57fadbc0a8443c217750f06968fe2a7d
Zen Software <ZenSoftware@users.noreply.github.com>
2025-10-12 04:51:27 -0600

    fixed test

--- typescript/solved/permutations.spec.ts
+++ typescript/solved/permutations.spec.ts
@@ -5,9 +5,9 @@ describe('Permutations', () => {
     const result = permute([1, 2, 3]);
     expect(permute([1, 2, 3])).toEqual([
       [1, 2, 3],
-      [1, 3, 2],
       [2, 1, 3],
       [2, 3, 1],
+      [1, 3, 2],
       [3, 1, 2],
       [3, 2, 1],
     ]);
