825703293c5dab5d4a7d587176aef7753d641ef1
Zen Software <ZenSoftware@users.noreply.github.com>
2025-11-12 17:22:01 -0700

    remove

--- python/today55/perfect_number.py
+++
@@ -1,7 +0,0 @@
-class Solution(object):
-    def checkPerfectNumber(self, num):
-        total = 0
-        for i in range(1, num):
-            if num % i == 0:
-                total += i
-        return total == num
--- python/today55/perfect_number_test.py
+++
@@ -1,9 +0,0 @@
-from perfect_number import Solution
-
-
-def test1():
-    assert Solution().checkPerfectNumber(28) == True
-
-
-def test2():
-    assert Solution().checkPerfectNumber(7) == False
