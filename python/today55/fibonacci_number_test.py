9dfcf86cca420cd8906402581c623c096a9e0204
Zen Software <ZenSoftware@users.noreply.github.com>
2025-11-12 20:18:05 -0700

    brute force

---
+++ python/today55/fibonacci_number_test.py
@@ -0,0 +1,13 @@
+from fibonacci_number import Solution
+
+
+def test1():
+    assert Solution().fib(2) == 1
+
+
+def test2():
+    assert Solution().fib(3) == 2
+
+
+def test3():
+    assert Solution().fib(4) == 3
