025102404555a7b2251cf2fda0affaeae90917bb
Zen Software <ZenSoftware@users.noreply.github.com>
2025-11-15 01:46:56 -0700

    indexed

--- python/today56/coin_change_ii.py
+++ python/today56/coin_change_ii.py
@@ -5,23 +5,22 @@ class Solution(object):
     def change(self, amount, coins):
         memo = {}
 
-        def search(current, remaining):
-            if (current, len(remaining)) in memo:
-                return memo[(current, len(remaining))]
-            if len(remaining) == 0:
+        def search(current, i):
+            if (current, i) in memo:
+                return memo[(current, i)]
+            if i == len(coins):
                 return 0
             if current > amount:
                 return 0
             if current == amount:
-
                 return 1
 
-            with_first = search(current + remaining[0], remaining)
-            without_first = search(current, remaining[1:])
+            with_first = search(current + coins[i], i)
+            without_first = search(current, i + 1)
 
-            memo[(current + remaining[0], len(remaining))] = with_first
-            memo[current, len(remaining) - 1] = without_first
+            memo[(current + coins[i], i)] = with_first
+            memo[(current, i + 1)] = without_first
 
             return with_first + without_first
 
-        return search(0, coins)
+        return search(0, 0)
