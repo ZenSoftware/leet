f5961b5bc51eec3d7e0d1c328f04dee2f25b2b85
Zen Software <ZenSoftware@users.noreply.github.com>
2025-11-16 15:06:56 -0700

    renamed

--- python/today56/detect_capital.py
+++ python/today56/detect_capital.py
@@ -3,9 +3,9 @@
 
 class Solution(object):
     def detectCapitalUse(self, word):
-        return word.isupper() or word.islower() or self.isCamel(word)
+        return word.isupper() or word.islower() or self.isPascal(word)
 
-    def isCamel(self, word):
+    def isPascal(self, word):
         if not word:
             return False
 
