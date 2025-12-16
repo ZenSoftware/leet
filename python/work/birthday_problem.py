6472d3b6dfba22eeb938199df1f76a37f7a1552c
Zen Software <ZenSoftware@users.noreply.github.com>
2025-12-16 03:51:01 -0700

    refactor

--- python/work/birthday_problem.py
+++ python/work/birthday_problem.py
@@ -1,9 +1,7 @@
 # https://www.youtube.com/watch?v=m7hI2LulMxE&list=PLMrJAkhIeNNR3sNYvfgiKgcStwuPSts9V&index=5
-from math import factorial
 
 
 def birthday_problem(n):
-    # return factorial(365) / (factorial(365 - n) * (365**n))
     p = 1
     for i in range(1, n + 1):
         p *= (365 - i + 1) / 365
